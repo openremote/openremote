5a00497bbb6d6932e3c27c361250481617f8820f
Eric Bariaux <eric@openremote.org>
2017-01-25 21:56:21 +0100

    Empty app to start to iOS Shell

---
+++ or-shell/iOS/or-shell/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  or-shell
+//
+//  Created by Eric Bariaux on 20/01/17.
+//  Copyright © 2017 OpenRemote. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func login(_ sender: Any) {
+        print ("login")
+    }
+}
+
