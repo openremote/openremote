eada03ada6cb7e412b25cf543fdaea498b026e27
mhallin <mhallin@tinsys.com>
2017-01-26 16:41:51 +0100

    Add CORS filter this need to be fine tune on production

---
+++ container/src/main/java/org/openremote/container/web/CORSFilter.java
@@ -0,0 +1,26 @@
+package org.openremote.container.web;
+
+import io.undertow.servlet.api.FilterInfo;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class CORSFilter implements ContainerResponseFilter {
+    private static final Logger LOG = Logger.getLogger(CORSFilter.class.getName());
+    @Override
+    public void filter(final ContainerRequestContext requestContext,
+                       final ContainerResponseContext cres) throws IOException {
+        cres.getHeaders().add("Access-Control-Allow-Origin", "*");
+        cres.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        cres.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        cres.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        cres.getHeaders().add("Access-Control-Max-Age", "1209600");
+    }
+
+}
