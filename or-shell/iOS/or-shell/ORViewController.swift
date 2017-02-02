b5adbd9df18d85585fe974a0a3126517a5f493e0
William Balcaen <wbalcaen@MacBook-Pro.local>
2017-02-02 17:49:44 +0100

    iOS shell

---
+++ or-shell/iOS/or-shell/ORViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ORViewController.swift
+//  or-shell
+//
+//  Created by William Balcaen on 02/02/17.
+//  Copyright © 2017 OpenRemote. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import WebKit
+
+class ORViewcontroller : UIViewController, WKUIDelegate, WKNavigationDelegate {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let webConfiguration = WKWebViewConfiguration()
+        webConfiguration.preferences.javaScriptEnabled = true;
+        webConfiguration.ignoresViewportScaleLimits = true;
+        webConfiguration.preferences.javaScriptCanOpenWindowsAutomatically = true;
+        let myWebView = WKWebView(frame: .zero, configuration: webConfiguration)
+        myWebView.translatesAutoresizingMaskIntoConstraints = false
+        myWebView.navigationDelegate = self
+
+        view.addSubview(myWebView)
+        
+        [myWebView.topAnchor.constraint(equalTo: view.topAnchor),
+         myWebView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+         myWebView.leftAnchor.constraint(equalTo: view.leftAnchor),
+         myWebView.rightAnchor.constraint(equalTo: view.rightAnchor)].forEach  { anchor in
+            anchor.isActive = true
+        }
+        
+        var resourceURLString = Bundle.main.path(forResource: "index", ofType: "html")?.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlPathAllowed);
+        resourceURLString = "file://"+resourceURLString!
+        let myURL = URL(string: resourceURLString!)
+        myWebView.frame = view.frame
+        myWebView.loadFileURL(myURL!, allowingReadAccessTo: myURL!)
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        print("error")
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("finished")
+    }
+    
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        print("receiving")
+    }
+}
+
--- or-shell/iOS/or-shell/ViewController.swift
+++ or-shell/iOS/or-shell/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import AeroGearOAuth2
+import AeroGearHttp
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginViewController: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,8 +23,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func login(_ sender: Any) {
-        print ("login")
+    @IBAction func showLoginPage(_ sender: Any) {
+        let http = Http()
+        let keycloakConfig = Config.init(base: "http://192.168.99.100:8080/auth", authzEndpoint: "realms/master/protocol/openid-connect/auth", redirectURL: "http://192.168.99.100:8080/master", accessTokenEndpoint: "realms/master/protocol/openid-connect/token", clientId: "or-manager", refreshTokenEndpoint: "realms/master/protocol/openid-connect/token", revokeTokenEndpoint: "realms/master/protocol/openid-connect/logout", isOpenIDConnect: true, userInfoEndpoint: nil, scopes: ["openid", "email", "profile"], clientSecret: nil, accountId: nil, isWebView: true)
+        /*let keycloakConfig = KeycloakConfig(
+            clientId: "or-manager",
+            host: "http://192.168.99.100:8080/",
+            realm: "master",
+            isOpenIDConnect: true)*/
+        keycloakConfig.accountId = "admin"
+        let oauth2Module = AccountManager.addAccountWith(config: keycloakConfig, moduleClass: OAuth2Module.self)
+        http.authzModule = oauth2Module
+                oauth2Module.login {(accessToken: AnyObject?, claims: OpenIdClaim?, error: NSError?) in
+            if let userInfo = claims {
+                if let name = userInfo.name {
+                    print("name ",name)
+                }
+            }
+        }
     }
+
 }
+
 
