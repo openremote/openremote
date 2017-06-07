bc4922b6335a98d606f8c8b9585ac288c9cb6821
Eric Bariaux <eric@openremote.org>
2017-06-07 14:40:25 +0200

    Fixed source code headers

--- console/iOS/console/ViewController.swift
+++ console/iOS/console/ViewController.swift
@@ -1,10 +1,22 @@
-//
-//  ViewController.swift
-//  or-shell
-//
-//  Created by Eric Bariaux on 20/01/17.
-//  Copyright © 2017 OpenRemote. All rights reserved.
-//
+/*
+ * Copyright 2017, OpenRemote Inc.
+ *
+ * See the CONTRIBUTORS.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 import UIKit
 
