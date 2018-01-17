a968a1ce66f043ce3ed33cb500f18561eb485c5b
robin-dekkers <robin@stofloos.nl>
2018-01-17 10:13:42 +0100

    Smart Start fix

--- manager/src/main/java/org/openremote/manager/setup/builtin/ManagerDemoSetup.java
+++ manager/src/main/java/org/openremote/manager/setup/builtin/ManagerDemoSetup.java
@@ -50,7 +50,7 @@ public class ManagerDemoSetup extends AbstractManagerSetup {
     final protected boolean importDemoScenes;
 
     // Update these numbers whenever you change a RULE_STATE flag in test data
-    public static final int DEMO_RULE_STATES_APARTMENT_1 = 18;
+    public static final int DEMO_RULE_STATES_APARTMENT_1 = 19;
     public static final int DEMO_RULE_STATES_APARTMENT_2 = 9;
     public static final int DEMO_RULE_STATES_APARTMENT_3 = 0;
     public static final int DEMO_RULE_STATES_SMART_HOME = DEMO_RULE_STATES_APARTMENT_1 + DEMO_RULE_STATES_APARTMENT_2 + DEMO_RULE_STATES_APARTMENT_3;
