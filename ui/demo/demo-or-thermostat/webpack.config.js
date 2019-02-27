cd00762e2d4e8347fd5696b6f89fc5dda20c8bfd
Rich Turner <richard.turner83@gmail.com>
2019-02-27 10:02:58 +0100

    Initial commit of or-thermostat component and demo (copied from or-header)

---
+++ ui/demo/demo-or-thermostat/webpack.config.js
@@ -0,0 +1,72 @@
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var path = require("path");
+var CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        'bundle': './src/index.js'
+    },
+    output: {
+        path: __dirname + "/dist",
+        filename: "[name].[hash].js",
+        publicPath: ""
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        port: 9000,
+        contentBase: './dist',
+        publicPath: "/" + __dirname.split(path.sep).slice(-1)[0]  + "/"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            chunksSortMode: 'none',
+            inject: false,
+            template: 'index.html'
+        }),
+        new CopyWebpackPlugin([
+            {
+                from: path.dirname(require.resolve("@webcomponents/webcomponentsjs")),
+                to: "modules/@webcomponents/webcomponentsjs",
+                ignore: "!*.js"
+            }
+        ])
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: ["source-map-loader"],
+                enforce: "pre",
+                exclude: [
+                    /node_modules/
+                ]
+            },
+            {
+                test: /\.js$/,
+                include: function(modulePath) {
+                    return /(@webcomponents[\/|\\]shadycss|lit-css|styled-lit-element|lit-html|lit-element|@polymer|@lit|pwa-helpers)/.test(modulePath) || !/node_modules/.test(modulePath);
+                },
+                use: [
+                    {
+                        loader: 'babel-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: "css-loader" }
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                loader: 'url-loader',
+                options: {
+                    outputPath: "images/",
+                    limit: 10000
+                }
+            }
+        ]
+    }
+};
