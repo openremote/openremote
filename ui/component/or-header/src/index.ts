21918fdbcbb6b4dc1e7e31a380cd244d751a06b0
Rich Turner <richard.turner83@gmail.com>
2019-07-30 22:00:13 +0100

    UI updates: or-header updated to use standard color palette and remove need for style sheet

--- ui/component/or-header/src/index.ts
+++ ui/component/or-header/src/index.ts
@@ -1,20 +1,6 @@
-import {OrHeaderStyle} from "@openremote/or-header/src/or-header-style";
-import {css, html, LitElement, property, customElement} from "lit-element";
-
-const menuIcon = html`<svg height="24" viewBox="0 0 24 24" width="24"><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path></svg>`;
-
-const menu = html`<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 24 24">
-  <g>
-    <path d="M24,3c0-0.6-0.4-1-1-1H1C0.4,2,0,2.4,0,3v2c0,0.6,0.4,1,1,1h22c0.6,0,1-0.4,1-1V3z"/>
-    <path d="M24,11c0-0.6-0.4-1-1-1H1c-0.6,0-1,0.4-1,1v2c0,0.6,0.4,1,1,1h22c0.6,0,1-0.4,1-1V11z"/>
-    <path d="M24,19c0-0.6-0.4-1-1-1H1c-0.6,0-1,0.4-1,1v2c0,0.6,0.4,1,1,1h22c0.6,0,1-0.4,1-1V19z"/>
-  </g>
-</svg>
-`;
-
-const close = html`<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21.9 21.9" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 21.9 21.9">
-  <path d="M14.1,11.3c-0.2-0.2-0.2-0.5,0-0.7l7.5-7.5c0.2-0.2,0.3-0.5,0.3-0.7s-0.1-0.5-0.3-0.7l-1.4-1.4C20,0.1,19.7,0,19.5,0  c-0.3,0-0.5,0.1-0.7,0.3l-7.5,7.5c-0.2,0.2-0.5,0.2-0.7,0L3.1,0.3C2.9,0.1,2.6,0,2.4,0S1.9,0.1,1.7,0.3L0.3,1.7C0.1,1.9,0,2.2,0,2.4  s0.1,0.5,0.3,0.7l7.5,7.5c0.2,0.2,0.2,0.5,0,0.7l-7.5,7.5C0.1,19,0,19.3,0,19.5s0.1,0.5,0.3,0.7l1.4,1.4c0.2,0.2,0.5,0.3,0.7,0.3  s0.5-0.1,0.7-0.3l7.5-7.5c0.2-0.2,0.5-0.2,0.7,0l7.5,7.5c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3l1.4-1.4c0.2-0.2,0.3-0.5,0.3-0.7  s-0.1-0.5-0.3-0.7L14.1,11.3z"/>
-</svg>`;
+import {style} from "./style";
+import {customElement, html, LitElement, property, query} from "lit-element";
+import "@openremote/or-icon";
 
 @customElement("or-header")
 class OrHeader extends LitElement {
@@ -25,129 +11,60 @@ class OrHeader extends LitElement {
     @property({type: String})
     private logo = "";
 
-    static styles = css`
-                  #app-header {
-                    position: absolute;
-                    top: 0;
-                    left: 0;
-                    width: 100%;
-                    height: 60px;
-                    text-align: center;
-                    background-color: var(--app-header-background-color);
-                    color: var(--app-header-text-color);
-                    z-index: 9999999;
-                    box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.57);
-                  }
-                  
-                  .toolbar-top {
-                    display: flex;
-                    background-color: var(--app-header-background-color);
-                    padding: 0;
-                  }
-                 
-                  #logo {
-                    margin: var(--header-logo-margin, 0);
-                    height: var(--header-logo-height, 60px);
-                    display: block;
-                  }
-                  
-                  slot[name="desktop-right"] {
-                        display: none;
-                  }
-                  
-                  slot[name="desktop-right"]{
-                    margin-left: auto;
-                    margin-right: 20px;
-                  }
-                                  
-                  app-drawer {
-                    z-index: 999999;
-                    position: absolute;
-                    top: 60px;
-                    width: 100%;
-                    height: calc(100% - 60px);
-                    transition: all 300ms ease-in;
-                    transition-property: -webkit-transform;
-                    transition-property: transform;
-                    -webkit-transform: translate3d(0, -100%, 0);
-                    transform: translate3d(0, -100%, 0);
-                  }
-                  
-                  app-drawer[opened] {
-                    -webkit-transform: translate3d(0, 0, 0);
-                    transform: translate3d(0, 0, 0);
-                  }
-                  
-                 .menu-btn {
-                    background: none;
-                    border: none;
-                    fill: var(--app-header-text-color);
-                    cursor: pointer;
-                    height: 40px;
-                    width: 40px;
-                    margin: 10px 0 10px auto;
-                  }
-                  
-                  .menu-btn svg {
-                    width: 24px;
-                    height: 24px;
-                  }
-                  .menu-btn svg,
-                  .menu-btn svg path {
-                    fill: #FFF;
-                  }
-                  
-                  .drawer-list {
-                    box-sizing: border-box;
-                    width: 100%;
-                    height: 100%;
-                    padding: 24px;
-                    background: var(--app-drawer-background-color);
-                    position: relative;
-                  }
-                   slot[name="desktop-right"] {
-                    margin-left: auto;
-                   }
-                  /* Wide layout: when the viewport width is bigger than 460px, layout
-                  changes to a wide layout. */
-                  @media (min-width: 460px) {
-                    .menu-btn {
-                      display: none;
-                    }
-                    slot[name="desktop-left"],
-                    slot[name="desktop-right"] {
-                        display: block;
-                    }
-                  }
-    `;
+    @query("slot[name=mobile-bottom]")
+    protected _mobileBottomSlot?: HTMLSlotElement;
+
+    @query("div[id=mobile-bottom]")
+    protected _mobileBottomDiv?: HTMLDivElement;
+
+    static styles = style;
 
     protected render() {
         return html`
            <!-- Header -->
-            <div id="app-header">
-                <div class="toolbar-top">
-                        <div><img id="logo" src="${this.logo}" /></div>
-                        <!-- This gets hidden on a small screen-->
-                        
-                        <nav class="toolbar-list">
-                         <slot name="desktop-left"></slot>
-                        </nav>
+            <div id="header">
+                <div id="toolbar-top">
+                    <div><img id="logo" src="${this.logo}" /></div>
+                    <!-- This gets hidden on a small screen-->
                     
-                         <slot name="desktop-right"></slot>
-                    
-                        <button class="menu-btn" title="Menu" @click="${this._menuButtonClicked}">${this._drawerOpened ? close : menu}</button>
+                    <nav id="toolbar-list">
+                        <div id="desktop-left">
+                            <slot name="desktop-left"></slot>
+                        </div>
+                    </nav>
+                
+                    <div id="desktop-right">
+                        <slot name="desktop-right"></slot>
                     </div>
+                
+                    <button id="menu-btn" title="Menu" @click="${this._menuButtonClicked}">${this._drawerOpened ? html`<or-icon icon="close"></or-icon>` : html`<or-icon icon="menu"></or-icon>`}</button>
+                </div>
             </div>
             
-            <app-drawer ?opened="${this._drawerOpened}" @click="${this._close}">
-                <nav class="drawer-list">
-                     <slot name="mobile-top"></slot>
-                     
-                     <slot name="mobile-bottom"></slot>
-                </nav>
-            </app-drawer>
+            <div id="drawer" ?opened="${this._drawerOpened}" @click="${this._close}">
+                <div>                    
+                    <div id="mobile-top">
+                        <nav id="drawer-list">
+                            <slot name="mobile-top"></slot>
+                        </nav>
+                    </div>
+                    
+                    <div id="mobile-bottom">
+                        <slot name="mobile-bottom"></slot>
+                    </div>
+                </div>
+            </divr>
             
         `;
+    }
+
+    protected firstUpdated(_changedProperties: Map<PropertyKey, unknown>): void {
+        if (this._mobileBottomSlot && this._mobileBottomDiv) {
+            const childNodes = this._mobileBottomSlot.assignedNodes();
+            if (childNodes.length > 0) {
+                this._mobileBottomDiv.classList.add("has-children");
+            }
+        }
     }
 
     private _close() {
---
+++ ui/component/or-header/src/style.ts
@@ -0,0 +1,166 @@
+import {css, unsafeCSS} from "lit-element";
+import {DefaultColor1, DefaultColor2, DefaultColor3, DefaultColor4, DefaultColor5, DefaultBoxShadow, DefaultHeaderHeight} from "@openremote/core";
+
+// language=CSS
+export const style = css`
+    
+        :host {
+            --internal-or-header-color: var(--or-header-color, var(--or-app-color1, ${unsafeCSS(DefaultColor1)}));    
+            --internal-or-header-selected-color: var(--or-header-color, var(--or-app-color4, ${unsafeCSS(DefaultColor4)}));    
+            --internal-or-header-text-color: var(--or-header-text-color, var(--or-app-color3, ${unsafeCSS(DefaultColor3)}));
+            --internal-or-header-height: var(--or-header-height, ${unsafeCSS(DefaultHeaderHeight)});
+            --internal-or-header-logo-margin: var(--or-header-logo-margin, 0);
+            --internal-or-header-logo-height: var(--or-header-logo-height, var(--internal-or-header-height, ${unsafeCSS(DefaultHeaderHeight)}));
+            --internal-or-header-item-size: var(--or-header-item-size, calc(${unsafeCSS(DefaultHeaderHeight)} - 20px));
+            --internal-or-header-drawer-color: var(--or-header-drawer-color, var(--or-app-color2, ${unsafeCSS(DefaultColor2)}));
+            --internal-or-header-drawer-text-color: var(--or-header-drawer-text-color, var(--or-app-color3, ${unsafeCSS(DefaultColor3)}));
+            --internal-or-header-drawer-item-size: var(--or-header-drawer-item-size, 40px);
+            --internal-or-header-drawer-separator-color: var(--or-header-drawer-separator-color, var(--or-app-color5, ${unsafeCSS(DefaultColor5)}));
+            
+            display: block;
+        }
+          
+        #toolbar-top {
+            display: flex;
+            padding: 0;
+        }
+        
+        #logo {
+            margin: var(--internal-or-header-logo-margin);
+            height: var(--internal-or-header-logo-height);
+            display: block;
+        }
+                                        
+        #header {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: var(--internal-or-header-height);
+            text-align: center;
+            background-color: var(--internal-or-header-color);
+            color: var(--internal-or-header-text-color);
+            --or-icon-fill: var(--internal-or-header-text-color);
+            --or-icon-height: calc(var(--internal-or-header-item-size) - 10px);
+            --or-icon-width: calc(var(--internal-or-header-item-size) - 10px);
+            z-index: 9999999;
+            box-shadow: ${unsafeCSS(DefaultBoxShadow)};
+        }
+                
+        #drawer {
+            z-index: 999999;
+            position: absolute;
+            top: var(--internal-or-header-height);
+            width: 100%;
+            height: calc(100% - var(--internal-or-header-height));
+            transition: all 300ms ease-in;
+            transition-property: -webkit-transform;
+            transition-property: transform;
+            -webkit-transform: translate3d(0, -100%, 0);
+            transform: translate3d(0, -100%, 0);
+            background: var(--internal-or-header-drawer-color);
+            color: var(--internal-or-header-drawer-text-color);
+            --or-icon-fill: var(--internal-or-header-drawer-text-color);
+            --or-icon-height: calc(var(--internal-or-header-drawer-item-size) - 10px);
+            --or-icon-width: calc(var(--internal-or-header-drawer-item-size) - 10px);
+        }
+        
+        #drawer[opened] {
+            -webkit-transform: translate3d(0, 0, 0);
+            transform: translate3d(0, 0, 0);
+        }
+                        
+        #drawer > div {
+            box-sizing: border-box;
+            width: 100%;
+            height: 100%;
+            padding: 24px;            
+            position: relative;
+        }
+          
+        #menu-btn {
+            background: none;
+            border: none;
+            cursor: pointer;
+            padding: 0;
+            margin: 10px 10px 10px auto;
+        }
+        
+        #desktop-right {
+            margin-left: auto;
+            display: none;
+            margin-left: auto;
+            margin-right: 20px;
+        }
+        
+        #mobile-bottom {
+            margin-top: 20px;
+        }
+                        
+        #mobile-bottom.has-children {
+            border-top: 2px solid var(--internal-or-header-drawer-separator-color);
+        }
+         
+        ::slotted(*) {
+            opacity: 0.8;
+            cursor: pointer;
+            text-transform: uppercase;
+            text-decoration: none !important;                       
+        }
+
+        ::slotted(*), ::slotted(*:link), ::slotted(*:visited) {
+            color: inherit;
+        }
+        
+        ::slotted(*[selected]), ::slotted([slot="desktop-right"]) {
+            font-weight: 400;
+            opacity: 1;
+        }
+                
+        #header ::slotted(*) {
+            padding: 0 30px;
+            font-size: 14px;
+        }
+        
+        ::slotted([slot="desktop-left"]) {
+            line-height: calc(var(--internal-or-header-height) - 4px);
+        }
+        
+        ::slotted([slot="desktop-right"]) {
+            line-height: var(--internal-or-header-height);
+        }
+        
+        #drawer ::slotted(*) {
+            display: block;
+            line-height: var(--internal-or-header-drawer-item-size);
+            margin: 10px 0;
+        }
+        
+        ::slotted([slot="desktop-left"]) {
+            display: none;
+        }
+        
+        ::slotted([slot="desktop-left"][selected]) {
+            display: inline-block;
+        }
+        
+        /* Wide layout: when the viewport width is bigger than 460px, layout
+        changes to a wide layout. */
+        @media (min-width: 460px) {
+            #menu-btn {
+              display: none;
+            }
+            
+            #desktop-right {
+                display: block;
+            }
+            
+            ::slotted([slot="desktop-left"]) {
+                display: inline-block;
+            }
+
+            ::slotted([slot="desktop-left"][selected]) {                
+                border-bottom: 4px solid var(--internal-or-header-selected-color);
+            }
+        }
+`;
