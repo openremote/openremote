2df5a61828468a178a8236637fccadb74286e3c2
Michal Rutka <michal.rutka@gmail.com>
2019-11-21 15:47:07 +0100

    Preparation for a multinode swarm, tested on AWS

--- keycloak/docker-entrypoint.sh
+++ keycloak/docker-entrypoint.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 
-# Symlink each theme from deployment this preserves the default keycloak theme, reasons for this mechanism:
-# Cannot volume map a subdir of a named volume
-# Want all customisation to be in the /deployment dir and deployment-data volume
-if [ -d "/deployment/keycloak/themes" ]; then
-    for dir in /deployment/keycloak/themes/*; do
-        if [ -d ${dir} ]; then
-            echo "${dir##*/}"
-            ln -s ${dir} /opt/jboss/keycloak/themes/${dir##*/}
-        fi
-    done
-fi
-
 exec /opt/jboss/tools/docker-entrypoint.sh $@
 exit $?
