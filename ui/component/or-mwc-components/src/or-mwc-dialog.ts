8e91ac56c6f1ecc48d2da0cbdac131a0617c220c
Rich Turner <richard.turner83@gmail.com>
2020-04-16 16:01:29 +0100

    Added or-mwc-dialog.ts UI component

---
+++ ui/component/or-mwc-components/src/or-mwc-dialog.ts
@@ -0,0 +1,157 @@
+import {
+    css,
+    customElement,
+    html,
+    LitElement,
+    property,
+    PropertyValues,
+    query,
+    TemplateResult,
+    unsafeCSS
+} from "lit-element";
+import {MDCDialog, MDCDialogCloseEventDetail} from "@material/dialog";
+import "@openremote/or-translate";
+
+const dialogStyle = require("!!raw-loader!@material/dialog/dist/mdc.dialog.css");
+
+export interface DialogAction {
+    default?: boolean;
+    content: TemplateResult;
+    actionName: string;
+    action?: () => void;
+}
+
+export class OrMwcDialogOpenedEvent extends CustomEvent<void> {
+
+    public static readonly NAME = "or-mwc-dialog-opened";
+
+    constructor() {
+        super(OrMwcDialogOpenedEvent.NAME, {
+            bubbles: true,
+            composed: true
+        });
+    }
+}
+
+export class OrMwcDialogClosedEvent extends CustomEvent<string | undefined> {
+
+    public static readonly NAME = "or-mwc-dialog-closed";
+
+    constructor(action?: string) {
+        super(OrMwcDialogClosedEvent.NAME, {
+            detail: action,
+            bubbles: true,
+            composed: true
+        });
+    }
+}
+
+declare global {
+    export interface HTMLElementEventMap {
+        [OrMwcDialogOpenedEvent.NAME]: OrMwcDialogOpenedEvent;
+        [OrMwcDialogClosedEvent.NAME]: OrMwcDialogClosedEvent;
+    }
+}
+
+// language=CSS
+const style = css`
+    :host {
+        position: relative;
+    }
+`;
+
+@customElement("or-mwc-dialog")
+export class OrMwcDialog extends LitElement {
+
+    static get styles() {
+        return [
+            css`${unsafeCSS(dialogStyle)}`,
+            style
+        ];
+    }
+
+    @property({type: String})
+    public dialogTitle?: string;
+
+    @property({type: Object, attribute: false})
+    public dialogContent?: TemplateResult;
+
+    @property({type: Array, attribute: false})
+    public dialogActions?: DialogAction[];
+
+    @query("#dialog")
+    protected _mdcElem!: HTMLElement;
+
+    protected _mdcComponent?: MDCDialog;
+
+    public open() {
+        if (this._mdcComponent) {
+            this._mdcComponent.open();
+        }
+    }
+
+    public close(action?: string) {
+        if (this._mdcComponent) {
+            this._mdcComponent.close(action);
+        }
+    }
+
+    disconnectedCallback(): void {
+        super.disconnectedCallback();
+        if (this._mdcComponent) {
+            this._mdcComponent.destroy();
+            this._mdcComponent = undefined;
+        }
+    }
+
+    protected render() {
+
+        return html`
+            <div id="dialog"
+                class="mdc-dialog"
+                role="alertdialog"
+                aria-modal="true"
+                aria-labelledby="dialog-title"
+                aria-describedby="dialog-content"
+                @MDCDialog:opened="${() => this._onDialogOpened()}"
+                @MDCDialog:closed="${(evt: any) => this._onDialogClosed(evt.detail.action)}">
+                <div class="mdc-dialog__container">
+                    <div class="mdc-dialog__surface">
+                    <h2 class="mdc-dialog__title" id="dialog-title"><or-translate value="${this.dialogTitle}"></or-translate></h2>
+                    <div class="dialog-container mdc-dialog__content" id="dialog-content">
+                        ${this.dialogContent ? this.dialogContent : html`<div></div>`}
+                    </div>
+                    <footer class="mdc-dialog__actions">
+                        ${this.dialogActions ? this.dialogActions.map((action) => {
+                            return html`<div class="mdc-button mdc-dialog__button" ?data-mdc-dialog-button-default="${action.default}" data-mdc-dialog-action="${action.actionName}">${action.content}</div>`                                        
+                        }) : ``}
+                    </footer>
+                    </div>
+                </div>
+                <div class="mdc-dialog__scrim"></div>
+            </div>
+        `;
+    }
+
+    protected firstUpdated(_changedProperties: PropertyValues): void {
+        super.firstUpdated(_changedProperties);
+        if (this._mdcElem) {
+            this._mdcComponent = new MDCDialog(this._mdcElem);
+        }
+    }
+
+    protected _onDialogOpened() {
+        this.dispatchEvent(new OrMwcDialogOpenedEvent());
+    }
+
+    protected _onDialogClosed(action?: string) {
+        this.dispatchEvent(new OrMwcDialogClosedEvent(action));
+
+        if (action && this.dialogActions) {
+            const matchedAction = this.dialogActions.find((dialogAction) => dialogAction.actionName === action);
+            if (matchedAction && matchedAction.action) {
+                matchedAction.action();
+            }
+        }
+    }
+}
