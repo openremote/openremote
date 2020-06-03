de2eec553fa130e1648d3a65625047241fd0755d
Vincent <vincentlichtenberg@gmail.com>
2020-06-03 13:33:49 +0200

    Revert "don't wrap kpi cards into a panel wrapper"
    This reverts commit 1b31a8086e5274782b7345e3f60bbe5ce770c0eb.

--- ui/app/or-app/src/pages/page-insights.ts
+++ ui/app/or-app/src/pages/page-insights.ts
@@ -32,14 +32,6 @@ const viewerConfig: DataViewerConfig = {
             assetId:"5442Ite2XVp3Ky9X2y647a",
             attributes: ["UVIndex"]
         }]
-    },
-    "kpi": {
-        type: "kpi",
-        hideOnMobile: true,
-        defaults: [{
-            assetId:"5442Ite2XVp3Ky9X2y647a",
-            attributes: ["UVIndex"]
-        }]
     }
    }
 };
