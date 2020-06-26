3e3b4ac8a0b26be250b345dd36a3880d01eca288
Rich Turner <richard.turner83@gmail.com>
2020-06-26 17:38:43 +0100

    Minor keycloak/docker-entrypoint.sh update to try and remove stale symlinks

--- keycloak/docker-entrypoint.sh
+++ keycloak/docker-entrypoint.sh
@@ -3,6 +3,15 @@
 # Symlink each theme from deployment this preserves the default keycloak theme, reasons for this mechanism:
 # Cannot volume map a subdir of a named volume
 # Want all customisation to be in the /deployment dir and deployment-data volume
+
+# Remove any stale symlinks
+for dir in /opt/jboss/keycloak/themes/*; do
+  if [ -L ${dir} ]; then
+    rm ${dir}
+  fi
+done
+
+# Link standard themes
 if [ -d "/deployment/keycloak/themes" ]; then
     for dir in /deployment/keycloak/themes/*; do
         if [ -d ${dir} ]; then
