02159ead5d378f29f571f46ad33a137928797b93
Rich Turner <richard.turner83@gmail.com>
2021-03-19 20:37:01 +0000

    Moved test resources to main module so the manager can be started using the test setup

---
+++ test/src/test/groovy/org/openremote/test/energy/EnergyOptimisationAssetTest.groovy
@@ -0,0 +1,32 @@
+package org.openremote.test.energy
+
+import org.openremote.test.ContainerTrait
+import org.openremote.test.ManagerContainerTrait
+import spock.lang.Specification
+
+/*
+ * Copyright 2021, OpenRemote Inc.
+ *
+ * See the CONTRIBUTORS.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class EnergyOptimisationAssetTest extends Specification implements ManagerContainerTrait {
+    def "Test storage asset with consumer and producer"() {
+
+        when: "a storage asset is added"
+    }
+}
