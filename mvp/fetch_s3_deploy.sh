650b2668e1e48784f5e185b6a5c5bcc7543664d8
Michal Rutka <michal.rutka@gmail.com>
2021-04-22 10:42:20 +0200

    Improved MVP deployment script

--- mvp/fetch_s3_deploy.sh
+++ mvp/fetch_s3_deploy.sh
@@ -2,15 +2,16 @@ export DEPLOYMENT_NAME=${1:?usage $0 DEPLOYMENT_NAME}
 AWS_ECR_REPO=$(docker run --rm -v ~/.aws:/root/.aws -v /var/run/docker.sock:/var/run/docker.sock --entrypoint aws openremote/openremote-cli ecr describe-repositories --repository-names $DEPLOYMENT_NAME --query 'repositories[0].repositoryUri' --profile openremote-cli | xargs echo)
 DOCKER_PASSWORD=$(docker run --rm -v ~/.aws:/root/.aws -v /var/run/docker.sock:/var/run/docker.sock --entrypoint aws openremote/openremote-cli ecr get-login-password --profile openremote-cli)
 docker login --username AWS --password $DOCKER_PASSWORD $AWS_ECR_REPO
-docker run --rm -v ~/.aws:/root/.aws -v $(pwd):/$DEPLOYMENT_NAME openremote/openremote-cli map -f $DEPLOYMENT_NAME
+docker run --rm -v ~/.aws:/root/.aws -v $(pwd):/$DEPLOYMENT_NAME openremote/openremote-cli map -f $DEPLOYMENT_NAME -v
 echo $AWS_ECR_REPO
 docker-compose -p openremote down
 docker images --filter "reference=$AWS_ECR_REPO" -q | xargs docker rmi
 sudo rm -rf deployment
-docker run --rm -v ~/.aws:/root/.aws -v $(pwd):/$DEPLOYMENT_NAME openremote/openremote-cli map -a download -f $DEPLOYMENT_NAME/mapdata.mbtiles -v -t
-docker run --rm -v ~/.aws:/root/.aws -v $(pwd):/$DEPLOYMENT_NAME openremote/openremote-cli map -a download -f $DEPLOYMENT_NAME/deployment.tar.gz -v -t
+docker run --rm -v ~/.aws:/root/.aws -v $(pwd):/$DEPLOYMENT_NAME openremote/openremote-cli map -a download -f $DEPLOYMENT_NAME -v -t
 sudo chown -R ubuntu:ubuntu .
+mv $DEPLOYMENT_NAME/* .
 sudo tar -xf deployment.tar.gz
 sudo mv mapdata.mbtiles deployment/map/
 docker-compose -p openremote up -d
 docker images --filter "reference=openremote/*" -q | xargs docker rmi
+rmdir $DEPLOYMENT_NAME
