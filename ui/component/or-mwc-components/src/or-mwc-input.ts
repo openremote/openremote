83c5b65f74bc01bf0339f379bde07ca76f726c5c
Rich Turner <richard.turner83@gmail.com>
2021-08-23 16:19:56 +0100

    UI updates: Fixes #469 make select dropdowns scrollable again (scrolling page will cause issues but no easy fix for this known problem)

--- ui/component/or-mwc-components/src/or-mwc-input.ts
+++ ui/component/or-mwc-components/src/or-mwc-input.ts
@@ -890,7 +890,6 @@ export class OrMwcInput extends LitElement {
                     };
 
                     const menuCloseHandler = () => {
-                        Util.enableScroll();
                         const v = (this._tempValue ?? this.value);
                         window.setTimeout(() => {
                             if (this._mdcComponent) {
@@ -905,10 +904,6 @@ export class OrMwcInput extends LitElement {
                         const val = [...this._tempValue];
                         this._tempValue = undefined;
                         this.onValueChange(undefined, val);
-                    };
-
-                    const menuOpenedHandler = () => {
-                        Util.disableScroll();
                     };
 
                     return html`
@@ -944,7 +939,7 @@ export class OrMwcInput extends LitElement {
                                     </span>
                                     ${!outlined ? html`<div class="mdc-line-ripple"></div>` : ``}
                                 </div>
-                                <div id="mdc-select-menu" class="mdc-select__menu mdc-menu mdc-menu-surface mdc-menu-surface--fixed" @MDCMenuSurface:opened="${menuOpenedHandler}" @MDCMenuSurface:closed="${menuCloseHandler}">
+                                <div id="mdc-select-menu" class="mdc-select__menu mdc-menu mdc-menu-surface mdc-menu-surface--fixed" @MDCMenuSurface:closed="${menuCloseHandler}">
                                 ${getListTemplate(
                                     this.multiple ? ListType.MULTI_TICK : ListType.SELECT,
                                     opts ? html`${opts.map(([optValue, optDisplay], index) => {
