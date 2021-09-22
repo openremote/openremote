baa9c6b3bf63b7fc17337c78cc120594936cade5
Rich Turner <richard.turner83@gmail.com>
2021-09-22 09:17:59 +0100

    UI update: Asset editor, prevent disabling save button on validation errors (temporary fix until validation issues resolved)

--- ui/component/or-asset-viewer/src/index.ts
+++ ui/component/or-asset-viewer/src/index.ts
@@ -1204,7 +1204,7 @@ export class OrAssetViewer extends subscribe(manager)(translate(i18next)(LitElem
                     <div id="error-wrapper">${validationErrors.length === 0 ? `` : html`<span .title="${validationErrors.join("\n")}"><or-icon icon="alert"></or-icon><or-translate value="validation.invalidAsset"></or-translate></span>`}</div>
                     <div id="right-wrapper" class="mobileHidden">
                         ${this.asset!.createdOn ? html`<or-translate id="created-time" value="createdOnWithDate" .options="${{ date: new Date(this.asset!.createdOn!) } as TOptions<InitOptions>}"></or-translate>` : ``}
-                        ${editMode ? html`<or-mwc-input id="save-btn" .disabled="${!this.isModified() || validationErrors.length > 0}" raised .type="${InputType.BUTTON}" .label="${i18next.t("save")}" @or-mwc-input-changed="${() => this._onSaveClicked()}"></or-mwc-input>` : ``}
+                        ${editMode ? html`<or-mwc-input id="save-btn" .disabled="${!this.isModified()}" raised .type="${InputType.BUTTON}" .label="${i18next.t("save")}" @or-mwc-input-changed="${() => this._onSaveClicked()}"></or-mwc-input>` : ``}
                         ${!this._isReadonly() ? html`<or-mwc-input id="edit-btn" class="mobileHidden" outlined .type="${InputType.BUTTON}" .value="${this.editMode}" .label="${this.editMode ? i18next.t("view") : i18next.t("modify")}" icon="${this.editMode ? "eye" : "pencil"}" @or-mwc-input-changed="${() => this._onEditToggleClicked(!this.editMode!)}"></or-mwc-input>
                         `: ``}
                     </div>
