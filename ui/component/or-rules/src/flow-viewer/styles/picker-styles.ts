5918fe1e0654f38f58bd22d4f42aaaef31fad3f2
Mestiez <mestiez@outlook.com>
2021-10-31 14:19:47 +0100

    minor styling

--- ui/component/or-rules/src/flow-viewer/styles/picker-styles.ts
+++ ui/component/or-rules/src/flow-viewer/styles/picker-styles.ts
@@ -8,8 +8,7 @@ textarea{
     min-width: 150px;
     min-height: 37px;
 }
-textarea, input[type=text], input[type=number], select
-{
+textarea, input[type=text], input[type=number], select{
     font-family: inherit;
     padding: 10px;
     border-radius: var(--roundness);    
@@ -18,10 +17,11 @@ textarea, input[type=text], input[type=number], select
 }
 .attribute-label{
     padding: 5px;
-    background: rgba(0,0,0,0.2);
-    color: white;
+    background: rgba(0,0,0,0.05);
+    text-align: center;
     border-radius: var(--roundness);
     font-size: 110%;
+    color: rgb(76, 76, 76);
     font-weight: 400;
 }
 `;
