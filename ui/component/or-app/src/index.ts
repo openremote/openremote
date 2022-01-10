04a5189de6f5de38cc1eeabe7d755627b82d8dc6
Rich Turner <7072278+richturner@users.noreply.github.com>
2022-01-10 19:28:20 +0000

    UI updates: Various minor updates

--- ui/component/or-app/src/index.ts
+++ ui/component/or-app/src/index.ts
@@ -30,6 +30,7 @@ const DefaultLogo = require("../images/logo.svg");
 const DefaultMobileLogo = require("../images/logo-mobile.svg");
 const DefaultFavIcon = require("../images/favicon.ico");
 
+export {AnyAction};
 export * from "./app";
 export * from "./or-header";
 export * from "./types";
@@ -365,7 +366,7 @@ export class OrApp<S extends AppStateKeyed> extends LitElement {
         showDialog(new OrMwcDialog()
             .setHeading("language")
             .setDismissAction(null)
-            .setActions(Object.entries(DEFAULT_LANGUAGES).map(([key, value]) => {
+            .setActions(Object.entries(this.appConfig!.languages || DEFAULT_LANGUAGES).map(([key, value]) => {
                 return {
                     content: i18next.t(value),
                     actionName: key,
