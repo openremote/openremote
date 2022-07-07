e13a05b32d38cb96580e5a2e3185820f2882da7a
Martin Peeters <406336@student.fontys.nl>
2022-07-07 17:06:31 +0200

    Bugfix for moving widgets around in sidebar browser. Also improved code and stability of or-dashboard-browser.

--- ui/component/or-dashboard-builder/src/or-dashboard-browser.ts
+++ ui/component/or-dashboard-builder/src/or-dashboard-browser.ts
@@ -1,6 +1,6 @@
-import { GridStack } from "gridstack";
+import {GridStack, GridStackNode } from "gridstack";
 import {css, html, LitElement, unsafeCSS } from "lit";
-import { customElement} from "lit/decorators.js";
+import { customElement, state} from "lit/decorators.js";
 import {style} from "./style";
 import { DashboardWidgetType } from "@openremote/model";
 
@@ -40,6 +40,13 @@ const browserStyling = css`
 @customElement("or-dashboard-browser")
 export class OrDashboardBrowser extends LitElement {
 
+    @state()
+    private sidebarGrid?: GridStack;
+
+    @state()
+    private backgroundGrid?: GridStack;
+
+
     static get styles() {
         return [unsafeCSS(gridcss), unsafeCSS(extracss), browserStyling, style];
     }
@@ -47,59 +54,86 @@ export class OrDashboardBrowser extends LitElement {
     constructor() {
         super();
         this.updateComplete.then(() => {
-            if (this.shadowRoot != null) {
+            this.renderGrid();
+        })
+    }
 
-                // Setup of Sidebar
-                const sidebarElement = this.shadowRoot.getElementById("sidebarElement");
-                const sidebarItems = [
-                    {x: 0, y: 0, w: 2, h: 2, autoPosition: false, widgetType: DashboardWidgetType.CHART, locked: true, content: '<div class="sidebarItem"><or-icon icon="chart-bell-curve-cumulative"></or-icon><span class="itemText">Line Chart</span></div>'},
-/*                    {x: 2, y: 0, w: 2, h: 2, autoPosition: false, widgetType: DashboardWidgetType.CHART, locked: true, content: '<div class="sidebarItem"><or-icon icon="chart-bar"></or-icon><span>Bar Chart</span></div>'},
-                    {x: 0, y: 2, w: 2, h: 2, autoPosition: false, widgetType: DashboardWidgetType.CHART, locked: true, content: '<div class="sidebarItem"><or-icon icon="speedometer"></or-icon><span>Gauge</span></div>'},*/
-                    {x: 2, y: 0, w: 2, h: 2, autoPosition: false, widgetType: DashboardWidgetType.KPI, locked: true, content: '<div class="sidebarItem"><or-icon icon="label"></or-icon><span class="itemText">KPI</span></div>'},
-                    //{x: 2, y: 3, w: 2, h: 2, locked: true, noMove: true, content: 'w'} // Invisible widget
-                ];
-                const sidebarGrid = GridStack.init({
-                    acceptWidgets: false,
-                    column: 4,
-                    cellHeight: 67,
-                    disableOneColumnMode: true,
-                    disableResize: true,
-                    draggable: {
-                        appendTo: 'parent'
-                    },
-                    margin: 8,
-                    maxRow: 4
+    /* --------------------------------- */
 
-                    // @ts-ignore typechecking, because we can only provide an HTMLElement (which GridHTMLElement inherits)
-                }, sidebarElement);
-                sidebarGrid.load(sidebarItems);
+    protected renderGrid() {
+        const sidebarElement = this.shadowRoot?.getElementById("sidebarElement");
+        const sidebarItems = [
+            {x: 0, y: 0, w: 2, h: 2, widgetType: DashboardWidgetType.CHART, locked: true, content: '<div class="sidebarItem"><or-icon icon="chart-bell-curve-cumulative"></or-icon><span class="itemText">Line Chart</span></div>'},
+            {x: 2, y: 0, w: 2, h: 2, widgetType: DashboardWidgetType.KPI, locked: true, content: '<div class="sidebarItem"><or-icon icon="label"></or-icon><span class="itemText">KPI</span></div>'},
+            {x: 0, y: 2, w: 2, h: 2, locked: true, noMove: true, content: ''}, // Invisible widget
+            {x: 2, y: 2, w: 2, h: 2, locked: true, noMove: true, content: ''} // Invisible widget
+        ];
 
+        // Setting Sidebar height depending on sidebarItems
+        let sidebarHeight = 0;
+        sidebarItems.forEach((item) => {
+            if((item.y + item.h) > sidebarHeight) {
+                sidebarHeight = (item.y + item.h);
+            }
+        });
 
-                // Seperate Static Background grid (to make it look like the items duplicate)
-                const sidebarBgElement = this.shadowRoot.getElementById("sidebarBgElement");
-                const backgroundGrid = GridStack.init({
-                    staticGrid: true,
-                    column: 4,
-                    cellHeight: 67,
-                    disableOneColumnMode: true,
-                    margin: 8
+        // Creation of the sidebarGrid. Only loads items if already existing
+        if(this.sidebarGrid != undefined) {
+            this.sidebarGrid.removeAll();
+            this.sidebarGrid.load(sidebarItems);
+        } else {
+            this.sidebarGrid = GridStack.init({
+                acceptWidgets: false,
+                column: 4,
+                cellHeight: 67,
+                disableOneColumnMode: true,
+                disableResize: true,
+                draggable: {
+                    appendTo: 'parent'
+                },
+                margin: 8
 
-                    // @ts-ignore typechecking, because we can only provide an HTMLElement (which GridHTMLElement inherits)
-                }, sidebarBgElement);
-                backgroundGrid.load(sidebarItems);
+                // @ts-ignore typechecking, because we can only provide an HTMLElement (which GridHTMLElement inherits)
+            }, sidebarElement);
+            this.sidebarGrid.load(sidebarItems);
 
-
-                // If an item gets dropped on the main grid, the dragged item needs to be reset to the sidebar.
-                // This is done by just loading the initial/original widget back in the sidebar.
-                // @ts-ignore typechecking since we assume they are not undefined
-                sidebarGrid.on('removed', (_event: Event, items: GridStackNode[]) => {
+            // If an item gets dropped on the main grid, the dragged item needs to be reset to the sidebar.
+            // This is done by just loading the initial/original widget back in the sidebar.
+            // @ts-ignore typechecking since we assume they are not undefined
+            this.sidebarGrid.on('removed', (_event: Event, items: GridStackNode[]) => {
+                if(items.length == 1) {
                     const filteredItems = sidebarItems.filter(widget => {
                         return (widget.content == items[0].content);
-                    });  // Filter the GridstackWidgets: the input (GridstackNode) extends on GridstackWidget
-                    sidebarGrid.load([filteredItems[0]]);
-                });
-            }
-        })
+                    });
+                    this.sidebarGrid?.load([filteredItems[0]]);
+                }
+            });
+
+            // On any change, recreate the grid to prevent users moving Sidebar items around.
+            // @ts-ignore typechecking since we assume they are not undefined
+            this.sidebarGrid.on('change', (_event: Event, _items: GridStackNode[]) => {
+                this.renderGrid();
+            });
+        }
+
+
+        // Seperate Static Background grid (to make it look like the items duplicate)
+        const sidebarBgElement = this.shadowRoot?.getElementById("sidebarBgElement");
+        if(this.backgroundGrid != undefined) {
+            this.backgroundGrid.removeAll();
+            this.backgroundGrid.load(sidebarItems);
+        } else {
+            this.backgroundGrid = GridStack.init({
+                staticGrid: true,
+                column: 4,
+                cellHeight: 67,
+                disableOneColumnMode: true,
+                margin: 8
+
+                // @ts-ignore typechecking, because we can only provide an HTMLElement (which GridHTMLElement inherits)
+            }, sidebarBgElement);
+            this.backgroundGrid.load(sidebarItems);
+        }
     }
 
     protected render() {
