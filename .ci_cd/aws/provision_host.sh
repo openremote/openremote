55b085254f08c0bbc659be9788832e06f13c4e7d
Rich Turner <7072278+richturner@users.noreply.github.com>
2022-07-23 09:33:20 +0100

    CI/CD Improvements (#770)

--- .ci_cd/aws/provision_host.sh
+++ .ci_cd/aws/provision_host.sh
@@ -193,6 +193,15 @@ EOF
   if [ -n "$DNSHostedZoneRoleArn" ]; then
     PARAMS="$PARAMS ParameterKey=DNSHostedZoneRoleArn,ParameterValue=$DNSHostedZoneRoleArn"
   fi
+  
+  # Get OR VPC ID, Subnet ID and SSH Security Group ID
+  VPCID=$(aws ec2 describe-vpcs --filters Name=tag:Name,Values=or-vpc --query "Vpcs[0].VpcId" --output text $ACCOUNT_PROFILE 2>/dev/null)
+  SUBNETNAME="or-subnet-public-$(( $RANDOM % 2 + 1 ))"
+  SUBNETID=$(aws ec2 describe-subnets --filters Name=tag:Name,Values=$SUBNETNAME --query "Subnets[0].SubnetId" --output text $ACCOUNT_PROFILE 2>/dev/null)
+  SGID=$(aws ec2 describe-security-groups --filters Name=tag:Name,Values=ssh-access --query "SecurityGroups[0].GroupId" --output text $ACCOUNT_PROFILE 2>/dev/null)
+  PARAMS="$PARAMS ParameterKey=VpcId,ParameterValue=$VPCID"
+  PARAMS="$PARAMS ParameterKey=SSHSecurityGroupId,ParameterValue=$SGID"
+  PARAMS="$PARAMS ParameterKey=SubnetId,ParameterValue=$SUBNETID"
 
   # Create standard stack resources in specified account
   STACK_ID=$(aws cloudformation create-stack --capabilities CAPABILITY_NAMED_IAM --stack-name $STACK_NAME --template-body file://$TEMPLATE_PATH --parameters $PARAMS --output text $ACCOUNT_PROFILE)
--- .ci_cd/aws/ssh_whitelist.sh
+++ .ci_cd/aws/ssh_whitelist.sh
@@ -24,7 +24,14 @@ fi
 
 if [ -n "$CIDR" ]; then
   echo "Granting SSH access for CIDR '$CIDR' on AWS"
-  aws ec2 authorize-security-group-ingress --group-name ssh-access --ip-permissions "IpProtocol=tcp,FromPort=22,ToPort=22,IpRanges=[{CidrIp=$CIDR,Description=$DESCRIPTION}]" $PROFILE
+  SGID=$(aws ec2 describe-security-groups --filters Name=tag:Name,Values=ssh-access --query "SecurityGroups[0].GroupId" --output text $PROFILE)
+
+  if [ $? -ne 0 ]; then
+    echo "Failed to find ssh-access security group"
+    exit 1
+  fi
+
+  aws ec2 authorize-security-group-ingress --group-id $SGID --ip-permissions "IpProtocol=tcp,FromPort=22,ToPort=22,IpRanges=[{CidrIp=$CIDR,Description=$DESCRIPTION}]" $PROFILE
 
   if [ $? -ne 0 ]; then
     echo "SSH Access failed might not be able to SSH into host(s)"
