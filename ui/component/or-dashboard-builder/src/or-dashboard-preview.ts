d1c80b13e471767eaf9cdc87c38453b19839fb8e
Martin Peeters <406336@student.fontys.nl>
2022-09-05 13:10:47 +0200

    Improved init performance by removing duplicates of grid rendering

--- ui/component/or-dashboard-builder/src/or-dashboard-preview.ts
+++ ui/component/or-dashboard-builder/src/or-dashboard-preview.ts
@@ -131,7 +131,7 @@ export class OrDashboardPreview extends LitElement {
             this.latestChanges = changes;
             this.requestUpdate("template", oldValue);
 
-        } else {
+        } else if(newValue != undefined) {
             this._template = newValue;
             console.log("Setting up Grid.. [#1]");
             this.setupGrid(false, false);
@@ -280,8 +280,10 @@ export class OrDashboardPreview extends LitElement {
 
         // Switching edit/view mode needs recreation of Grid
         if(changedProperties.has("editMode")) {
-            console.log("Setting up Grid.. [#4]");
-            this.setupGrid(true, true);
+            if(changedProperties.get('editMode') != undefined) {
+                console.log("Setting up Grid.. [#4]");
+                this.setupGrid(true, true);
+            }
         }
 
         // Adjusting previewSize when manual pixels control changes
