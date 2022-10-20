abae1239b1a8d3d5c579f6fda61e38ccde864c2d
Neal Geilen <info@nealgeilen.nl>
2022-10-20 16:26:30 +0200

    Deleting navigation

--- ui/app/manager/src/pages/page-configuration.ts
+++ ui/app/manager/src/pages/page-configuration.ts
@@ -1,5 +1,5 @@
 import {css, html, TemplateResult} from "lit";
-import {customElement ,property} from "lit/decorators.js";
+import {customElement} from "lit/decorators.js";
 import manager from "@openremote/core";
 import "@openremote/or-components/or-panel";
 import "@openremote/or-translate";
@@ -10,15 +10,12 @@ import "@openremote/or-components/or-collapsible-panel";
 import "@openremote/or-mwc-components/or-mwc-input";
 import '@openremote/or-configuration/or-conf-realm/index'
 import '@openremote/or-configuration/or-conf-rules/index'
-import '@openremote/or-configuration/or-conf-navigation/index'
 
 export function pageConfigurationProvider(store: Store<AppStateKeyed>): PageProvider<AppStateKeyed> {
     return {
         name: "configuration",
         routes: [
             "/configuration",
-            "/configuration/:category",
-            "/configuration/:category/:subcategory"
         ],
         pageCreator: () => {
             return new PageConfiguration(store);
@@ -32,25 +29,6 @@ export class PageConfiguration extends Page<AppStateKeyed>  {
     static get styles() {
         // language=CSS
         return css`
-            .shadow{
-                box-shadow: rgb(0 0 0 / 10%) 5px 5px 5px -5px;
-            }
-            .conf-navigation-container{
-                width: 15%;
-                min-width: 250px;
-                max-width: 300px;
-                padding: 15px 0 0 30px;
-            }
-            .conf-category-content-container{
-                width: 100%;
-                padding: 15px;
-            }
-            .conf-navigation-bar{
-                background-color: #fff;
-                border-radius: 4px;
-                min-width: 150px;
-                width: 100%;
-            }
             :host {
                 --or-collapisble-panel-background-color: #fff;
             }
@@ -60,9 +38,6 @@ export class PageConfiguration extends Page<AppStateKeyed>  {
     get name(): string {
         return "configuration";
     }
-
-    @property()
-    protected _category: string = "style/realms";
 
     constructor(store: Store<AppStateKeyed>) {
         super(store);
@@ -79,11 +54,6 @@ export class PageConfiguration extends Page<AppStateKeyed>  {
         const managerConfiguration = manager.managerAppConfig
 
         return html`
-            <div class="conf-navigation-container">
-                <div class="conf-navigation-bar shadow">
-                    <or-conf-navigation></or-conf-navigation>
-                </div>
-            </div>
             <div class="conf-category-content-container">
                 <or-conf-realm .realms="${managerConfiguration?.realms}"></or-conf-realm>
                 <or-conf-rules .rules="${managerConfiguration?.pages?.rules}"></or-conf-rules>
@@ -94,10 +64,5 @@ export class PageConfiguration extends Page<AppStateKeyed>  {
     }
 
     public stateChanged(state: AppStateKeyed) {
-        this._category = !!state.app.params ? state.app.params.category : null;
-        if (this._category === null){
-            location.hash = "/configuration/style/realms"
-            this._category = "/configuration/style/realms"
-        }
     }
 }
