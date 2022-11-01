86fe9e664c892e9f74229b3f7b9cf039fad6ec1c
Rich Turner <7072278+richturner@users.noreply.github.com>
2022-11-01 15:44:33 +0000

    Improve test robustness

--- test/src/test/groovy/org/openremote/test/provisioning/UserAndAssetProvisioningTest.groovy
+++ test/src/test/groovy/org/openremote/test/provisioning/UserAndAssetProvisioningTest.groovy
@@ -706,7 +706,7 @@ class UserAndAssetProvisioningTest extends Specification implements ManagerConta
 
         then: "the broker should have published to the response topic an error message"
         conditions.eventually {
-            assert deviceNResponses.size() == 1s
+            assert deviceNResponses.size() == 1
             assert deviceNResponses.get(0) instanceof ErrorResponseMessage
             assert ((ErrorResponseMessage)deviceNResponses.get(0)).error == ErrorResponseMessage.Error.USER_DISABLED
         }
