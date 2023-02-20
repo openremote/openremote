7dd6d570e828a9ecd2d8893e63d26b7fe4038148
NinaBianca <100276915+NinaBianca@users.noreply.github.com>
2023-02-20 17:26:06 +0100

    Commit alarm object

--- manager/src/main/java/org/openremote/manager/alarm/AlarmResourceImpl.java
+++ manager/src/main/java/org/openremote/manager/alarm/AlarmResourceImpl.java
@@ -11,8 +11,8 @@ import org.openremote.model.alarm.SentAlarm;
 import org.openremote.model.alarm.AlarmAssetLink;
 import org.openremote.model.http.RequestParams;
 
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Response.Status;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response.Status;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -123,7 +123,7 @@ public class AlarmResourceImpl extends WebResource implements AlarmResource {
         List<AlarmUserLink> result = alarmService.getUserLinks(alarmId, realm);
         return result;
     }
-    
+
     @Override
     public List<AlarmAssetLink> getAssetLinks(RequestParams requestParams, Long alarmId, String realm) {
         if (alarmId == null) {
@@ -135,7 +135,7 @@ public class AlarmResourceImpl extends WebResource implements AlarmResource {
         List<AlarmAssetLink> result = alarmService.getAssetLinks(alarmId, realm);
         return result;
     }
-    
+
     protected void verifyAccess(SentAlarm sentAlarm) {
         if (sentAlarm == null) {
             LOG.fine("DENIED: Alarm not found");
@@ -153,5 +153,5 @@ public class AlarmResourceImpl extends WebResource implements AlarmResource {
         }
     }
 
-    
+
 }
