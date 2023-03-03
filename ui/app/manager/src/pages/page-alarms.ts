aa097dd6f4bedc62a886a587031d54074df18536
NinaBianca <100276915+NinaBianca@users.noreply.github.com>
2023-03-03 17:31:10 +0100

    Added initial JSON rule action for alarms

--- ui/app/manager/src/pages/page-alarms.ts
+++ ui/app/manager/src/pages/page-alarms.ts
@@ -170,7 +170,11 @@ export class PageAlarms extends Page<AppStateKeyed> {
                     gap: 12px;
                     width: 100%;
                 }
-
+                
+                .row > td[value="LOW"]{
+                    color: green;
+                }
+                
                 .column {
                     display: flex;
                     flex-direction: column;
