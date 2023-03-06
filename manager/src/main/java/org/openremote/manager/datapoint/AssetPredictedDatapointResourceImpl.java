16a6fce77c4a66c33506d5e4644af189ca09288b
Rich Turner <7072278+richturner@users.noreply.github.com>
2023-03-06 15:36:42 +0000

    Fix gradle issue and revert security checks for datapoint resources

--- manager/src/main/java/org/openremote/manager/datapoint/AssetPredictedDatapointResourceImpl.java
+++ manager/src/main/java/org/openremote/manager/datapoint/AssetPredictedDatapointResourceImpl.java
@@ -62,15 +62,15 @@ public class AssetPredictedDatapointResourceImpl extends ManagerWebResource impl
                 throw new WebApplicationException(Response.Status.FORBIDDEN);
             }
 
-            if (!isRealmActiveAndAccessible(getRequestRealmName())) {
-                LOG.info("Forbidden access for user '" + getUsername() + "': " + getRequestRealm());
-                throw new WebApplicationException(Response.Status.FORBIDDEN);
-            }
-
             Asset<?> asset = assetStorageService.find(assetId, true);
 
-            if (asset == null || !asset.getRealm().equals(getRequestRealmName())) {
+            if (asset == null) {
                 throw new WebApplicationException(Response.Status.NOT_FOUND);
+            }
+
+            if (!isRealmActiveAndAccessible(asset.getRealm())) {
+                LOG.info("Forbidden access for user '" + getUsername() + "': " + asset.getRealm());
+                throw new WebApplicationException(Response.Status.FORBIDDEN);
             }
 
             Attribute<?> attribute = asset.getAttribute(attributeName).orElseThrow(() ->
