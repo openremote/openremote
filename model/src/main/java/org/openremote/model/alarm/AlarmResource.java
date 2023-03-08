a68f100f26dfccd0fbac1cf26a06ed85b9e21454
NinaBianca <100276915+NinaBianca@users.noreply.github.com>
2023-03-08 17:10:16 +0100

    Groovy testing included

--- model/src/main/java/org/openremote/model/alarm/AlarmResource.java
+++ model/src/main/java/org/openremote/model/alarm/AlarmResource.java
@@ -20,6 +20,7 @@ public interface AlarmResource {
      */
     @Path("all")
     @GET
+    @Path("all")
     @Produces(APPLICATION_JSON)
     SentAlarm[] getAlarms(@BeanParam RequestParams requestParams);
 
