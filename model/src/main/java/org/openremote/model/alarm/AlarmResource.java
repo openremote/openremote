6d973fd3209d531b96eafaab2d658e769a528208
NinaBianca <100276915+NinaBianca@users.noreply.github.com>
2023-03-13 17:08:37 +0100

    Create and getAll works

--- model/src/main/java/org/openremote/model/alarm/AlarmResource.java
+++ model/src/main/java/org/openremote/model/alarm/AlarmResource.java
@@ -21,7 +21,6 @@ public interface AlarmResource {
     @Path("all")
     @GET
     @Produces(APPLICATION_JSON)
-    @RolesAllowed({ Constants.READ_ALARMS_ROLE })
     SentAlarm[] getAlarms(@BeanParam RequestParams requestParams);
 
     // /**
@@ -54,7 +53,6 @@ public interface AlarmResource {
      */
     @POST
     @Consumes(APPLICATION_JSON)
-    @RolesAllowed({ Constants.WRITE_ALARMS_ROLE })
     void createAlarm(@BeanParam RequestParams requestParams,
             Alarm alarm);
 
