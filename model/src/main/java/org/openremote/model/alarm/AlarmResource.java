52666fc0533a550c8eabc9c47c71e787f4fbde4f
N.B.A. Taken-Koevoets <ninataken2@hotmail.com>
2023-03-13 07:42:50 +0100

    alarm resource update

--- model/src/main/java/org/openremote/model/alarm/AlarmResource.java
+++ model/src/main/java/org/openremote/model/alarm/AlarmResource.java
@@ -20,7 +20,6 @@ public interface AlarmResource {
      */
     @Path("all")
     @GET
-    @Path("all")
     @Produces(APPLICATION_JSON)
     @RolesAllowed({ Constants.READ_ALARMS_ROLE })
     SentAlarm[] getAlarms(@BeanParam RequestParams requestParams);
