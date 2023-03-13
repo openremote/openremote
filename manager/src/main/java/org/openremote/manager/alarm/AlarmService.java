bba5c76e872c4e0721c7274849dac75bf8193efe
N.B.A. Taken-Koevoets <ninataken2@hotmail.com>
2023-03-13 07:42:50 +0100

    alarm resource update

--- manager/src/main/java/org/openremote/manager/alarm/AlarmService.java
+++ manager/src/main/java/org/openremote/manager/alarm/AlarmService.java
@@ -50,7 +50,7 @@ public class AlarmService extends RouteBuilder implements ContainerService {
     protected MessageBrokerService messageBrokerService;
     //protected Map<String, NotificationHandler> notificationHandlerMap = new HashMap<>();
 
-    protected static Processor handleNotificationProcessingException(Logger logger) {
+    protected static Processor handleAlarmProcessingException(Logger logger) {
         return exchange -> {
             Alarm alarm = exchange.getIn().getBody(Alarm.class);
             Exception exception = (Exception) exchange.getProperty(Exchange.EXCEPTION_CAUGHT);
@@ -66,17 +66,16 @@ public class AlarmService extends RouteBuilder implements ContainerService {
             if (protocolName != null) {
                 error.append(" (protocol: ").append(protocolName).append(")");
             }
-//
-//            // TODO Better exception handling - dead letter queue?
-//            if (exception instanceof NotificationProcessingException) {
-//                NotificationProcessingException processingException = (NotificationProcessingException) exception;
-//                error.append(" - ").append(processingException.getReasonPhrase());
-//                error.append(": ").append(notification.toString());
-//                logger.warning(error.toString());
-//            } else {
-//                error.append(": ").append(notification.toString());
-//                logger.log(Level.WARNING, error.toString(), exception);
-//            }
+
+            if (exception instanceof AlarmProcessingException) {
+                AlarmProcessingException processingException = (AlarmProcessingException) exception;
+                error.append(" - ").append(processingException.getReasonPhrase());
+                error.append(": ").append(alarm.toString());
+                logger.warning(error.toString());
+            } else {
+                error.append(": ").append(alarm.toString());
+                logger.log(Level.WARNING, error.toString(), exception);
+            }
 
             exchange.getMessage().setBody(false);
         };
@@ -95,16 +94,12 @@ public class AlarmService extends RouteBuilder implements ContainerService {
         this.identityService = container.getService(ManagerIdentityService.class);
         this.messageBrokerService = container.getService(MessageBrokerService.class);
         container.getService(MessageBrokerService.class).getContext().addRoutes(this);
-
-//        container.getServices(NotificationHandler.class).forEach(notificationHandler ->
-//                notificationHandlerMap.put(notificationHandler.getTypeName(), notificationHandler));
-//
-//        container.getService(ManagerWebService.class).addApiSingleton(
-//                new NotificationResourceImpl(this,
-//                        container.getService(MessageBrokerService.class),
-//                        container.getService(AssetStorageService.class),
-//                        container.getService(ManagerIdentityService.class))
-//        );
+        container.getService(ManagerWebService.class).addApiSingleton(
+                new AlarmResourceImpl(this,
+                        container.getService(MessageBrokerService.class),
+                        container.getService(AssetStorageService.class),
+                        container.getService(ManagerIdentityService.class))
+        );
     }
 
     @Override
@@ -141,18 +136,6 @@ public class AlarmService extends RouteBuilder implements ContainerService {
                     if (source == null) {
                         throw new AlarmProcessingException(MISSING_SOURCE);
                     }
-
-//                    // Validate handler and message
-//                    NotificationHandler handler = notificationHandlerMap.get(notification.getMessage().getType());
-//                    if (handler == null) {
-//                        throw new NotificationProcessingException(UNSUPPORTED_MESSAGE_TYPE, "No handler for message type: " + notification.getMessage().getType());
-//                    }
-//                    if (!handler.isValid()) {
-//                        throw new NotificationProcessingException(NOTIFICATION_HANDLER_CONFIG_ERROR, "Handler is not valid: " + handler.getTypeName());
-//                    }
-//                    if (!handler.isMessageValid(notification.getMessage())) {
-//                        throw new NotificationProcessingException(INVALID_MESSAGE);
-//                    }
 
                     // Validate access and map targets to handler compatible targets
                     String realm = null;
@@ -199,83 +182,12 @@ public class AlarmService extends RouteBuilder implements ContainerService {
                             break;
                     }
 
-                    LOG.fine("Sending " + alarm.getContent() + " alarm '" + alarm.getTitle() + "': '" + source + ":" + sourceId.get() + "'");
+                    LOG.fine("Creating " + alarm.getContent() + " alarm '" + alarm.getTitle() + "': '" + source + ":" + sourceId.get() + "'");
 
-//                    // Check access permissions
-//                    checkAccess(source, sourceId.get(), notification.getTargets(), realm, userId, isSuperUser, isRestrictedUser, assetId);
-//
-//                    // Get the list of notification targets
-//                    List<Notification.Target> mappedTargetsList = handler.getTargets(source, sourceId.get(), notification.getTargets(), notification.getMessage());
-//
-//                    if (mappedTargetsList == null || mappedTargetsList.isEmpty()) {
-//                        throw new NotificationProcessingException(MISSING_TARGETS, "Notification targets must be set");
-//                    }
-//
-//                    // Filter targets based on repeat frequency
-//                    if (!TextUtil.isNullOrEmpty(notification.getName()) && (!TextUtil.isNullOrEmpty(notification.getRepeatInterval()) || notification.getRepeatFrequency() != null)) {
-//                        mappedTargetsList = mappedTargetsList.stream()
-//                                .filter(target -> okToSendNotification(source, sourceId.get(), target, notification))
-//                                .collect(Collectors.toList());
-//                    }
-//
-//                    // Send message to each applicable target
-//                    AtomicBoolean success = new AtomicBoolean(true);
-//
-//                    mappedTargetsList.forEach(
-//                            target -> {
-//                                boolean targetSuccess = persistenceService.doReturningTransaction(em -> {
-//
-//                                    // commit the notification first to get the ID
-//                                    SentNotification sentNotification = new SentNotification()
-//                                            .setName(notification.getName())
-//                                            .setType(notification.getMessage().getType())
-//                                            .setSource(source)
-//                                            .setSourceId(sourceId.get())
-//                                            .setTarget(target.getType())
-//                                            .setTargetId(target.getId())
-//                                            .setMessage(notification.getMessage())
-//                                            .setSentOn(Date.from(timerService.getNow()));
-//
-//                                    sentNotification = em.merge(sentNotification);
-//                                    long id = sentNotification.getId();
-//
-//                                    try {
-//                                        NotificationSendResult result = handler.sendMessage(
-//                                                id,
-//                                                source,
-//                                                sourceId.get(),
-//                                                target,
-//                                                notification.getMessage());
-//
-//                                        if (result.isSuccess()) {
-//                                            LOG.fine("Notification sent '" + id + "': " + target);
-//                                        } else {
-//                                            LOG.warning("Notification failed '" + id + "': " + target + ", reason=" + result.getMessage());
-//                                            sentNotification.setError(TextUtil.isNullOrEmpty(result.getMessage()) ? "Unknown error" : result.getMessage());
-//                                        }
-//                                        // Merge the sent notification again with the message included just in case the handler modified the message
-//                                        sentNotification.setMessage(notification.getMessage());
-//                                        em.merge(sentNotification);
-//                                    } catch (Exception e) {
-//                                        LOG.log(Level.SEVERE,
-//                                                "Notification handler threw an exception whilst sending notification '" + id + "'",
-//                                                e);
-//                                        sentNotification.setError(TextUtil.isNullOrEmpty(e.getMessage()) ? "Unknown error" : e.getMessage());
-//                                        em.merge(sentNotification);
-//                                    }
-//                                    return sentNotification.getError() == null;
-//                                });
-//                                if (!targetSuccess) {
-//                                    success.set(false);
-//                                }
-//                            }
-//                    );
-//
-//                    exchange.getMessage().setBody(success.get());
                 })
                 .endDoTry()
                 .doCatch(AlarmProcessingException.class)
-                .process(handleNotificationProcessingException(LOG));
+                .process(handleAlarmProcessingException(LOG));
     }
 
     public boolean sendAlarm(Alarm alarm) {
@@ -283,6 +195,7 @@ public class AlarmService extends RouteBuilder implements ContainerService {
     }
 
     public boolean sendAlarm(Alarm alarm, Alarm.Source source, String sourceId) {
+        LOG.fine("send alarm method triggered");
         Map<String, Object> headers = new HashMap<>();
         headers.put(Alarm.HEADER_SOURCE, source);
         headers.put(Alarm.HEADER_SOURCE_ID, sourceId);
@@ -319,7 +232,6 @@ public class AlarmService extends RouteBuilder implements ContainerService {
         StringBuilder builder = new StringBuilder();
         builder.append("select n from SentAlarm n where 1=1");
         List<Object> parameters = new ArrayList<>();
-        processCriteria(builder, parameters,false);
         builder.append(" order by n.createdOn asc");
         return persistenceService.doReturningTransaction(entityManager -> {
             TypedQuery<SentAlarm> query = entityManager.createQuery(builder.toString(), SentAlarm.class);
