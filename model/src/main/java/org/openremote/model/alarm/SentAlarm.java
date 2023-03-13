30bc90b4df4a6bd672b803d0466a41db6aab9266
NinaBianca <100276915+NinaBianca@users.noreply.github.com>
2023-03-13 17:08:37 +0100

    Create and getAll works

--- model/src/main/java/org/openremote/model/alarm/SentAlarm.java
+++ model/src/main/java/org/openremote/model/alarm/SentAlarm.java
@@ -18,7 +18,7 @@ public class SentAlarm {
     @Id
     @Column(name = "ID")
     @GeneratedValue(generator = PERSISTENCE_SEQUENCE_ID_GENERATOR)
-    protected String id;
+    protected Long id;
 
     @NotNull
     @Column(name = "REALM", nullable = false, updatable = false)
@@ -56,9 +56,9 @@ public class SentAlarm {
     @Column(name = "ACKNOWLEDGED_ON", columnDefinition = "TIMESTAMP WITH TIME ZONE")
     protected Date acknowledgedOn;
 
-    public String getId() { return id; }
+    public Long getId() { return id; }
 
-    public SentAlarm setId(String id) {
+    public SentAlarm setId(Long id) {
         this.id = id;
         return this;
     }
