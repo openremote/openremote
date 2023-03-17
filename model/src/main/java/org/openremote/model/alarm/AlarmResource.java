51d978b690c4171f6addcaa083371e3c6dd61f47
N.B.A. Taken-Koevoets <ninataken2@hotmail.com>
2023-03-17 07:37:59 +0100

    single alarm page updated

--- model/src/main/java/org/openremote/model/alarm/AlarmResource.java
+++ model/src/main/java/org/openremote/model/alarm/AlarmResource.java
@@ -64,6 +64,12 @@ public interface AlarmResource {
             @PathParam("alarmId") Long alarmId,
             Alarm alarm);
 
+    @Path("{alarmId}/update")
+    @PUT
+    void updateAlarm(@BeanParam RequestParams requestParams,
+                     @PathParam("alarmId") Long alarmId,
+                     Alarm alarm);
+
     @Path("{alarmId}/setStatus")
     @PUT
     @Consumes(APPLICATION_JSON)
