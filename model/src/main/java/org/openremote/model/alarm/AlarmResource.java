50cc3b3f424cac702663d6f47af0f3afd2529e8f
NinaBianca <100276915+NinaBianca@users.noreply.github.com>
2023-03-17 17:05:11 +0100

    alarm page enhancements

--- model/src/main/java/org/openremote/model/alarm/AlarmResource.java
+++ model/src/main/java/org/openremote/model/alarm/AlarmResource.java
@@ -64,12 +64,6 @@ public interface AlarmResource {
             @PathParam("alarmId") Long alarmId,
             Alarm alarm);
 
-    @Path("{alarmId}/update")
-    @PUT
-    void updateAlarm(@BeanParam RequestParams requestParams,
-                     @PathParam("alarmId") Long alarmId,
-                     Alarm alarm);
-
     @Path("{alarmId}/setStatus")
     @PUT
     @Consumes(APPLICATION_JSON)
