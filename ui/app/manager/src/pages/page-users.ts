48ae67f99f21b93c641cf9d64ce3146b91d9763a
DonWillems <d.a.j.willems@gmail.com>
2023-04-25 16:00:30 +0200

    UI updates: User navigtion from assets page and small styling updates (#970)

--- ui/app/manager/src/pages/page-users.ts
+++ ui/app/manager/src/pages/page-users.ts
@@ -769,6 +769,9 @@ export class PageUsers extends Page<AppStateKeyed> {
         const rows = userSessionsResponse.data.map((session) => {
             return [session.remoteAddress, new Date(session.startTimeMillis), html`<or-mwc-input .type="${InputType.BUTTON}" label="${i18next.t("disconnect")}" @or-mwc-input-changed="${() => {this.disconnectSession(user, session)}}"></or-mwc-input>`]
         });
+        if (rows.length < 1){
+            return html`<or-mwc-table .rows="${[['This user has no active MQTT sessions',null]]}" .config="${{stickyFirstColumn:false}}" .columns="${cols}"></or-mwc-table>`;
+        }
 
         return html`<or-mwc-table id="session-table" .rows="${rows}" .config="${{stickyFirstColumn:false}}" .columns="${cols}"></or-mwc-table>`;
     }
