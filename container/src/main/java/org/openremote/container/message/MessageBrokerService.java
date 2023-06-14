2031838e4e144e9402989fa686c7f10a8b5bc9c6
Rich Turner <7072278+richturner@users.noreply.github.com>
2023-06-14 20:05:14 +0100

    WIP

--- container/src/main/java/org/openremote/container/message/MessageBrokerService.java
+++ container/src/main/java/org/openremote/container/message/MessageBrokerService.java
@@ -20,10 +20,13 @@
 package org.openremote.container.message;
 
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tags;
+import io.micrometer.core.instrument.binder.jvm.ExecutorServiceMetrics;
 import org.apache.camel.FluentProducerTemplate;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.DefaultErrorHandlerBuilder;
 import org.apache.camel.component.micrometer.spi.InstrumentedThreadPoolFactory;
+import org.apache.camel.component.micrometer.spi.TimedScheduledExecutorService;
 import org.apache.camel.component.snmp.SnmpComponent;
 import org.apache.camel.health.HealthCheckRegistry;
 import org.apache.camel.impl.DefaultCamelContext;
@@ -51,6 +54,7 @@ import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
 import static org.openremote.container.util.MapAccess.getString;
@@ -77,31 +81,62 @@ public class MessageBrokerService implements ContainerService {
 
         MeterRegistry meterRegistry = container.getMeterRegistry();
         final ExecutorServiceManager executorServiceManager = context.getExecutorServiceManager();
+
+        // Not using InstrumentedThreadPoolFactory directly as it only uses the ThreadPoolProfile ID for naming
         ThreadPoolFactory threadPoolFactory = new ThreadPoolFactory() {
+            private static final AtomicLong COUNTER = new AtomicLong();
+
             @Override
             public ExecutorService newCachedThreadPool(ThreadFactory threadFactory) {
-                // This is an unlimited pool used probably only be multicast aggregation
-                return new ContainerExecutor(getExecutorName("MessagingPool", threadFactory), 1, Integer.MAX_VALUE, 10, -1);
+                // This is an unlimited pool used probably only by multicast aggregation
+                ExecutorService executorService = new ContainerExecutor(
+                    getExecutorName("CachedPool", threadFactory),
+                    1,
+                    Integer.MAX_VALUE,
+                    10,
+                    -1);
+
+                if (meterRegistry != null) {
+                    executorService = ExecutorServiceMetrics.monitor(meterRegistry, executorService, name("instrumented-delegate-"));
+                }
+
+                return executorService;
             }
 
             @Override
             public ExecutorService newThreadPool(ThreadPoolProfile profile, ThreadFactory threadFactory) {
                 // This pool is used by SEDA consumers, so the endpoint parameters define the pool and queue sizes
-                return new ContainerExecutor(
-                    getExecutorName("Messaging", threadFactory),
+                ExecutorService executorService = new ContainerExecutor(
+                    getExecutorName("Pool", threadFactory),
                     profile.getPoolSize(),
                     profile.getMaxPoolSize(),
                     profile.getKeepAliveTime(),
                     profile.getMaxQueueSize()
                 );
+
+                if (meterRegistry != null) {
+                    String name = getExecutorName("", threadFactory);
+                    name = "".equals(name) ? profile.getId() : name;
+                    executorService = ExecutorServiceMetrics.monitor(meterRegistry, executorService, name(name));
+                }
+
+                return executorService;
             }
 
             @Override
             public ScheduledExecutorService newScheduledThreadPool(ThreadPoolProfile profile, ThreadFactory threadFactory) {
-                return new ContainerScheduledExecutor(
-                    getExecutorName("MessagingTasks", threadFactory),
+                ScheduledExecutorService scheduledExecutorService = new ContainerScheduledExecutor(
+                    getExecutorName("ScheduledPool", threadFactory),
                     profile.getPoolSize()
                 );
+
+                if (meterRegistry != null) {
+                    String name = getExecutorName("", threadFactory);
+                    name = "".equals(name) ? profile.getId() : name;
+                    scheduledExecutorService = new TimedScheduledExecutorService(meterRegistry, scheduledExecutorService, name(name), Tags.empty());
+                }
+
+                return scheduledExecutorService;
             }
 
             protected String getExecutorName(String name, ThreadFactory threadFactory) {
@@ -114,12 +149,11 @@ public class MessageBrokerService implements ContainerService {
                 }
                 return name;
             }
-        };
 
-        if (meterRegistry != null) {
-            // Add instrumentation to thread pool factory
-            threadPoolFactory = new InstrumentedThreadPoolFactory(meterRegistry, threadPoolFactory);
-        }
+            private String name(String prefix) {
+                return prefix + COUNTER.incrementAndGet();
+            }
+        };
 
         executorServiceManager.setThreadNamePattern("#counter# #name#");
         executorServiceManager.setThreadPoolFactory(threadPoolFactory);
