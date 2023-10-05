06e92fa56506e667f17931d7540f5e4264278946
Panos <pan.kalog16@gmail.com>
2023-10-05 18:47:03 +0200

    Teltonika MQTT Handler Improvements
    - Fixed latitude and longitude regex to properly recognize all coordinates

--- model/src/main/java/org/openremote/model/teltonika/State.java
+++ model/src/main/java/org/openremote/model/teltonika/State.java
@@ -72,7 +72,7 @@ public class State {
     }
 
     private GeoJSONPoint ParseLatLngToGeoJSONObject(String latlngString) {
-        String regexPattern = "(([0-9]{2}).([0-9]+)),(([0-9]{2}).([0-9]+))";
+        String regexPattern = "^([-+]?[0-8]?\\d(\\.\\d+)?|90(\\.0+)?),([-+]?(1?[0-7]?[0-9](\\.\\d+)?|180(\\.0+)?))$";
 
         Pattern r = Pattern.compile(regexPattern);
         Matcher m = r.matcher(latlngString);
