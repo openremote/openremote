fd78a1399360779a7c9c4db3daaa55b6ffc518d4
Rich Turner <7072278+richturner@users.noreply.github.com>
2023-11-13 14:19:39 +0000

    Reintroduce allowedValueNames into allowed values constraint (#1166)

--- ui/component/or-mwc-components/src/or-mwc-input.ts
+++ ui/component/or-mwc-components/src/or-mwc-input.ts
@@ -346,7 +346,7 @@ export const getValueHolderInputTemplateProvider: ValueInputProviderGenerator = 
         pattern = ".+";
     }
     if (allowedValuesConstraint && allowedValuesConstraint.allowedValues) {
-        const allowedLabels = allowedValuesConstraint.allowedValues;
+        const allowedLabels = allowedValuesConstraint.allowedValueNames && allowedValuesConstraint.allowedValueNames.length === allowedValuesConstraint.allowedValues.length ? allowedValuesConstraint.allowedValueNames : undefined;
         selectOptions = allowedValuesConstraint.allowedValues.map((v, i) => {
             let label = allowedLabels ? allowedLabels[i] : "" + v;
             label = Util.getAllowedValueLabel(label)!;
