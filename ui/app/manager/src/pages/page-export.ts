8d432a7d6f486f0d20d83d3d025f91e79321b70b
Rich Turner <7072278+richturner@users.noreply.github.com>
2024-01-03 22:45:50 +0000

    UI update: Fix typescript build issue (#1220)

--- ui/app/manager/src/pages/page-export.ts
+++ ui/app/manager/src/pages/page-export.ts
@@ -382,6 +382,7 @@ export class PageExport extends Page<AppStateKeyed> {
             
         }).then(response => {
             // This is the best we can do with xhr - would need to return a link to the file for proper streamed download support
+            // @ts-ignore
             const url = window.URL.createObjectURL(new Blob([response.data]));
             const link = document.createElement('a');
             link.href = url;
