b14ab63ecf028e63ad7a97e06fbd3ce0d8041e38
Rich Turner <7072278+richturner@users.noreply.github.com>
2024-02-14 20:52:58 +0000

    Updated to latest keycloak and undertow versions

--- manager/src/main/java/org/openremote/manager/security/MultiTenantClientCredentialsGrantsLoginModule.java
+++ manager/src/main/java/org/openremote/manager/security/MultiTenantClientCredentialsGrantsLoginModule.java
@@ -32,10 +32,10 @@ import org.keycloak.KeycloakPrincipal;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.adapters.AdapterUtils;
 import org.keycloak.adapters.RefreshableKeycloakSecurityContext;
-import org.keycloak.adapters.authentication.ClientCredentialsProviderUtils;
 import org.keycloak.adapters.jaas.AbstractKeycloakLoginModule;
 import org.keycloak.adapters.rotation.AdapterTokenVerifier;
 import org.keycloak.common.VerificationException;
+import org.keycloak.protocol.oidc.client.authentication.ClientCredentialsProviderUtils;
 import org.keycloak.representations.AccessToken;
 import org.keycloak.representations.AccessTokenResponse;
 import org.keycloak.representations.idm.OAuth2ErrorRepresentation;
@@ -234,7 +234,7 @@ public class MultiTenantClientCredentialsGrantsLoginModule extends AbstractKeycl
                 HttpPost post = new HttpPost(logoutUri);
 
                 List<NameValuePair> formparams = new ArrayList<>();
-                ClientCredentialsProviderUtils.setClientCredentials(deployment, post, formparams);
+                AdapterUtils.setClientCredentials(deployment, post, formparams);
                 formparams.add(new BasicNameValuePair(OAuth2Constants.REFRESH_TOKEN, refreshToken));
 
                 UrlEncodedFormEntity form = new UrlEncodedFormEntity(formparams, "UTF-8");
