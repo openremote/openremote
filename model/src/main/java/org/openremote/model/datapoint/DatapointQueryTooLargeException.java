92cf1b99f6056ab120bb050b8992cfc8cfbc98ce
MartinaeyNL <business@martinaey.nl>
2024-12-13 15:22:12 +0100

    Removed unnecessary line breaks in DatapointQueryTooLargeException

--- model/src/main/java/org/openremote/model/datapoint/DatapointQueryTooLargeException.java
+++ model/src/main/java/org/openremote/model/datapoint/DatapointQueryTooLargeException.java
@@ -2,8 +2,6 @@ package org.openremote.model.datapoint;
 
 public class DatapointQueryTooLargeException extends RuntimeException {
 
-
-
     public DatapointQueryTooLargeException(String message) {
         super(message);
     }
