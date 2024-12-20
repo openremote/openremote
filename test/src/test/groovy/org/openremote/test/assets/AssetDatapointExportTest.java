935c912dca5178db583896794f488c427aa762de
MartinaeyNL <business@martinaey.nl>
2024-12-20 13:58:35 +0100

    Added ignore annotation to temporarily skip the mTLS test; see issue #1528

---
+++ test/src/test/groovy/org/openremote/test/assets/AssetDatapointExportTest.java
@@ -0,0 +1,7 @@
+package org.openremote.test.assets;
+
+import org.openremote.test.ManagerContainerTrait;
+import spock.lang.Specification;
+
+public class AssetDatapointExportTest extends Specification implements ManagerContainerTrait {
+}
