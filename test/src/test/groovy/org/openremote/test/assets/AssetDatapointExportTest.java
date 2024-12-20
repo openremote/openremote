add74477d6857eddf5d3cbdb6a23e13e0ce6565d
MartinaeyNL <business@martinaey.nl>
2024-12-20 14:01:26 +0100

    Removed accidentally added test class

--- test/src/test/groovy/org/openremote/test/assets/AssetDatapointExportTest.java
+++
@@ -1,7 +0,0 @@
-package org.openremote.test.assets;
-
-import org.openremote.test.ManagerContainerTrait;
-import spock.lang.Specification;
-
-public class AssetDatapointExportTest extends Specification implements ManagerContainerTrait {
-}
