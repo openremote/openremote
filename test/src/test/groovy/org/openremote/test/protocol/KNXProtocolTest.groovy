337144e60fe678e2dec0cfeb76cc2381a08ecfdf
Rich Turner <7072278+richturner@users.noreply.github.com>
2025-02-04 14:04:07 +0000

    Fix tests

--- test/src/test/groovy/org/openremote/test/protocol/KNXProtocolTest.groovy
+++ test/src/test/groovy/org/openremote/test/protocol/KNXProtocolTest.groovy
@@ -69,23 +69,6 @@ class KNXProtocolTest extends Specification implements ManagerContainerTrait {
             }
         }
         def knxEmulationServer = new Launcher(configUri.toString())
-//        def sc = knxEmulationServer.xml.svcContainers.remove(0)
-//        def sc2 = new DefaultServiceContainer(
-//            sc.getName(),
-//            NetworkInterface.getByInetAddress(InetAddress.getLoopbackAddress()).name,
-//            sc.getControlEndpoint(),
-//            sc.getMediumSettings(),
-//            sc.reuseControlEndpoint(),
-//            sc.isNetworkMonitoringAllowed())
-//        knxEmulationServer.xml.svcContainers.add(sc2)
-//        def netIf = knxEmulationServer.xml.subnetNetIf.remove(sc)
-//        knxEmulationServer.xml.subnetNetIf.put(sc2, netIf)
-//        def linkClasses = knxEmulationServer.xml.subnetLinkClasses.remove(sc)
-//        knxEmulationServer.xml.subnetLinkClasses.put(sc2, linkClasses)
-//        def groupFilters = knxEmulationServer.xml.groupAddressFilters.remove(sc)
-//        knxEmulationServer.xml.groupAddressFilters.put(sc2, groupFilters)
-//        def addAddresses = knxEmulationServer.xml.additionalAddresses.remove(sc)
-//        knxEmulationServer.xml.additionalAddresses.put(sc2, addAddresses)
         def knxServerThread = new Thread(knxEmulationServer)
         knxServerThread.start()
         KNXTestingNetworkLink knxTestingNetwork
