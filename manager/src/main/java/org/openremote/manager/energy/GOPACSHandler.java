72147fcc32506964509fbfc854bc6dcc35c6647f
Michael Rademaker <michael@byrt.nl>
2025-02-12 14:54:09 +0100

    Updated to latest version of shapeshifter lib. Added attributes to ElectricitySupplierAsset for GOPACS attributes

--- manager/src/main/java/org/openremote/manager/energy/GOPACSHandler.java
+++ manager/src/main/java/org/openremote/manager/energy/GOPACSHandler.java
@@ -27,6 +27,7 @@ import org.lfenergy.shapeshifter.core.model.OutgoingUftpMessage;
 import org.lfenergy.shapeshifter.core.model.SigningDetails;
 import org.lfenergy.shapeshifter.core.model.UftpParticipant;
 import org.lfenergy.shapeshifter.core.model.UftpRoleInformation;
+import org.lfenergy.shapeshifter.core.service.ParticipantAuthorizationProvider;
 import org.lfenergy.shapeshifter.core.service.UftpParticipantService;
 import org.lfenergy.shapeshifter.core.service.crypto.LazySodiumBase64Pool;
 import org.lfenergy.shapeshifter.core.service.crypto.LazySodiumFactory;
@@ -46,12 +47,14 @@ import org.openremote.model.Container;
 import org.openremote.container.web.WebApplication;
 import org.openremote.container.web.WebService;
 import org.openremote.model.Constants;
+import org.openremote.model.asset.impl.ElectricitySupplierAsset;
 import org.openremote.model.attribute.AttributeEvent;
 import org.openremote.model.datapoint.ValueDatapoint;
 import org.openremote.model.syslog.SyslogCategory;
 import org.openremote.model.util.TextUtil;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.*;
@@ -69,7 +72,7 @@ import static org.openremote.container.web.WebTargetBuilder.createClient;
 import static org.openremote.model.syslog.SyslogCategory.API;
 
 
-public class GOPACSHandler implements UftpPayloadHandler, UftpParticipantService {
+public class GOPACSHandler implements UftpPayloadHandler, UftpParticipantService, ParticipantAuthorizationProvider {
 
     private static final Logger LOG = SyslogCategory.getLogger(API, GOPACSHandler.class);
     public static final String GOPACS_PRIVATE_KEY_FILE = "GOPACS_PRIVATE_KEY_FILE";
@@ -144,7 +147,7 @@ public class GOPACSHandler implements UftpPayloadHandler, UftpParticipantService
         this.cryptoService = new UftpCryptoService(participantResolutionService, new LazySodiumFactory(), new LazySodiumBase64Pool());
         this.uftpValidationService = new UftpValidationService(new ArrayList<>());
         this.uftpReceivedMessageService = new UftpReceivedMessageService(uftpValidationService, this);
-        this.uftpSendMessageService = new UftpSendMessageService(serializer, cryptoService, participantResolutionService, uftpValidationService);
+        this.uftpSendMessageService = new UftpSendMessageService(serializer, cryptoService, participantResolutionService, this, uftpValidationService);
 
 
         String goPacsPrivateKeyFile = container.getConfig().get(GOPACS_PRIVATE_KEY_FILE);
@@ -257,7 +260,61 @@ public class GOPACSHandler implements UftpPayloadHandler, UftpParticipantService
         }
     }
 
-    protected void handleFlexMessage(UftpParticipant participant, FlexRequest flexRequest) {
+    @Override
+    public void notifyNewIncomingMessage(IncomingUftpMessage<? extends PayloadMessageType> message) {
+        LOG.fine("Received message: " + message);
+        var messageType = message.payloadMessage().getClass();
+
+        if (FlexRequest.class.isAssignableFrom(messageType)) {
+            var flexRequest = (FlexRequest) message.payloadMessage();
+            LOG.fine("Processing FlexRequest: " + flexRequest);
+            handleFlexRequestMessage(message.sender(), flexRequest);
+            sendFlexOffer(message.sender(), flexRequest);
+            LOG.fine("Finished processing FlexRequest: " + flexRequest);
+        } else if (FlexOfferResponse.class.isAssignableFrom(messageType)) {
+            var flexOfferResponse = (FlexOfferResponse) message.payloadMessage();
+            LOG.fine("Processing FlexOfferResponse: " + flexOfferResponse);
+            handleFlexOfferResponseMessage(message.sender(), flexOfferResponse);
+            LOG.fine("Finished processing FlexOfferResponse: " + flexOfferResponse);
+        } else if (FlexOrder.class.isAssignableFrom(messageType)) {
+            var flexOrder = (FlexOrder) message.payloadMessage();
+            LOG.fine("Processing FlexOrder: " + flexOrder);
+            handleFlexOrderMessage(message.sender(), flexOrder);
+            LOG.fine("Finished processing FlexOrder: " + flexOrder);
+        }
+    }
+
+    @Override
+    public void notifyNewOutgoingMessage(OutgoingUftpMessage<? extends PayloadMessageType> message) {
+        LOG.fine("Notifying message: " + message);
+        try {
+            LOG.fine("Sending message: " + message);
+            if (message.payloadMessage() instanceof FlexRequestResponse ||
+                    message.payloadMessage() instanceof FlexOffer ||
+                    message.payloadMessage() instanceof FlexOrderResponse) {
+
+                String recipientDomain = message.payloadMessage().getRecipientDomain();
+                uftpSendMessageService.attemptToSendMessage(
+                        message.payloadMessage(),
+                        new SigningDetails(
+                                message.sender(),
+                                this.privateKey,
+                                new UftpParticipant(recipientDomain, UftpRoleInformation.getRecipientRoleBySenderRole(message.sender().role()))
+                        )
+                );
+            }
+            LOG.fine("Finished sending message: " + message);
+        } catch (Exception e) {
+            LOG.log(Level.SEVERE, "Failed to send message", e);
+        }
+    }
+
+    @Override
+    public String getAuthorizationHeader(UftpParticipant uftpParticipant) {
+        return "";
+    }
+
+    protected void handleFlexRequestMessage(UftpParticipant participant, FlexRequest flexRequest) {
         LOG.fine("Received Flex Request: " + flexRequest);
         int year = flexRequest.getPeriod().getYear();
         int month = flexRequest.getPeriod().getMonthValue();
@@ -270,7 +327,8 @@ public class GOPACSHandler implements UftpPayloadHandler, UftpParticipantService
             LocalTime start = FlexRequestISPTypeHelper.getISPStart(flexRequestISPType.getStart(), year, month, day, flexRequest.getTimeZone());
             double importMax = flexRequestISPType.getMaxPower() == 0L ? this.powerImportMax : (double) flexRequestISPType.getMaxPower() / 1000.0F;
             double exportMax = flexRequestISPType.getMinPower() == 0L ? this.powerExportMax : (double) Math.abs(flexRequestISPType.getMinPower()) / 1000.0F;
-            this.schedulePowerUpdates(start, importMax, exportMax);
+            this.schedulePowerUpdate(start, ElectricitySupplierAsset.POWER_IMPORT_MAX_GOPACS.getName(), exportMax);
+            this.schedulePowerUpdate(start, ElectricitySupplierAsset.POWER_EXPORT_MAX_GOPACS.getName(), exportMax);
 
             // Correct usage of ZoneId.of instead of ZoneOffset.of
             ZoneId zoneId = ZoneId.of(flexRequest.getTimeZone());
@@ -280,61 +338,107 @@ public class GOPACSHandler implements UftpPayloadHandler, UftpParticipantService
             powerExportMaxGopacsDatapoints.add(new ValueDatapoint<>(startEpochMilli, exportMax));
         }
 
-        this.setPredictedDataPoints("powerImportMaxGopacs", powerImportMaxGopacsDatapoints);
-        this.setPredictedDataPoints("powerExportMaxGopacs", powerExportMaxGopacsDatapoints);
+        this.setPredictedDataPoints(ElectricitySupplierAsset.POWER_IMPORT_MAX_GOPACS.getName(), powerImportMaxGopacsDatapoints);
+        this.setPredictedDataPoints(ElectricitySupplierAsset.POWER_EXPORT_MAX_GOPACS.getName(), powerExportMaxGopacsDatapoints);
         LOG.fine("Finished processing Flex Request: " + flexRequest);
     }
 
-    protected void schedulePowerUpdates(LocalTime start, double maxPower, double minPower) {
+    protected void handleFlexOfferResponseMessage(UftpParticipant participant, FlexOfferResponse flexOfferResponse) {
+        // Handle the response to our FlexOffer
+        if (flexOfferResponse.getResult() == AcceptedRejectedType.ACCEPTED) {
+            LOG.info("FlexOffer accepted: " + flexOfferResponse.getFlexOfferMessageID());
+        } else {
+            LOG.warning("FlexOffer rejected: " + flexOfferResponse.getFlexOfferMessageID());
+        }
+    }
+
+    protected void handleFlexOrderMessage(UftpParticipant participant, FlexOrder flexOrder) {
+        LOG.info("Received FlexOrder for FlexOffer: " + flexOrder.getFlexOfferMessageID());
+
+        int year = flexOrder.getPeriod().getYear();
+        int month = flexOrder.getPeriod().getMonthValue();
+        int day = flexOrder.getPeriod().getDayOfMonth();
+        List<ValueDatapoint<?>> powerDatapoints = new ArrayList<>();
+
+        for (int i = 0; i < flexOrder.getISPS().size(); i++) {
+            FlexOrderISPType flexOrderISPType = flexOrder.getISPS().get(i);
+            LocalTime start = FlexRequestISPTypeHelper.getISPStart(flexOrderISPType.getStart(), year, month, day, flexOrder.getTimeZone());
+            double power = flexOrderISPType.getPower() / 1000.0F;
+            this.schedulePowerUpdate(start, ElectricitySupplierAsset.POWER_FLEX_ORDER_GOPACS.getName(), power);
+
+            // Correct usage of ZoneId.of instead of ZoneOffset.of
+            ZoneId zoneId = ZoneId.of(flexOrder.getTimeZone());
+            long startEpochMilli = flexOrder.getPeriod().atTime(start).atZone(zoneId).toInstant().toEpochMilli();
+
+            powerDatapoints.add(new ValueDatapoint<>(startEpochMilli, power));
+        }
+
+        this.setPredictedDataPoints(ElectricitySupplierAsset.POWER_FLEX_ORDER_GOPACS.getName(), powerDatapoints);
+    }
+
+    protected void sendFlexOffer(UftpParticipant recipient, FlexRequest flexRequest) {
+        try {
+            FlexOffer flexOffer = new FlexOffer();
+            flexOffer.setMessageID(UUID.randomUUID().toString());
+            flexOffer.setCongestionPoint(flexRequest.getCongestionPoint());
+            flexOffer.setContractID(flexRequest.getContractID());
+            flexOffer.setFlexRequestMessageID(flexRequest.getMessageID());
+            flexOffer.setPeriod(flexRequest.getPeriod());
+            flexOffer.setTimeZone(flexRequest.getTimeZone());
+            flexOffer.setExpirationDateTime(flexRequest.getExpirationDateTime());
+            flexOffer.setConversationID(flexRequest.getConversationID());
+            flexOffer.setISPDuration(flexRequest.getISPDuration());
+            flexOffer.setSenderDomain(flexRequest.getSenderDomain());
+            flexOffer.setRecipientDomain(flexRequest.getRecipientDomain());
+            flexOffer.setCurrency("EUR");
+
+            // Set ISPs based on the FlexRequest ISPs
+            List<FlexOfferOptionISPType> isps = new ArrayList<>();
+            for (FlexRequestISPType requestIsp : flexRequest.getISPS()) {
+                FlexOfferOptionISPType offerIsp = new FlexOfferOptionISPType();
+                offerIsp.setStart(requestIsp.getStart());
+                offerIsp.setDuration(requestIsp.getDuration());
+                // Set power values based on your available flexibility
+                offerIsp.setPower(calculatePower(requestIsp));
+                isps.add(offerIsp);
+            }
+            FlexOfferOptionType flexOfferOptionType = new FlexOfferOptionType();
+            flexOfferOptionType.setOptionReference(UUID.randomUUID().toString());
+            flexOfferOptionType.setPrice(calculatePrice());
+            flexOfferOptionType.getISPS().addAll(isps);
+            flexOffer.getOfferOptions().add(flexOfferOptionType);
+
+            // Send the FlexOffer
+            UftpParticipant sender = new UftpParticipant(contractedEan, USEFRoleType.AGR);
+            notifyNewOutgoingMessage(OutgoingUftpMessage.create(sender, flexOffer));
+        } catch (Exception e) {
+            LOG.log(Level.SEVERE, "Failed to send FlexOffer", e);
+        }
+    }
+
+    protected void schedulePowerUpdate(LocalTime start, String attributeName, double power) {
         long currentTimeMillis = timerService.getCurrentTimeMillis();
         long ispStartMillis = start.atDate(LocalDate.now()).atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
         long delay = ispStartMillis - currentTimeMillis;
-        scheduledExecutorService.schedule(() -> updatePowerValues(maxPower, minPower), delay, TimeUnit.MILLISECONDS);
+        scheduledExecutorService.schedule(() -> updatePowerValues(attributeName, power), delay, TimeUnit.MILLISECONDS);
     }
 
-    protected void updatePowerValues(double maxPower, double minPower) {
-        assetProcessingService.sendAttributeEvent(new AttributeEvent(electricitySupplierAssetId, "powerImportMaxGopacs", maxPower), getClass().getSimpleName());
-        assetProcessingService.sendAttributeEvent(new AttributeEvent(electricitySupplierAssetId, "powerExportMaxGopacs", minPower), getClass().getSimpleName());
+    protected void updatePowerValues(String attributeName, double power) {
+        assetProcessingService.sendAttributeEvent(new AttributeEvent(electricitySupplierAssetId, attributeName, power), getClass().getSimpleName());
     }
 
     protected void setPredictedDataPoints(String attributeName, List<ValueDatapoint<?>> valuesAndTimestamps) {
         assetPredictedDatapointService.updateValues(electricitySupplierAssetId, attributeName, valuesAndTimestamps);
     }
 
-    @Override
-    public void notifyNewIncomingMessage(IncomingUftpMessage<? extends PayloadMessageType> message) {
-        LOG.fine("Received message: " + message);
-        var messageType = message.payloadMessage().getClass();
-        if (!FlexRequest.class.isAssignableFrom(messageType)) {
-            return;
-        }
-
-        var flexRequest = (FlexRequest) message.payloadMessage();
-
-        LOG.fine("Processing message: " + flexRequest);
-
-        this.handleFlexMessage(message.sender(), flexRequest);
-
-        LOG.fine("Finished processing message: " + flexRequest);
+    protected BigDecimal calculatePrice() {
+        //TODO: Implement price calculation logic here
+        return BigDecimal.valueOf(0); // Price in cents per kWh
     }
 
-    @Override
-    public void notifyNewOutgoingMessage(OutgoingUftpMessage<? extends PayloadMessageType> message) {
-        LOG.fine("Notifying message: " + message);
-        var messageType = message.payloadMessage().getClass();
-        if (!FlexRequestResponse.class.isAssignableFrom(messageType)) {
-            return;
-        }
-
-        var flexRequestResponse = (FlexRequestResponse) message.payloadMessage();
-
-        try {
-            LOG.fine("Sending message: " + message);
-            uftpSendMessageService.attemptToSendMessage(message.payloadMessage(), new SigningDetails(message.sender(), this.privateKey, new UftpParticipant(flexRequestResponse.getRecipientDomain(), UftpRoleInformation.getRecipientRoleBySenderRole(message.sender().role()))));
-            LOG.fine("Finished sending message: " + message);
-        } catch (Exception e) {
-            LOG.log(Level.SEVERE, "Failed to send message", e);
-        }
+    protected long calculatePower(FlexRequestISPType requestIsp) {
+        //TODO: Implement power calculation logic here
+        return requestIsp.getMaxPower() == 0 ? requestIsp.getMinPower() : requestIsp.getMaxPower();
     }
 
     protected void processRawMessage(String transportXml) {
