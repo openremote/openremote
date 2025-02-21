b64d41dfe090bd3d27fa9fcf04f98b680b8058fa
MartinaeyNL <business@martinaey.nl>
2025-02-21 11:50:30 +0100

    Modifications to SQL widget migration script, and simplified it. Authored by @mihaelaaleks

--- manager/src/main/resources/org/openremote/manager/setup/database/V20250217_01__KeepExistingWidgetConfig.sql
+++ manager/src/main/resources/org/openremote/manager/setup/database/V20250217_01__KeepExistingWidgetConfig.sql
@@ -1,35 +1,19 @@
-/*
-* Set existing widgets configs to match their pre-update behavior.
-*/
-
-WITH json_data AS (
-    SELECT
-        id,
-        template,
-        COALESCE(
-            jsonb_set(
-                template,
-                '{widgets}',
-                (
-                    SELECT jsonb_agg(
-                        jsonb_set(
-                            widget,
-                            '{widgetConfig,allOfType}',
-                            CASE
-                                WHEN (widget ->> 'widgetTypeId') = 'map' THEN 'true'::jsonb
-                                WHEN (widget ->> 'widgetTypeId') = 'table' THEN 'false'::jsonb
-                                ELSE widget -> 'widgetConfig' -> 'allOfType'
-                            END
-                        )
-                    )
-                    FROM jsonb_array_elements(template -> 'widgets') AS widget
-                )
-            ),
-            template
-        ) AS updated_json
-    FROM dashboard
-)
 UPDATE dashboard
-SET template = json_data.updated_json
-FROM json_data
-WHERE dashboard.id = json_data.id;
+SET template = jsonb_set(
+        template,
+        '{widgets}',
+        (
+            SELECT jsonb_agg(
+                           CASE
+                               WHEN widget->>'widgetTypeId' = 'map' THEN
+                                   jsonb_set(widget, '{widgetConfig,allOfType}', 'true'::jsonb)
+                               WHEN widget->>'widgetTypeId' = 'table' THEN
+                                   jsonb_set(widget, '{widgetConfig,allOfType}', 'false'::jsonb)
+                               ELSE
+                                   widget  -- Leave non-map/table widgets unchanged
+                               END
+                   )
+            FROM jsonb_array_elements(template->'widgets') widget
+        )
+)
+WHERE template->'widgets' IS NOT NULL;
