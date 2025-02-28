c3df61b73dcdf304f7734626ee188fcc4f27b7b8
Rich Turner <7072278+richturner@users.noreply.github.com>
2025-02-28 11:49:25 +0000

    MQTT Agent support for specifying QoS level

--- agent/src/main/java/org/openremote/agent/protocol/mqtt/MQTTAgent.java
+++ agent/src/main/java/org/openremote/agent/protocol/mqtt/MQTTAgent.java
@@ -23,8 +23,10 @@ import jakarta.persistence.Entity;
 import org.openremote.agent.protocol.io.IOAgent;
 import org.openremote.model.asset.agent.Agent;
 import org.openremote.model.asset.agent.AgentDescriptor;
+import org.openremote.model.http.HTTPMethod;
 import org.openremote.model.value.AttributeDescriptor;
 import org.openremote.model.value.ValueConstraint;
+import org.openremote.model.value.ValueDescriptor;
 import org.openremote.model.value.ValueType;
 
 import java.util.Optional;
@@ -32,12 +34,16 @@ import java.util.Optional;
 @Entity
 public class MQTTAgent extends IOAgent<MQTTAgent, MQTTProtocol, MQTTAgentLink> {
 
+    public static final ValueDescriptor<Integer> VALUE_MQTT_QOS = new ValueDescriptor<>("MQTTQos", Integer.class,
+        new ValueConstraint.AllowedValues(0,1,2)
+    );
+
     public static final AttributeDescriptor<String> HOST = Agent.HOST.withOptional(false);
     public static final AttributeDescriptor<Integer> PORT = Agent.PORT.withOptional(false);
     public static final AttributeDescriptor<String> CLIENT_ID = new AttributeDescriptor<>("clientId", ValueType.TEXT);
     public static final AttributeDescriptor<Boolean> SECURE_MODE = new AttributeDescriptor<>("secureMode", ValueType.BOOLEAN);
-    public static final AttributeDescriptor<Integer> PUBLISH_QOS = new AttributeDescriptor<>("publishQoS", ValueType.POSITIVE_INTEGER.withConstraints(new ValueConstraint.Max(2)));
-    public static final AttributeDescriptor<Integer> SUBSCRIBE_QOS = new AttributeDescriptor<>("subscribeQoS", ValueType.POSITIVE_INTEGER.withConstraints(new ValueConstraint.Max(2)));
+    public static final AttributeDescriptor<Integer> PUBLISH_QOS = new AttributeDescriptor<>("publishQos", VALUE_MQTT_QOS);
+    public static final AttributeDescriptor<Integer> SUBSCRIBE_QOS = new AttributeDescriptor<>("subscribeQos", VALUE_MQTT_QOS);
     public static final AttributeDescriptor<String> CLIENT_CERTIFICATE_ALIAS = new AttributeDescriptor<>("certificateAlias", ValueType.TEXT);
     public static final AttributeDescriptor<Boolean> RESUME_SESSION = new AttributeDescriptor<>("resumeSession", ValueType.BOOLEAN);
     public static final AttributeDescriptor<Boolean> WEBSOCKET_MODE = new AttributeDescriptor<>("websocketMode", ValueType.BOOLEAN);
