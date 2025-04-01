557faf8f909175087f192d93c5b88dd0a5d7bfdb
denniskuijs <Dennis@denniskuijs.nl>
2025-04-01 09:22:13 +0200

    improved order of provisioning

--- .ci_cd/aws/provision_host.sh
+++ .ci_cd/aws/provision_host.sh
@@ -74,67 +74,6 @@ SMTP_STACK_NAME="$STACK_NAME-smtp"
 HEALTH_STACK_NAME="$STACK_NAME-healthcheck"
 EBS_STACK_NAME="$STACK_NAME-ebs-data-volume"
 
-SUBNET_NUMBER=$(( $RANDOM % 3 + 1 ))
-SUBNETNAME="or-subnet-public-$SUBNET_NUMBER"
-SUBNET_AZ=$(aws ec2 describe-subnets --filters Name=tag:Name,Values=$SUBNETNAME --query "Subnets[0].AvailabilityZone" --output text $ACCOUNT_PROFILE 2>/dev/null)
-
-# Provision EBS volume using CloudFormation (if stack doesn't already exist).
-echo "Provisioning EBS data volume"
-STATUS=$(aws cloudformation describe-stacks --stack-name $EBS_STACK_NAME --query "Stacks[0].StackStatus" --output text 2>/dev/null)
-
-if [ -n "$STATUS" ] && [ "$STATUS" != 'DELETE_COMPLETE' ]; then
-    echo "Stack already exists for this host '$HOST' current status is '$STATUS'"
-    STACK_ID=$(aws cloudformation describe-stacks --stack-name $EBS_STACK_NAME --query Stacks[0].StackId --output text 2>/dev/null)
-else
-
-    if [ -f "${awsDir}cloudformation-create-ebs-volume.yml" ]; then
-    EBS_TEMPLATE_PATH="${awsDir}cloudformation-create-ebs-volume.yml"
-    elif [ -f ".ci_cd/aws/cloudformation-create-ebs-volume.yml" ]; then
-    EBS_TEMPLATE_PATH=".ci_cd/aws/cloudformation-create-ebs-volume.yml"
-    elif [ -f "openremote/.ci_cd/aws/cloudformation-create-ebs-volume.yml" ]; then
-    EBS_TEMPLATE_PATH="openremote/.ci_cd/aws/cloudformation-create-ebs-volume.yml"
-    else
-        echo "Cannot determine location of cloudformation-create-ebs-volume.yml"
-        exit 1
-    fi
-
-    # Configure parameters.
-    PARAMS="ParameterKey=Host,ParameterValue=$HOST"
-    PARAMS="$PARAMS ParameterKey=AvailabilityZone,ParameterValue=$SUBNET_AZ"
-    PARAMS="$PARAMS ParameterKey=DiskSize,ParameterValue=$DATA_DISK_SIZE"
-
-    if [ -n "$SNAPSHOT_ID" ]; then
-        PARAMS="$PARAMS ParameterKey=SnapshotId,ParameterValue=$SNAPSHOT_ID"
-    fi
-
-    # Create standard stack resources in specified account.
-    STACK_ID=$(aws cloudformation create-stack --capabilities CAPABILITY_NAMED_IAM --stack-name $EBS_STACK_NAME --template-body file://$EBS_TEMPLATE_PATH --parameters $PARAMS --output text)
-
-    if [ $? -ne 0 ]; then
-      echo "Create stack failed"
-      exit 1
-    else
-      echo "Create stack in progress"
-    fi
-
-    # Wait for CloudFormation stack status to be CREATE_*.
-    echo "Waiting for stack to be created"
-    STATUS=$(aws cloudformation describe-stacks --stack-name $EBS_STACK_NAME --query "Stacks[?StackId=='$STACK_ID'].StackStatus" --output text 2>/dev/null)
-
-    while [[ "$STATUS" == 'CREATE_IN_PROGRESS' ]]; do
-        echo "Stack creation is still in progress .. Sleeping 30 seconds"
-        sleep 30
-        STATUS=$(aws cloudformation describe-stacks --stack-name $EBS_STACK_NAME --query "Stacks[?StackId=='$STACK_ID'].StackStatus" --output text 2>/dev/null)
-    done
-
-    if [ "$STATUS" != 'CREATE_COMPLETE' ]; then
-        echo "Stack creation has failed status is '$STATUS'"
-        exit 1
-    else
-        echo "Stack creation is complete"
-    fi
-  fi
-
 # Provision SMTP user using CloudFormation (if stack doesn't already exist).
 echo "Provisioning SMTP user"
 STATUS=$(aws cloudformation describe-stacks --stack-name $SMTP_STACK_NAME --query "Stacks[0].StackStatus" --output text 2>/dev/null)
@@ -193,38 +132,11 @@ if [ -n "$STATUS" ] && [ "$STATUS" != 'DELETE_COMPLETE' ]; then
   echo "Stack already exists for this host '$HOST' current status is '$STATUS'"
   STACK_ID=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].StackId" --output text $ACCOUNT_PROFILE 2>/dev/null)
 else
-  # Configure parameters.
-  PARAMS="ParameterKey=Host,ParameterValue=$HOST"
-
-  if [ -n "$INSTANCE_TYPE" ]; then
-    PARAMS="$PARAMS ParameterKey=InstanceType,ParameterValue=$INSTANCE_TYPE"
-  fi
-
-  if [ -n "$DISK_SIZE" ]; then
-    PARAMS="$PARAMS ParameterKey=DiskSize,ParameterValue=$DISK_SIZE"
-  fi
-
-  if [ -n "$ELASTIC_IP" ]; then
-    PARAMS="$PARAMS ParameterKey=ElasticIP,ParameterValue=$ELASTIC_IP"
-  fi
-
-  if [ -n "$ENABLE_METRICS" ]; then
-    PARAMS="$PARAMS ParameterKey=Metrics,ParameterValue=$ENABLE_METRICS"
-  fi
 
   # Get SMTP credentials.
   SMTP_HOST="email-smtp.$AWS_REGION.amazonaws.com"
   SMTP_USER=$(aws cloudformation describe-stacks --stack-name $SMTP_STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='SMTPUserKey'].OutputValue" --output text 2>/dev/null)
   SMTP_SECRET=$(aws cloudformation describe-stacks --stack-name $SMTP_STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='SMTPUserSecret'].OutputValue" --output text 2>/dev/null)
-
-  PARAMS="$PARAMS ParameterKey=SMTPHost,ParameterValue=$SMTP_HOST"
-
-  if [ -n "$SMTP_USER" ]; then
-    PARAMS="$PARAMS ParameterKey=SMTPUser,ParameterValue=$SMTP_USER"
-  fi
-  if [ -n "$SMTP_SECRET" ]; then
-    PARAMS="$PARAMS ParameterKey=SMTPSecret,ParameterValue=$SMTP_SECRET ParameterKey=SMTPRegion,ParameterValue=$AWS_REGION"
-  fi
 
   # Determine DNSHostedZoneName and DNSHostedZoneRoleArn (must be set if hosted zone is not in the same account as where the host is being created).
   echo "Determining DNS parameters"
@@ -278,14 +190,9 @@ EOF
     done
   fi
 
-  if [ -n "$DNSHostedZoneName" ]; then
-    PARAMS="$PARAMS ParameterKey=DNSHostedZoneName,ParameterValue=$DNSHostedZoneName"
-  fi
-  if [ -n "$DNSHostedZoneRoleArn" ]; then
-    PARAMS="$PARAMS ParameterKey=DNSHostedZoneRoleArn,ParameterValue=$DNSHostedZoneRoleArn"
-  fi
-
   # Get OR VPC ID, Subnet ID, SSH Security Group ID and EFS MOUNT TARGET IP.
+  SUBNET_NUMBER=$(( $RANDOM % 3 + 1 ))
+  SUBNETNAME="or-subnet-public-$SUBNET_NUMBER"
   VPCID=$(aws ec2 describe-vpcs --filters Name=tag:Name,Values=or-vpc --query "Vpcs[0].VpcId" --output text $ACCOUNT_PROFILE 2>/dev/null)
   SUBNETID=$(aws ec2 describe-subnets --filters Name=tag:Name,Values=$SUBNETNAME --query "Subnets[0].SubnetId" --output text $ACCOUNT_PROFILE 2>/dev/null)
   SUBNET_AZ=$(aws ec2 describe-subnets --filters Name=tag:Name,Values=$SUBNETNAME --query "Subnets[0].AvailabilityZoneId" --output text $ACCOUNT_PROFILE 2>/dev/null)
@@ -295,13 +202,106 @@ EOF
   EFS_ID=$(aws efs describe-file-systems --query "FileSystems[?Name=='or-map-efs'].FileSystemId" --output text)
   EFS_DNS=$(aws efs describe-mount-targets --file-system-id $EFS_ID --query "MountTargets[?AvailabilityZoneId=='$SUBNET_AZ'].IpAddress" --output text)
 
+  SUBNET_AZ=$(aws ec2 describe-subnets --filters Name=tag:Name,Values=$SUBNETNAME --query "Subnets[0].AvailabilityZone" --output text $ACCOUNT_PROFILE 2>/dev/null)
+
+  # Provision EBS volume using CloudFormation (if stack doesn't already exist).
+  echo "Provisioning EBS data volume"
+  STATUS=$(aws cloudformation describe-stacks --stack-name $EBS_STACK_NAME --query "Stacks[0].StackStatus" --output text 2>/dev/null)
+
+  if [ -n "$STATUS" ] && [ "$STATUS" != 'DELETE_COMPLETE' ]; then
+      echo "Stack already exists for this host '$HOST' current status is '$STATUS'"
+      STACK_ID=$(aws cloudformation describe-stacks --stack-name $EBS_STACK_NAME --query Stacks[0].StackId --output text 2>/dev/null)
+  else
+
+    if [ -f "${awsDir}cloudformation-create-ebs-volume.yml" ]; then
+    EBS_TEMPLATE_PATH="${awsDir}cloudformation-create-ebs-volume.yml"
+    elif [ -f ".ci_cd/aws/cloudformation-create-ebs-volume.yml" ]; then
+    EBS_TEMPLATE_PATH=".ci_cd/aws/cloudformation-create-ebs-volume.yml"
+    elif [ -f "openremote/.ci_cd/aws/cloudformation-create-ebs-volume.yml" ]; then
+    EBS_TEMPLATE_PATH="openremote/.ci_cd/aws/cloudformation-create-ebs-volume.yml"
+    else
+        echo "Cannot determine location of cloudformation-create-ebs-volume.yml"
+        exit 1
+    fi
+
+    # Configure parameters.
+    PARAMS="ParameterKey=Host,ParameterValue=$HOST"
+    PARAMS="$PARAMS ParameterKey=AvailabilityZone,ParameterValue=$SUBNET_AZ"
+    PARAMS="$PARAMS ParameterKey=DiskSize,ParameterValue=$DATA_DISK_SIZE"
+
+    if [ -n "$SNAPSHOT_ID" ]; then
+        PARAMS="$PARAMS ParameterKey=SnapshotId,ParameterValue=$SNAPSHOT_ID"
+    fi
+
+    # Create standard stack resources in specified account.
+    STACK_ID=$(aws cloudformation create-stack --capabilities CAPABILITY_NAMED_IAM --stack-name $EBS_STACK_NAME --template-body file://$EBS_TEMPLATE_PATH --parameters $PARAMS --output text)
+
+    if [ $? -ne 0 ]; then
+      echo "Create stack failed"
+      exit 1
+    else
+      echo "Create stack in progress"
+    fi
+
+    # Wait for CloudFormation stack status to be CREATE_*.
+    echo "Waiting for stack to be created"
+    STATUS=$(aws cloudformation describe-stacks --stack-name $EBS_STACK_NAME --query "Stacks[?StackId=='$STACK_ID'].StackStatus" --output text 2>/dev/null)
+
+    while [[ "$STATUS" == 'CREATE_IN_PROGRESS' ]]; do
+        echo "Stack creation is still in progress .. Sleeping 30 seconds"
+        sleep 30
+        STATUS=$(aws cloudformation describe-stacks --stack-name $EBS_STACK_NAME --query "Stacks[?StackId=='$STACK_ID'].StackStatus" --output text 2>/dev/null)
+    done
+
+    if [ "$STATUS" != 'CREATE_COMPLETE' ]; then
+        echo "Stack creation has failed status is '$STATUS'"
+        exit 1
+    else
+        echo "Stack creation is complete"
+    fi
+  fi
+
   DEVICE_NAME="/dev/sdf" # Only change if you know what you are doing.
 
+  PARAMS="ParameterKey=Host,ParameterValue=$HOST"
   PARAMS="$PARAMS ParameterKey=VpcId,ParameterValue=$VPCID"
   PARAMS="$PARAMS ParameterKey=SSHSecurityGroupId,ParameterValue=$SGID"
   PARAMS="$PARAMS ParameterKey=SubnetId,ParameterValue=$SUBNETID"
   PARAMS="$PARAMS ParameterKey=EFSDNS,ParameterValue=$EFS_DNS"
   PARAMS="$PARAMS ParameterKey=EBSDeviceName,ParameterValue=$DEVICE_NAME"
+  PARAMS="$PARAMS ParameterKey=SMTPHost,ParameterValue=$SMTP_HOST"
+
+  if [ -n "$INSTANCE_TYPE" ]; then
+    PARAMS="$PARAMS ParameterKey=InstanceType,ParameterValue=$INSTANCE_TYPE"
+  fi
+
+  if [ -n "$DISK_SIZE" ]; then
+    PARAMS="$PARAMS ParameterKey=DiskSize,ParameterValue=$DISK_SIZE"
+  fi
+
+  if [ -n "$ELASTIC_IP" ]; then
+    PARAMS="$PARAMS ParameterKey=ElasticIP,ParameterValue=$ELASTIC_IP"
+  fi
+
+  if [ -n "$ENABLE_METRICS" ]; then
+    PARAMS="$PARAMS ParameterKey=Metrics,ParameterValue=$ENABLE_METRICS"
+  fi
+
+  if [ -n "$SMTP_USER" ]; then
+    PARAMS="$PARAMS ParameterKey=SMTPUser,ParameterValue=$SMTP_USER"
+  fi
+
+  if [ -n "$SMTP_SECRET" ]; then
+    PARAMS="$PARAMS ParameterKey=SMTPSecret,ParameterValue=$SMTP_SECRET ParameterKey=SMTPRegion,ParameterValue=$AWS_REGION"
+  fi
+
+  if [ -n "$DNSHostedZoneName" ]; then
+    PARAMS="$PARAMS ParameterKey=DNSHostedZoneName,ParameterValue=$DNSHostedZoneName"
+  fi
+
+  if [ -n "$DNSHostedZoneRoleArn" ]; then
+    PARAMS="$PARAMS ParameterKey=DNSHostedZoneRoleArn,ParameterValue=$DNSHostedZoneRoleArn"
+  fi
 
   # Create standard stack resources in specified account.
   STACK_ID=$(aws cloudformation create-stack --capabilities CAPABILITY_NAMED_IAM --stack-name $STACK_NAME --template-body file://$TEMPLATE_PATH --parameters $PARAMS --output text $ACCOUNT_PROFILE)
