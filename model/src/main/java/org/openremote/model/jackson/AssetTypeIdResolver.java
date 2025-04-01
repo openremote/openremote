ed68b4f9f4fc0f69783ca790d521d33979c8a360
Rich Turner <7072278+richturner@users.noreply.github.com>
2025-04-01 14:51:23 +0100

    Update AssetTypeIdResolver.java

--- model/src/main/java/org/openremote/model/jackson/AssetTypeIdResolver.java
+++ model/src/main/java/org/openremote/model/jackson/AssetTypeIdResolver.java
@@ -26,7 +26,6 @@ import com.fasterxml.jackson.databind.jsontype.impl.TypeIdResolverBase;
 import org.openremote.model.asset.Asset;
 import org.openremote.model.asset.AssetDescriptor;
 import org.openremote.model.asset.impl.ThingAsset;
-import org.openremote.model.asset.impl.UnknownAsset;
 import org.openremote.model.util.ValueUtil;
 
 import java.io.IOException;
