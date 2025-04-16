9538df6dfe535ee5453ee032f089e4fe5126c104
Rich Turner <7072278+richturner@users.noreply.github.com>
2025-04-16 22:02:08 +0100

    WIP

--- model/src/main/java/org/openremote/model/util/LockByKey.java
+++ model/src/main/java/org/openremote/model/util/LockByKey.java
@@ -1,21 +1,21 @@
 /*
  * Copied from: https://github.com/eugenp/tutorials/blob/master/core-java-modules/core-java-concurrency-advanced-4/src/main/java/com/baeldung/lockbykey/LockByKey.java
- * Altered to use a Semaphore instead of ReentrantLock to avoid issues with camel thread context switching between lock/unlock calls (see issue #1812)
  */
 
 package org.openremote.model.util;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class LockByKey {
 
     private static final System.Logger LOG = System.getLogger(LockByKey.class.getName());
 
     private static class LockWrapper {
-        private final Semaphore lock = new Semaphore(1); // Don't change to ReentrantLock as camel switches thread between try/finally blocks on route processors
+        private final Lock lock = new ReentrantLock();
         private final AtomicInteger numberOfThreadsInQueue = new AtomicInteger(1);
 
         private LockWrapper addThreadInQueue() {
@@ -29,28 +29,21 @@ public class LockByKey {
 
     }
 
-    private final Map<String, LockWrapper> locks = new ConcurrentHashMap<>();
+    private Map<String, LockWrapper> locks = new ConcurrentHashMap<>();
 
     public void lock(String key) {
         LockWrapper lockWrapper = locks.compute(key, (k, v) -> v == null ? new LockWrapper() : v.addThreadInQueue());
-        try {
-            lockWrapper.lock.acquire();
-            LOG.log(System.Logger.Level.TRACE, () -> "Lock acquired: key=" + key);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt(); // Restore interrupted status
-            throw new RuntimeException("Interrupted while acquiring lock for key: " + key, e);
-        }
+        lockWrapper.lock.lock();
+        LOG.log(System.Logger.Level.TRACE, () -> "Lock acquired: key=" + key + ", threadName=" + Thread.currentThread().getName());
     }
 
     public void unlock(String key) {
+        LOG.log(System.Logger.Level.TRACE, () -> "Lock release: key=" + key + ", threadName=" + Thread.currentThread().getName());
         LockWrapper lockWrapper = locks.get(key);
-        if (lockWrapper != null) {
-            LOG.log(System.Logger.Level.TRACE, () -> "Lock release: key=" + key);
-            lockWrapper.lock.release();
-            if (lockWrapper.removeThreadFromQueue() == 0) {
-                // NB : We pass in the specific value to remove to handle the case where another thread would queue right before the removal
-                locks.remove(key, lockWrapper);
-            }
+        lockWrapper.lock.unlock();
+        if (lockWrapper.removeThreadFromQueue() == 0) {
+            // NB : We pass in the specific value to remove to handle the case where another thread would queue right before the removal
+            locks.remove(key, lockWrapper);
         }
     }
 
