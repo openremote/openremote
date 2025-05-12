95ef7e6276a9b8434ac77b8e3936e515676f973f
Rich Turner <7072278+richturner@users.noreply.github.com>
2025-05-12 17:07:38 +0100

    Update HibernateUniqueIdentifierGeneratorAssignable.java

--- model/src/main/java/org/openremote/model/util/HibernateUniqueIdentifierGeneratorAssignable.java
+++ model/src/main/java/org/openremote/model/util/HibernateUniqueIdentifierGeneratorAssignable.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024, OpenRemote Inc.
+ * Copyright 2025, OpenRemote Inc.
  *
  * See the CONTRIBUTORS.txt file in the distribution for a
  * full listing of individual contributors.
