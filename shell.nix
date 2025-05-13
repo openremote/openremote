111d8f9f6efeb021eb7896be1173f909b0c17e40
Ekhorn <koen@kschellingerhout.nl>
2025-05-13 09:48:53 +0200

    Fix navigation interrupted after logout

--- shell.nix
+++ shell.nix
@@ -11,6 +11,7 @@ pkgs.mkShell {
   shellHook = ''
     export PLAYWRIGHT_BROWSERS_PATH=${unstable.playwright-driver.browsers}
     export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
+    export PLAYWRIGHT_HOST_PLATFORM_OVERRIDE="ubuntu-24.04"
     export WEBSERVER_LISTEN_HOST=0.0.0.0
     export IDENTITY_NETWORK_WEBSERVER_PORT=443
     export IDENTITY_NETWORK_SECURE=true
