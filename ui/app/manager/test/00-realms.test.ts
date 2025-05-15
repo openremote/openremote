2a1ac558302018e6f9092355645d6f6555ab952a
Ekhorn <koen@kschellingerhout.nl>
2025-05-15 16:33:25 +0200

    refactor(e2e): move delete tests to page tests

--- ui/app/manager/test/00-realms.test.ts
+++ ui/app/manager/test/00-realms.test.ts
@@ -1,13 +1,10 @@
 import { expect } from "@playwright/test";
 import { test } from "./fixtures/manager.js";
 
-test.beforeEach(async ({ manager }) => {
-  // When Login to OpenRemote "master" realm as "admin"
+test("Add new Realm", async ({ page, manager, realmsPage }) => {
+  // Given Login to OpenRemote "master" realm as "admin"
   await manager.goToRealmStartPage("master");
   await manager.login("admin");
-});
-
-test("Add new Realm", async ({ page, realmsPage }) => {
   // When Navigate to "Realms" page
   await realmsPage.goto();
   // Then Add a new Realm
@@ -17,6 +14,23 @@ test("Add new Realm", async ({ page, realmsPage }) => {
   await page.click('li[role="menuitem"]:has-text("smartcity")');
   // Then We see the smartcity realm
   await expect(page.locator("#desktop-right #realm-picker")).toContainText("smartcity");
+});
+
+test("Delete realm", async ({ page, manager, realmsPage }) => {
+  // Given the Realm "smartcity" is setup
+  await manager.setup("smartcity");
+  // When Login to OpenRemote "master" realm as "admin"
+  await manager.goToRealmStartPage("master");
+  await manager.login("admin");
+  // When Navigate to "Realms" page
+  await manager.navigateToMenuItem("Realms");
+  // Then Delete realm
+  await realmsPage.deleteRealm("smartcity");
+  await page.waitForTimeout(500);
+  // Then We should not see the Realm picker
+  await manager.goToRealmStartPage("master");
+  // must wait for the realm picker to be rendered
+  await expect(page.locator("#desktop-right #realm-picker")).not.toBeVisible();
 });
 
 test.afterEach(async ({ manager }) => {
---
+++ ui/app/manager/test/02-roles.test.ts
@@ -0,0 +1,30 @@
+import { expect } from "@playwright/test";
+import { test } from "./fixtures/manager.js";
+import { custom } from "./fixtures/data/roles.js";
+
+test.beforeEach(async ({ manager }) => {
+  // Given the Realm "smartcity" with the user "smartcity" and assets is setup
+  await manager.setup("smartcity", { role: custom });
+  // When Login to OpenRemote "master" realm as "admin"
+  await manager.goToRealmStartPage("master");
+  await manager.login("admin");
+});
+
+test("Delete role", async ({ page, manager }) => {
+  // Then Switch to "smartcity" realm
+  await manager.switchToRealmByRealmPicker("smartcity");
+  // Then Navigate to "Roles" page
+  await manager.navigateToMenuItem("Roles");
+  // Then Delete role
+  await page.click("text=Custom");
+  await page.waitForTimeout(100);
+  await page.click('tr[class="attribute-meta-row expanded"] >> button:has-text("delete")');
+  await page.click('div[role="alertdialog"] button:has-text("Delete")');
+  await page.waitForTimeout(100);
+  // Then We should not see the Custom role
+  await expect(page.locator("text=Custom")).toHaveCount(0);
+});
+
+test.afterEach(async ({ manager }) => {
+  await manager.cleanUp();
+});
