57b7ef16c68bf758722a00646b16d3b097f0582f
Ekhorn <koen@kschellingerhout.nl>
2025-05-16 08:47:49 +0200

    test(e2e): add create role test

--- ui/app/manager/test/02-roles.test.ts
+++ ui/app/manager/test/02-roles.test.ts
@@ -2,15 +2,43 @@ import { expect } from "@playwright/test";
 import { test } from "./fixtures/manager.js";
 import { custom } from "./fixtures/data/roles.js";
 
-test.beforeEach(async ({ manager }) => {
+test("Create role", async ({ page, manager }) => {
+  // Given the Realm "smartcity" is setup
+  await manager.setup("smartcity", { role: custom });
+  // When Login to OpenRemote "master" realm as "admin"
+  await manager.goToRealmStartPage("master");
+  await manager.login("admin");
+  // When Navigate to "Roles" page
+  await manager.navigateToMenuItem("Roles");
+  // Then Create a new role
+  await page.getByText("Add Role").click();
+
+  // get total number of current roles
+  let rows = await page.$$(".mdc-data-table__row");
+  const count = await rows.length;
+
+  await page.fill(`#attribute-meta-row-${count - 1} input[type="text"] >> nth=0`, "Custom");
+  await page.fill(`#attribute-meta-row-${count - 1} input[type="text"] >> nth=1`, "read:asset, write:asset");
+  await page
+    .locator(`#attribute-meta-row-${count - 1}`)
+    .getByText("assets: Read asset data")
+    .click();
+  await page
+    .locator(`#attribute-meta-row-${count - 1}`)
+    .getByText("assets: Write asset data")
+    .click();
+
+  await page.click('button:has-text("create")');
+  // Then We see a new role
+  await expect(page.locator("text=Custom")).toHaveCount(1);
+});
+
+test("Delete role", async ({ page, manager }) => {
   // Given the Realm "smartcity" with the user "smartcity" and assets is setup
   await manager.setup("smartcity", { role: custom });
   // When Login to OpenRemote "master" realm as "admin"
   await manager.goToRealmStartPage("master");
   await manager.login("admin");
-});
-
-test("Delete role", async ({ page, manager }) => {
   // Then Switch to "smartcity" realm
   await manager.switchToRealmByRealmPicker("smartcity");
   // Then Navigate to "Roles" page
