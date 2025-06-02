79cdeb8add91986e7fe2fa829acbfa4e6b1e1f7c
Iskren Alexandrov <iskren.alexandrov@gmail.com>
2025-06-02 09:07:22 +0200

    remove the setup script from the gitignore

--- main.tf
+++ main.tf
@@ -34,39 +34,7 @@ resource "hcloud_server" "openremote" {
   image       = "ubuntu-22.04" # OS image
   location    = "nbg1"   # Data center (e.g., nbg1, fsn1, hel1)
   ssh_keys    = []
-  user_data   = <<-EOF
-    #!/bin/bash
-
-    apt-get update
-    apt-get install -y \
-        apt-transport-https \
-        ca-certificates \
-        curl \
-        gnupg \
-        lsb-release \
-        git
-
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-    apt-get update
-    apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-
-    systemctl enable docker
-    systemctl start docker
-
-    usermod -aG docker root
-
-    mkdir -p /opt/openremote
-    cd /opt/openremote
-
-    curl -o docker-compose.yml https://raw.githubusercontent.com/openremote/openremote/master/docker-compose.yml
-
-    docker compose pull
-
-    OR_HOSTNAME=$(hostname -I | awk '{print $1}') docker compose -p openremote up -d 
-  EOF
+  user_data   = file("setup.sh")
 
   lifecycle {
     create_before_destroy = true
