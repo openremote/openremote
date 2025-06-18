137100b25e999be0c8273207b1296283d44a4bac
MartinaeyNL <business@martinaey.nl>
2025-06-18 12:10:41 +0200

    Fix for SQL statement

--- manager/src/main/resources/org/openremote/manager/setup/database/V20250617_01__GatewayWidgetRefs.sql
+++ manager/src/main/resources/org/openremote/manager/setup/database/V20250617_01__GatewayWidgetRefs.sql
@@ -4,17 +4,21 @@ SET template = jsonb_set(
         '{widgets}',
         (SELECT jsonb_agg(
                         CASE
-                            WHEN widget ->> 'widgetTypeId' = 'gateway' AND
-                                 widget -> 'widgetConfig' ->> 'gatewayId' <> '' THEN
+                            WHEN widget ->> 'widgetTypeId' = 'gateway' THEN
                                 jsonb_set(
                                         widget,
                                         '{widgetConfig,attributeRefs}',
-                                        jsonb_build_array(
-                                                jsonb_build_object(
-                                                        'id', widget -> 'widgetConfig' ->> 'gatewayId',
-                                                        'name', NULL
+                                        CASE
+                                            WHEN widget -> 'widgetConfig' ->> 'gatewayId' <> '' THEN
+                                                jsonb_build_array(
+                                                        jsonb_build_object(
+                                                                'id', widget -> 'widgetConfig' ->> 'gatewayId',
+                                                                'name', NULL
+                                                        )
                                                 )
-                                        )
+                                            ELSE
+                                                '[]'::jsonb -- Set to an empty array if gatewayId is empty
+                                            END
                                 )
                             ELSE
                                 widget -- Leave non-gateway widgets unchanged
