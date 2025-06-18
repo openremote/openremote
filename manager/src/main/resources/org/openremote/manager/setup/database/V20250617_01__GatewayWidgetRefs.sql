47fa07021c956e94894d2ff188386b8a86445538
MartinaeyNL <business@martinaey.nl>
2025-06-18 11:19:53 +0200

    SQL migration script fix

--- manager/src/main/resources/org/openremote/manager/setup/database/V20250617_01__GatewayWidgetRefs.sql
+++ manager/src/main/resources/org/openremote/manager/setup/database/V20250617_01__GatewayWidgetRefs.sql
@@ -2,16 +2,19 @@ UPDATE dashboard
 SET template = jsonb_set(
         template,
         '{widgets}',
-        (
-            SELECT jsonb_agg(
-                           CASE
-                               WHEN widget->>'widgetTypeId' = 'gateway' THEN
-                                   jsonb_set(widget, '{widgetConfig,attributeRefs}', ('[' || widget->'widgetConfig'->>'gatewayId' || ']')::jsonb)
-                               ELSE
-                                   widget  -- Leave non-gateway widgets unchanged
-                               END
-                   )
-            FROM jsonb_array_elements(template->'widgets') widget
-        )
-)
-WHERE template->'widgets' IS NOT NULL;
+        (SELECT jsonb_agg(
+                        CASE
+                            WHEN widget ->> 'widgetTypeId' = 'gateway' THEN
+                                jsonb_set(widget, '{widgetConfig,attributeRefs}',
+                                          COALESCE(
+                                                  ('[' || widget -> 'widgetConfig' ->> 'gatewayId' || ']')::jsonb,
+                                                  '[]'::jsonb
+                                          )
+                                )
+                            ELSE
+                                widget -- Leave non-gateway widgets unchanged
+                            END
+                )
+         FROM jsonb_array_elements(template -> 'widgets') widget)
+               )
+WHERE template -> 'widgets' IS NOT NULL;
