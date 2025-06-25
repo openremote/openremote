506e187e7f781ed5b080eb6845747bee64afe76b
Ekhorn <koen@kschellingerhout.nl>
2025-06-25 16:31:08 +0200

    Use fallback keys feature

--- ui/component/or-json-forms/src/index.ts
+++ ui/component/or-json-forms/src/index.ts
@@ -169,7 +169,7 @@ export class OrJSONForms extends translate(i18next)(LitElement) implements OwnPr
                         const path = ["schema.item", camelCase(this.schema?.title), id].join(".")
                         const defPath = ["schema.item", camelCase(this.schema?.title), "definitions", id].join(".")
                         // console.log(path, id, defaultMessage, values, i18next.t(path, { defaultValue: defaultMessage }))
-                        return i18next.t(path, { defaultValue: i18next.t(defPath, { defaultValue: defaultMessage }) }) || defaultMessage!;
+                        return i18next.t([path, defPath], { defaultValue: defaultMessage }) || defaultMessage!;
                     },
                     translateError: (error, translate, uischema) => {
                         console.log(`Locale: ${this.contextValue?.i18n?.locale}, Error: ${error}, UI Schema: ${uischema}`);
