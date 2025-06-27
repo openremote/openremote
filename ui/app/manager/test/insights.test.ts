5ec21079476ed4dc4dfbfbaaefd36731367f8740
Ekhorn <koen@kschellingerhout.nl>
2025-06-27 15:35:36 +0200

    Remove unused insights test file

--- ui/app/manager/test/insights.test.ts
+++
@@ -1,8 +0,0 @@
-// empty for now
-// wait for future development
-
-import { test, userStatePath } from "./fixtures/manager.js";
-
-test.use({ storageState: userStatePath });
-
-test.fixme("Create a dashboard", async ({}) => {});
