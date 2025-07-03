174f93f10fc5f8aad78b67325b57504b31aae1e3
MartinaeyNL <business@martinaey.nl>
2025-07-03 16:50:16 +0200

    Hotfix for service user creation not being allowed.

--- ui/app/manager/src/pages/page-users.ts
+++ ui/app/manager/src/pages/page-users.ts
@@ -338,7 +338,7 @@ export class PageUsers extends Page<AppStateKeyed> {
             return false;
         }
         // New service users with the 'gateway-' prefix are not allowed
-        if(user.serviceAccount && (user.email.startsWith('gateway-') || user.username.startsWith('gateway-'))) {
+        if(user.serviceAccount && (user.email?.startsWith('gateway-') || user.username?.startsWith('gateway-'))) {
             showSnackbar(undefined, "noGatewayUsername", "dismiss")
             return false;
         }
@@ -1059,6 +1059,7 @@ export class PageUsers extends Page<AppStateKeyed> {
                                                   this.reset();
                                               }
                                           }).catch((ex) => {
+                                              console.error(ex);
                                               if (isAxiosError(ex)) {
                                                   error = {
                                                       status: ex.response.status,
