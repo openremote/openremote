0187cf2aca79987022304c3a34840abc47cbb9f7
Eric Bariaux <375613+ebariaux@users.noreply.github.com>
2025-07-15 11:24:09 +0200

    Configuration, script and documentation to deploy under AWS EKS (#1968)

---
+++ kubernetes/eks-common.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -eo pipefail
+
+# Name of cluster, not exposed but must be unique within account
+# !!! make sure to also update in cluster.yaml
+CLUSTER_NAME=testcluster
+
+# Hostname to use for public access to this instance, always under the openremote.app domain
+HOSTNAME=testmanager
+FQDN=$HOSTNAME.openremote.app
+MQTT_FQDN=mqtt.$FQDN
+MQTTS_FQDN=mqtts.$FQDN
+
+# !!! make sure region in cluster.yaml matches
+AWS_REGION="eu-west-1"
+AWS_ACCOUNT_ID="463235666115" # openremote
+
+DNSCHG_ROLE_ARN="arn:aws:iam::134517981306:role/route53-full-access"
+
+aws configure set region $AWS_REGION
+aws configure --profile or set region $AWS_REGION
+aws configure --profile or set output json
+aws configure --profile dnschg set source_profile or
+aws configure --profile dnschg set role_arn $DNSCHG_ROLE_ARN
+
+if [ -n "$AWS_ACCESS_KEY_ID" ]; then
+  aws configure --profile or set aws_access_key_id $AWS_ACCESS_KEY_ID
+fi
+
+if [ -n "$AWS_SECRET_ACCESS_KEY" ]; then
+  aws configure --profile or set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+fi
+
+if [ -n "$AWS_SESSION_TOKEN" ]; then
+  aws configure --profile or set aws_session_token $AWS_SESSION_TOKEN
+fi
+
+aws sts assume-role --role-arn $DNSCHG_ROLE_ARN --role-session-name dnschg --profile or --query "Credentials.[AccessKeyId, SecretAccessKey, SessionToken]" --output text | awk -F'\t' '{print "aws_access_key_id "$1"\naws_secret_access_key "$2"\naws_session_token "$3 }' | xargs -L 1 aws configure --profile dnschg set
