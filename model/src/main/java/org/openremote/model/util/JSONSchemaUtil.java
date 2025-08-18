bc4f28e64c9cec43da37b1f32c87f28dd1737fdc
Ekhorn <koen@kschellingerhout.nl>
2025-08-18 15:41:45 +0200

    Add container type annotation handling

--- model/src/main/java/org/openremote/model/util/JSONSchemaUtil.java
+++ model/src/main/java/org/openremote/model/util/JSONSchemaUtil.java
@@ -108,6 +108,8 @@ public class JSONSchemaUtil {
     public @interface JsonSchemaTitle {
         String keyword() default "label"; // Expected to be "label" by JSON Forms
         String value();
+        /* Whether to put the title on the root of the schema even when the class is wrapped in an array. */
+        boolean container() default true;
     }
 
     @Retention(RetentionPolicy.RUNTIME)
@@ -115,6 +117,7 @@ public class JSONSchemaUtil {
     public @interface JsonSchemaDescription {
         String keyword() default "description";
         String value();
+        boolean container() default true;
     }
 
     @Retention(RetentionPolicy.RUNTIME)
@@ -122,6 +125,7 @@ public class JSONSchemaUtil {
     public @interface JsonSchemaFormat {
         String keyword() default "format";
         String value();
+        boolean container() default true;
     }
 
     @Retention(RetentionPolicy.RUNTIME)
@@ -129,6 +133,7 @@ public class JSONSchemaUtil {
     public @interface JsonSchemaDefault {
         String keyword() default "default";
         String value();
+        boolean container() default true;
     }
 
     @Retention(RetentionPolicy.RUNTIME)
@@ -136,6 +141,7 @@ public class JSONSchemaUtil {
     public @interface JsonSchemaExamples {
         String keyword() default "examples";
         String[] value();
+        boolean container() default true;
     }
 
     public static class CustomModule implements Module {
@@ -221,7 +227,14 @@ public class JSONSchemaUtil {
                 ObjectNode schema,
                 ObjectMapper mapper
         ) {
-            A annotation = type.getDeclaredAnnotation(annotationClass);
+            A annotation;
+
+            if (type.isArray() && type.getComponentType() != null) {
+                annotation = type.getComponentType().getDeclaredAnnotation(annotationClass);
+            } else {
+                annotation = type.getDeclaredAnnotation(annotationClass);
+            }
+
             if (annotation != null) {
                 applyAnnotation(annotationClass, annotation, schema, mapper);
             }
