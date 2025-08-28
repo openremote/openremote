135664b73eb85ae22c22d487f1157ad499f37d98
Ekhorn <koen@kschellingerhout.nl>
2025-08-28 09:31:41 +0200

    Only include accesible class members in JSON Schema

--- model/src/main/java/org/openremote/model/util/JSONSchemaUtil.java
+++ model/src/main/java/org/openremote/model/util/JSONSchemaUtil.java
@@ -414,9 +414,7 @@ public class JSONSchemaUtil {
             Option.VALUES_FROM_CONSTANT_FIELDS,
             Option.PUBLIC_NONSTATIC_FIELDS,
             Option.NONPUBLIC_NONSTATIC_FIELDS_WITH_GETTERS,
-            Option.NONPUBLIC_NONSTATIC_FIELDS_WITHOUT_GETTERS,
             Option.ALLOF_CLEANUP_AT_THE_END,
-            // Option.DEFINITIONS_FOR_ALL_OBJECTS,
             Option.DUPLICATE_MEMBER_ATTRIBUTE_CLEANUP_AT_THE_END
         ))
         .with(new JacksonModule(
