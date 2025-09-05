72776471608f6bea99eafbae2cbb509c11006b3b
Ekhorn <koen@kschellingerhout.nl>
2025-09-05 09:22:32 +0200

    Add todo comment

--- model/src/main/java/org/openremote/model/util/JSONSchemaUtil.java
+++ model/src/main/java/org/openremote/model/util/JSONSchemaUtil.java
@@ -177,6 +177,7 @@ public class JSONSchemaUtil {
         @Override
         public void applyToConfigBuilder(SchemaGeneratorConfigBuilder builder) {
             // Set title on root of schema
+            // TODO: resolve titles for all custom types
             JSONSchemaTitleProvider titleProvider = new JSONSchemaTitleProvider();
             builder.forTypesInGeneral()
                 .withCustomDefinitionProvider(titleProvider)
