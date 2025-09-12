ecafd80bec51f38fe293819da0dc4756a49e480f
Rich Turner <7072278+richturner@users.noreply.github.com>
2025-09-12 11:03:15 +0100

    WIP

--- model/src/main/java/org/openremote/model/util/LockByKey.java
+++ model/src/main/java/org/openremote/model/util/LockByKey.java
@@ -12,6 +12,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class LockByKey {
 
+    protected static final System.Logger LOG = System.getLogger(LockByKey.class.getName());
+
     protected static class LockWrapper {
         protected final Semaphore lock = new Semaphore(1); // Don't change to ReentrantLock as camel switches thread between try/finally blocks on route processors
         protected final AtomicInteger numberOfThreadsInQueue = new AtomicInteger(1);
@@ -24,33 +26,20 @@ public class LockByKey {
         protected int removeThreadFromQueue() {
             return numberOfThreadsInQueue.decrementAndGet();
         }
-
     }
 
-    protected static final System.Logger LOG = System.getLogger(LockByKey.class.getName());
     protected final Map<String, LockWrapper> locks = new ConcurrentHashMap<>();
 
-    /**
-     * This method is here for overriding purposes in tests
-     */
-    @SuppressWarnings("unused")
     protected LockWrapper createLockWrapper() {
         return new LockWrapper();
     }
 
     public void lock(String key) {
-        LockWrapper lockWrapper = locks.compute(key, (k, v) -> v == null ? new LockWrapper() : v.addThreadInQueue());
+        LockWrapper lockWrapper = locks.compute(key, (k, v) -> v == null ? createLockWrapper() : v.addThreadInQueue());
         try {
             lockWrapper.lock.acquire();
             LOG.log(System.Logger.Level.TRACE, () -> "Lock acquired: key=" + key);
         } catch (InterruptedException e) {
-            // Atomically check the queue count and remove the wrapper if it's the last one.
-            locks.computeIfPresent(key, (k, v) -> {
-                if (v.removeThreadFromQueue() == 0) {
-                    return null; // Atomically remove if the queue is empty
-                }
-                return v;
-            });
             Thread.currentThread().interrupt(); // Restore interrupted status
             throw new RuntimeException("Interrupted while acquiring lock for key: " + key, e);
         }
@@ -61,12 +50,11 @@ public class LockByKey {
         if (lockWrapper != null) {
             LOG.log(System.Logger.Level.TRACE, () -> "Lock release: key=" + key);
             lockWrapper.lock.release();
-            locks.computeIfPresent(key, (k, v) -> {
-                if (v.removeThreadFromQueue() == 0) {
-                    return null; // Atomically remove if queue is empty
-                }
-                return v;
-            });
+            if (lockWrapper.removeThreadFromQueue() == 0) {
+                // NB : We pass in the specific value to remove to handle the case where another thread would queue right before the removal
+                locks.remove(key, lockWrapper);
+            }
         }
     }
+
 }
