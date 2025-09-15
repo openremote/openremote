5df1a31eb3b37ceb83e60b38c4eb40280fe67bce
Eric Bariaux <375613+ebariaux@users.noreply.github.com>
2025-09-15 14:59:10 +0200

    Couple more tests, failing, need to align on how things are supposed to work

--- agent/src/test/java/org/openremote/agent/protocol/simulator/SimulatorAgentLinkTest.java
+++ agent/src/test/java/org/openremote/agent/protocol/simulator/SimulatorAgentLinkTest.java
@@ -1,11 +1,15 @@
 package org.openremote.agent.protocol.simulator;
 
+import net.fortuna.ical4j.model.Recur;
+import net.fortuna.ical4j.transform.recurrence.Frequency;
 import org.junit.jupiter.api.Test;
+import org.openremote.model.util.TimeUtil;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SimulatorAgentLinkTest {
     @Test
@@ -17,5 +21,40 @@ public class SimulatorAgentLinkTest {
         assertEquals(40, schedule.getDelay(100));
         schedule = link.getSchedule(LocalDateTime.of(2000, 1, 1, 0, 2).toEpochSecond(ZoneOffset.UTC), LocalDateTime.of(2000, 1, 1, 0, 0));
         assertEquals(86_380, schedule.getDelay(100));
+    }
+
+    @Test
+    public void getDelayInfiniteCustomRecurring() throws Exception {
+        SimulatorAgentLink link = new SimulatorAgentLink("1");
+        link.setDuration(new TimeUtil.ExtendedPeriodAndDuration("PT20M"));
+        link.setRecurrence(new Recur<LocalDateTime>(Frequency.HOURLY));
+        SimulatorAgentLink.Schedule schedule = link.getSchedule(LocalDateTime.of(2000, 1, 1, 0, 0).toEpochSecond(ZoneOffset.UTC), LocalDateTime.of(2000, 1, 1, 0, 0));
+        assertEquals(100, schedule.getDelay(100));
+        schedule = link.getSchedule(LocalDateTime.of(2000, 1, 1, 0, 1).toEpochSecond(ZoneOffset.UTC), LocalDateTime.of(2000, 1, 1, 0, 0));
+        assertEquals(40, schedule.getDelay(100));
+        schedule = link.getSchedule(LocalDateTime.of(2000, 1, 1, 0, 2).toEpochSecond(ZoneOffset.UTC), LocalDateTime.of(2000, 1, 1, 0, 0));
+        assertEquals(3580, schedule.getDelay(100));
+    }
+
+    @Test
+    public void getDelayCustomRecurringWithEndDate() throws Exception {
+        SimulatorAgentLink link = new SimulatorAgentLink("1");
+        link.setDuration(new TimeUtil.ExtendedPeriodAndDuration("PT20M"));
+        link.setRecurrence(new Recur<LocalDateTime>("FREQ=HOURLY;COUNT=2"));
+        SimulatorAgentLink.Schedule schedule = link.getSchedule(LocalDateTime.of(2000, 1, 1, 0, 0).toEpochSecond(ZoneOffset.UTC), LocalDateTime.of(2000, 1, 1, 0, 0));
+        assertEquals(100, schedule.getDelay(100));
+        schedule = link.getSchedule(LocalDateTime.of(2000, 1, 1, 0, 1).toEpochSecond(ZoneOffset.UTC), LocalDateTime.of(2000, 1, 1, 0, 0));
+        assertEquals(40, schedule.getDelay(100));
+        schedule = link.getSchedule(LocalDateTime.of(2000, 1, 1, 0, 2).toEpochSecond(ZoneOffset.UTC), LocalDateTime.of(2000, 1, 1, 0, 0));
+        assertEquals(3580, schedule.getDelay(100));
+        schedule = link.getSchedule(LocalDateTime.of(2000, 1, 1, 1, 0).toEpochSecond(ZoneOffset.UTC), LocalDateTime.of(2000, 1, 1, 0, 0));
+        assertEquals(100, schedule.getDelay(100));
+        schedule = link.getSchedule(LocalDateTime.of(2000, 1, 1, 1, 1).toEpochSecond(ZoneOffset.UTC), LocalDateTime.of(2000, 1, 1, 0, 0));
+        assertEquals(40, schedule.getDelay(100));
+        schedule = link.getSchedule(LocalDateTime.of(2000, 1, 1, 1, 2).toEpochSecond(ZoneOffset.UTC), LocalDateTime.of(2000, 1, 1, 0, 0));
+        SimulatorAgentLink.Schedule finalSchedule = schedule;
+        assertThrows(Exception.class, () ->  {
+            finalSchedule.getDelay(100);
+        });
     }
 }
