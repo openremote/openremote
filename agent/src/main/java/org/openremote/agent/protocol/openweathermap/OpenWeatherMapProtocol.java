c40cd2cabc8a066862fc60cd0cea75c46a36ba6c
Dominique Kleeven <10584854+dominiquekleeven@users.noreply.github.com>
2025-10-13 12:51:37 +0200

    WIP: Hourly forecast is now written to attribute predicted datapoints

--- agent/src/main/java/org/openremote/agent/protocol/openweathermap/OpenWeatherMapProtocol.java
+++ agent/src/main/java/org/openremote/agent/protocol/openweathermap/OpenWeatherMapProtocol.java
@@ -37,6 +37,7 @@ import org.openremote.model.asset.agent.ConnectionStatus;
 import org.openremote.model.attribute.Attribute;
 import org.openremote.model.attribute.AttributeEvent;
 import org.openremote.model.attribute.AttributeRef;
+import org.openremote.model.datapoint.ValueDatapoint;
 import org.openremote.model.geo.GeoJSONPoint;
 import org.openremote.model.syslog.SyslogCategory;
 
@@ -49,10 +50,13 @@ import static org.openremote.model.syslog.SyslogCategory.PROTOCOL;
  * Protocol for the OpenWeatherMap API (One Call 3.0 API)
  * 
  * This protocol periodically retrieves the weather data for all linked
- * attributes grouped by their coordinates (Attribute Asset location) and makes
- * one API call per distinct location.
+ * attributes grouped by their asset, and using the asset location as the
+ * coordinates for the API call.
  * 
- * It then updates the attributes based on the returned weather data.
+ * The linked attributes have their current value updated based on the returned
+ * current weather data. Additionally, the linked attributes have their
+ * predicted values updated based on the returned forecasted weather data.
+ * (Hourly & Daily)
  * 
  */
 public class OpenWeatherMapProtocol extends AbstractProtocol<OpenWeatherMapAgent, OpenWeatherMapAgentLink> {
@@ -61,7 +65,7 @@ public class OpenWeatherMapProtocol extends AbstractProtocol<OpenWeatherMapAgent
     public static final String PROTOCOL_DISPLAY_NAME = "OpenWeatherMap";
     private static final AtomicReference<ResteasyClient> client = new AtomicReference<>();
 
-    // Initial delay is used so the system has time to establish agent links
+    // Initial delay is used so the system has time to populate the agent links
     private static final int INITIAL_MILLIS_DELAY = 5000; // 5 seconds
     private static final int DEFAULT_POLLING_MILLIS = 3600000; // 1 hour
 
@@ -80,19 +84,6 @@ public class OpenWeatherMapProtocol extends AbstractProtocol<OpenWeatherMapAgent
     @Override
     public String getProtocolInstanceUri() {
         return "https://openweathermap.org/";
-    }
-
-    /**
-     * Get the OpenWeatherMap One Call API URL for the given latitude and longitude
-     * 
-     * @param latitude latitude of the location
-     * @param longitude longitude of the location
-     * @return the OpenWeatherMap One Call API URL
-     */
-    public String getOneCallApiUrl(double latitude, double longitude) {
-        String apiKey = agent.getAPIKey().orElseThrow(() -> new IllegalStateException("API key is not set"));
-        return "https://api.openweathermap.org/data/3.0/onecall?lat=" + latitude + "&lon=" + longitude + "&units=metric&exclude=alerts,minutely&appid="
-                + apiKey;
     }
 
     @Override
@@ -115,7 +106,7 @@ public class OpenWeatherMapProtocol extends AbstractProtocol<OpenWeatherMapAgent
             return;
         }
 
-        // Schedule a task to retrieve weather data periodically
+        // Schedule the polling task
         int pollingMillis = agent.getPollingMillis().orElse(DEFAULT_POLLING_MILLIS);
         pollingFuture = scheduledExecutorService.scheduleAtFixedRate(this::updateWeatherData, INITIAL_MILLIS_DELAY, pollingMillis, TimeUnit.MILLISECONDS);
 
@@ -124,6 +115,7 @@ public class OpenWeatherMapProtocol extends AbstractProtocol<OpenWeatherMapAgent
 
     @Override
     protected void doStop(Container container) throws Exception {
+        // Cancel the polling task
         if (pollingFuture != null) {
             pollingFuture.cancel(true);
         }
@@ -138,11 +130,10 @@ public class OpenWeatherMapProtocol extends AbstractProtocol<OpenWeatherMapAgent
             if (provisionWeatherAsset) {
                 LOG.info("Provisioning weather asset");
 
-                // Uncheck button
                 sendAttributeEvent(new AttributeEvent(agent.getId(), OpenWeatherMapAgent.PROVISION_WEATHER_ASSET.getName(), false));
             }
 
-            return false; // Return false since its not a configuration attribute
+            return false;
         }
 
         return super.onAgentAttributeChanged(event);
@@ -184,7 +175,6 @@ public class OpenWeatherMapProtocol extends AbstractProtocol<OpenWeatherMapAgent
         // Group the attributes by asset ID
         getLinkedAttributes().forEach((attributeRef, attribute) -> assetGroups.computeIfAbsent(attributeRef.getId(), k -> new ArrayList<>()).add(attributeRef));
 
-        // Retrieve the weather data for each asset, updating their linked attributes
         assetGroups.forEach((assetId, attributeRefs) -> {
             Asset<?> asset = assetService.findAsset(assetId);
             if (asset == null) {
@@ -198,17 +188,34 @@ public class OpenWeatherMapProtocol extends AbstractProtocol<OpenWeatherMapAgent
                 return;
             }
 
-            // Fetch the weather data based on the asset location
-            OpenWeatherMapResponse weatherData = fetchWeatherData(getOneCallApiUrl(location.getY(), location.getX()));
+            // Fetch the weather data for the asset location
+            OpenWeatherMapResponse weatherData = fetchWeatherData(buildApiUrl(location.getY(), location.getX()));
 
             if (weatherData != null) {
-                updateAttributes(attributeRefs, weatherData);
+                // Update attributes with current weather data
+                try {
+                    updateAttributes(attributeRefs, weatherData.getCurrent()); // current timestamp
+                } catch (Exception e) {
+                    LOG.log(Level.SEVERE, e, () -> "Failed to update attributes: " + attributeRefs);
+                }
+
+                // Update predicted data points with weather forecast
+                try {
+                    updatePredictedDataPoints(attributeRefs, weatherData.getHourly()); // hourly data up to 48 hours
+                    // updatePredictedDataPoints(attributeRefs, weatherData.getDaily()); // daily
+                    // data up to 8 days
+                } catch (Exception e) {
+                    LOG.log(Level.SEVERE, e, () -> "Failed to write predicted data points: " + attributeRefs);
+                }
             }
         });
     }
 
     /**
      * Fetch the weather data from the OpenWeatherMap API for the given API URL
+     * 
+     * @param apiUrl the API URL
+     * @return the OpenWeatherMapResponse from the API
      */
     protected OpenWeatherMapResponse fetchWeatherData(String apiUrl) {
         try (Response response = client.get().target(apiUrl).request().get()) {
@@ -227,19 +234,18 @@ public class OpenWeatherMapProtocol extends AbstractProtocol<OpenWeatherMapAgent
     }
 
     /**
-     * Update values of the linked attributes based on the given weather data
+     * Update values of the linked attributes based on the given weather datapoint
+     * 
+     * @param attributeRefs list of attribute references
+     * @param currentWeatherDatapoint the current weather datapoint
      */
-    protected void updateAttributes(List<AttributeRef> attributeRefs, OpenWeatherMapResponse weatherData) {
+    protected void updateAttributes(List<AttributeRef> attributeRefs, OpenWeatherMapResponse.WeatherDatapoint currentWeatherDatapoint) {
         for (AttributeRef attributeRef : attributeRefs) {
-            Attribute<?> attribute = linkedAttributes.get(attributeRef);
-
-            // Get the agent link
-            OpenWeatherMapAgentLink agentLink = agent.getAgentLink(attribute);
-            OpenWeatherMapField field = agentLink.getField();
+            OpenWeatherMapField field = getWeatherFieldType(attributeRef);
 
             // Get the corresponding `field` value from the weather data object
-            OpenWeatherMapResponse.WeatherDatapoint currentWeatherData = weatherData.getCurrent();
-            Object value = getFieldValue(currentWeatherData, field);
+
+            Object value = getWeatherFieldValue(currentWeatherDatapoint, field);
 
             if (value == null) {
                 LOG.log(Level.WARNING, () -> "Field or value is null for attribute: " + attributeRef);
@@ -251,9 +257,56 @@ public class OpenWeatherMapProtocol extends AbstractProtocol<OpenWeatherMapAgent
     }
 
     /**
-     * Get the corresponding `field` value from the weather data object
+     * Write the predicted data points for the given attributes and weather data
+     * 
+     * @param attributeRefs list of attribute references
+     * @param weatherDatapoints list of weather datapoints
      */
-    protected Object getFieldValue(OpenWeatherMapResponse.WeatherDatapoint weatherData, OpenWeatherMapField field) {
+    protected void updatePredictedDataPoints(List<AttributeRef> attributeRefs, List<OpenWeatherMapResponse.WeatherDatapoint> weatherDatapoints) {
+        for (AttributeRef attributeRef : attributeRefs) {
+
+            if (weatherDatapoints == null || weatherDatapoints.isEmpty()) {
+                LOG.log(Level.WARNING, () -> "Weather data points are null or empty for attribute: " + attributeRef);
+                continue;
+            }
+
+            // Build the predicted datapoints
+            List<ValueDatapoint<?>> valuesAndTimestamps = buildValueDatapoints(attributeRef, weatherDatapoints);
+
+            // Update the predicted datapoints for the attribute reference
+            predictedDatapointService.updateValues(attributeRef.getId(), attributeRef.getName(), valuesAndTimestamps);
+        }
+    }
+
+    /**
+     * Builds a list of ValueDatapoints based on the provided weather data and
+     * attribute reference
+     * 
+     * @param attributeRef the attribute reference
+     * @param weatherData the weather data
+     * @return a list of ValueDatapoints
+     */
+    protected List<ValueDatapoint<?>> buildValueDatapoints(AttributeRef attributeRef, List<OpenWeatherMapResponse.WeatherDatapoint> weatherData) {
+        List<ValueDatapoint<?>> values = new ArrayList<>();
+        OpenWeatherMapField field = getWeatherFieldType(attributeRef);
+
+        // Populate the values and timestamps for the attribute
+        for (OpenWeatherMapResponse.WeatherDatapoint weatherDataPoint : weatherData) {
+            Object value = getWeatherFieldValue(weatherDataPoint, field);
+            values.add(new ValueDatapoint<>(toMillis(weatherDataPoint.getTimestamp()), value));
+        }
+
+        return values;
+    }
+
+    /**
+     * Get the corresponding `field` value from the weather data object
+     * 
+     * @param weatherData the weather datapoint
+     * @param field the field to get the value from
+     * @return the corresponding `field` value
+     */
+    protected Object getWeatherFieldValue(OpenWeatherMapResponse.WeatherDatapoint weatherData, OpenWeatherMapField field) {
         switch (field) {
         case TEMPERATURE:
             return weatherData.getTemperature();
@@ -281,10 +334,38 @@ public class OpenWeatherMapProtocol extends AbstractProtocol<OpenWeatherMapAgent
     }
 
     /**
-     * Health check, by sending a request to the OpenWeatherMap API
+     * Get the agent link OpenWeatherMapField for the given attribute reference
+     * 
+     * @param attributeRef the attribute reference
+     * @return the OpenWeatherMapField
+     */
+    protected OpenWeatherMapField getWeatherFieldType(AttributeRef attributeRef) {
+        Attribute<?> attribute = linkedAttributes.get(attributeRef);
+        OpenWeatherMapAgentLink agentLink = agent.getAgentLink(attribute);
+        return agentLink.getField();
+    }
+
+    /**
+     * Build the OpenWeatherMap API URL for the given latitude and longitude (One
+     * Call 3.0 API)
+     * 
+     * @param latitude the latitude
+     * @param longitude the longitude
+     * @return the OpenWeatherMap API URL
+     */
+    public String buildApiUrl(double latitude, double longitude) {
+        String apiKey = agent.getAPIKey().orElseThrow(() -> new IllegalStateException("API key is not set"));
+        return "https://api.openweathermap.org/data/3.0/onecall?lat=" + latitude + "&lon=" + longitude + "&units=metric&exclude=alerts,minutely&appid="
+                + apiKey;
+    }
+
+    /**
+     * Perform a health check by sending a request to the OpenWeatherMap API
+     * 
+     * @return true if the health check is successful, false otherwise
      */
     protected boolean healthCheck() {
-        String apiUrl = getOneCallApiUrl(0, 0);
+        String apiUrl = buildApiUrl(0, 0);
         try (Response response = client.get().target(apiUrl).request().get()) {
             if (response.getStatus() != 200) {
                 LOG.warning("Health check failed with status: " + response.getStatus());
@@ -312,6 +393,10 @@ public class OpenWeatherMapProtocol extends AbstractProtocol<OpenWeatherMapAgent
                 client.set(createClient(org.openremote.container.Container.SCHEDULED_EXECUTOR));
             }
         }
+    }
+
+    protected long toMillis(long timestamp) {
+        return timestamp * 1000;
     }
 
 }
