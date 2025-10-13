45282dd5124ccbe6fcdc4450fd415b315e383099
MartinaeyNL <business@martinaey.nl>
2025-10-13 14:47:19 +0200

    WIP: Slight adjustments to rsbuild alias resolve again

--- ui/app/storybook/rsbuild.config.ts
+++ ui/app/storybook/rsbuild.config.ts
@@ -14,19 +14,11 @@ export default defineConfig({
             // Webpack fallbacks for Node.js modules
             vm: false,
             // Workspace aliases for proper resolution
-            "@openremote/model": "../../component/model",
-            "@openremote/rest": "../../component/rest",
-            "@openremote/core": "../../component/core",
-            "@openremote/or-app": "../../component/or-app",
-            "@openremote/or-translate": "../../component/or-translate",
-            "@openremote/or-components": "../../component/or-components",
-            "@openremote/or-mwc-components": "../../component/or-mwc-components",
-            "@openremote/or-asset-tree": "../../component/or-asset-tree",
-            "@openremote/or-chart": "../../component/or-chart",
-            "@openremote/or-icon": "../../component/or-icon",
-            "@openremote/or-input": "../../component/or-input",
-            "@openremote/or-map": "../../component/or-map",
-            "@openremote/or-rules": "../../component/or-rules",
+            "@openremote/core": "../../component/core/src",
+            "@openremote/model": "../../component/model/src",
+            "@openremote/or-mwc-components": "../../component/or-mwc-components/src",
+            "@openremote/or-translate": "../../component/or-translate/src",
+            "@openremote/or-tree-menu": "../../component/or-tree-menu/src",
             "@openremote/util": "../../util"
         },
         // Enable symlinks resolution for workspace packages
