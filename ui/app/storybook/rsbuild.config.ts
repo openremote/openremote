fa9a2020b93dabc7dd0b5d3b056314a62e97b5d7
MartinaeyNL <business@martinaey.nl>
2025-10-13 14:08:46 +0200

    Attempt to fix local workspace references not working, by manually resolving them in rsbuild.config.ts

--- ui/app/storybook/rsbuild.config.ts
+++ ui/app/storybook/rsbuild.config.ts
@@ -8,5 +8,28 @@ export default defineConfig({
         decorators: {
             version: "legacy"
         }
+    },
+    resolve: {
+        alias: {
+            // Webpack fallbacks for Node.js modules
+            vm: false,
+            // Workspace aliases for proper resolution
+            "@openremote/model": "../../component/model/src",
+            "@openremote/rest": "../../component/rest/src",
+            "@openremote/core": "../../component/core/src",
+            "@openremote/or-app": "../../component/or-app/src",
+            "@openremote/or-translate": "../../component/or-translate/src",
+            "@openremote/or-components": "../../component/or-components/src",
+            "@openremote/or-mwc-components": "../../component/or-mwc-components/src",
+            "@openremote/or-asset-tree": "../../component/or-asset-tree/src",
+            "@openremote/or-chart": "../../component/or-chart/src",
+            "@openremote/or-icon": "../../component/or-icon/src",
+            "@openremote/or-input": "../../component/or-input/src",
+            "@openremote/or-map": "../../component/or-map/src",
+            "@openremote/or-rules": "../../component/or-rules/src",
+            "@openremote/util": "../../util"
+        },
+        // Enable symlinks resolution for workspace packages
+        symlinks: true
     }
 });
