b6df7d40844548af7f5f960391450e7642412175
MartinaeyNL <business@martinaey.nl>
2025-10-15 11:19:28 +0200

    Added automatically generated component documentation using a custom componentdocs.mdx file, and overall added a lot of utility functions.

--- ui/app/storybook/.storybook/preview.ts
+++ ui/app/storybook/.storybook/preview.ts
@@ -1,11 +1,25 @@
 import type {Preview} from "@storybook/web-components";
 
+import './styles.css';
+
 const preview: Preview = {
     parameters: {
         controls: {
             matchers: {
                 color: /(background|color)$/i,
                 date: /Date$/i
+            }
+        },
+        docs: {
+            toc: {
+                disable: false,
+                headingSelector: "h2,h3"
+            }
+        },
+        options: {
+            storySort: {
+                method: "alphabetical",
+                order: ["Introduction", "How to Install"]
             }
         }
     }
--- ui/component/or-tree-menu/stories/or-tree-menu.stories.ts
+++ ui/component/or-tree-menu/stories/or-tree-menu.stories.ts
@@ -1,12 +1,19 @@
 import {setCustomElementsManifest, type Meta, type StoryObj } from "@storybook/web-components";
 import customElements from "../custom-elements.json" with { type: "json" };
+import packageJson from "../package.json" with { type: "json" };
 import "../src/index";
 
 type Story = StoryObj;
 setCustomElementsManifest(customElements);
 
 const meta: Meta = {
-    component: "or-tree-menu"
+    title: "Playground/or-tree-menu",
+    component: "or-tree-menu",
+    parameters: {
+        docs: {
+            subtitle: "<or-tree-menu>"
+        }
+    }
 };
 
 export const Primary: Story = {
@@ -14,5 +21,19 @@ export const Primary: Story = {
         menuTitle: "My custom title"
     }
 };
+
+export const TreeExample: Story = {
+    name: "Building a Tree structure",
+    parameters: {
+        summary: "You can define the tree using `<or-tree-group>` and `<or-tree-node>` elements."
+    },
+    args: {
+        menuTitle: "My Assets"
+    }
+};
+
+export const examples: Story[] = [TreeExample];
+
+export {customElements, packageJson};
 
 export default meta;
