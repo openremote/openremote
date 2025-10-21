67b473bdd3045eab2bdda3b27db2dcdf60353953
MartinaeyNL <business@martinaey.nl>
2025-10-21 14:32:05 +0200

    Removed unused reference to HTML template in rsbuild config.

--- ui/app/storybook/rsbuild.config.ts
+++ ui/app/storybook/rsbuild.config.ts
@@ -1,9 +1,6 @@
 import {defineConfig} from "@rsbuild/core";
 
 export default defineConfig({
-    html: {
-        template: "./src/index.html"
-    },
     source: {
         decorators: {
             version: "legacy"
