13129bde42fce4b898680b99cb19bed7a7ce39c2
MartinaeyNL <business@martinaey.nl>
2025-10-29 11:45:50 +0100

    Added automatic generation of JSX types for the UI components

--- ui/component/custom-elements-manifest.config.mjs
+++ ui/component/custom-elements-manifest.config.mjs
@@ -1,4 +1,9 @@
+import { jsxTypesPlugin } from "@wc-toolkit/jsx-types";
+
 export default {
-  globs: ['src/**.ts'],
-  litelement: true
+    globs: ['src/**.ts'],
+    litelement: true,
+    plugins: [
+        jsxTypesPlugin({fileName: "custom-elements-jsx.d.ts"})
+    ],
 }
