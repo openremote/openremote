d68dc86bd6a73b2a5821a631b4c650f76e836a3a
MartinaeyNL <business@martinaey.nl>
2025-11-28 16:26:46 +0100

    WIP: Initial setup of Vaadin components. Be aware, not working yet.

---
+++ ui/component/or-vaadin-input/src/index.ts
@@ -0,0 +1,12 @@
+import {html, LitElement } from "lit";
+import { customElement } from "lit/decorators.js";
+
+@customElement("or-vaadin-input")
+export class OrVaadinInput extends LitElement {
+
+    render() {
+        return html`
+            <span>Work in progress.</span>
+        `;
+    }
+}
