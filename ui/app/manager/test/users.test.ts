56b9c19d5db837aeb99c87cc46127ddf88bc55e7
MartinaeyNL <business@martinaey.nl>
2026-01-07 13:53:01 +0100

    Add test on the intentional behavior when switching browser tabs

--- ui/app/manager/test/users.test.ts
+++ ui/app/manager/test/users.test.ts
@@ -1,5 +1,6 @@
 import { expect } from "@openremote/test";
 import { adminStatePath, test} from "./fixtures/manager";
+import permissions from "./fixtures/data/permissions";
 
 
 test.use({ storageState: adminStatePath });
@@ -60,6 +61,28 @@ test(`Create service users with tags and search by tag`, async ({ page, manager,
   await expect(page.getByRole("cell", { name: "serviceuser1", exact: true })).toBeVisible();
   await expect(page.getByRole("cell", { name: "serviceuser2", exact: true })).toBeVisible();
 });
+
+/**
+ * @given Logged into the "master" realm as "admin"
+ * @and Navigated to the "Users" page
+ * @when Trying to create a regular user in the creation window
+ * @and You switch to a different tab in your browser, and navigate back later. (simulated using the visibilitychange event)
+ * @then The creation window still has the same state, so you can continue creating that user.
+ */
+test(`Verify browser behavior while creating regular users`, async ({ context, page, manager, usersPage }) => {
+    await usersPage.gotoUserCreation("master", "regular");
+    await page.locator("label").filter({ hasText: "Username" }).fill("mycustomusername");
+    await page.locator("label").filter({ hasText: "Email" }).fill("mycustom@email.com");
+    await usersPage.toHavePermissions()
+    await usersPage.toggleUserRoles("Read", "Write");
+    await usersPage.toHavePermissions(...permissions);
+    await page.evaluate(() => {
+        document.dispatchEvent(new Event('visibilitychange'));
+    })
+    await expect(await page.getByRole('textbox', {name: /username/i }).inputValue()).toBe("mycustomusername");
+    await expect(await page.getByRole('textbox', {name: /email/i }).inputValue()).toBe("mycustom@email.com");
+    await usersPage.toHavePermissions(...permissions);
+})
 
 /**
  * @given Logged into the "smartcity" realm as "admin"
