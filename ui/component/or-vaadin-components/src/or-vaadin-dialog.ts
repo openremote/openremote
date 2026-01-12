054c33a12abf44a0f078c698c64072f143c52aa6
MartinaeyNL <business@martinaey.nl>
2026-01-12 13:31:48 +0100

    Added Vaadin dialog

---
+++ ui/component/or-vaadin-components/src/or-vaadin-dialog.ts
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2026, OpenRemote Inc.
+ *
+ * See the CONTRIBUTORS.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+import {Dialog} from "@vaadin/dialog";
+import {OrVaadinComponent} from "./util";
+import {customElement} from "lit/decorators.js";
+
+export {dialogFooterRenderer, dialogRenderer} from "@vaadin/dialog/lit.js";
+
+@customElement("or-vaadin-dialog")
+export class OrVaadinDialog extends Dialog implements OrVaadinComponent {
+
+}
