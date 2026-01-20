edcfef94b520dcac0dfb878fafc466c75f338902
Ekhorn <koen@kschellingerhout.nl>
2026-01-20 18:12:11 +0100

    Refactor walkForm test fixture for or-json-forms

--- ui/component/or-json-forms/test/fixtures/index.ts
+++ ui/component/or-json-forms/test/fixtures/index.ts
@@ -79,8 +79,13 @@ export interface JsonSchema extends JsonSchema7 {
     else?: JsonSchema;
 }
 
+type Path = (string | number)[];
+type Parent = "array" | "object"; // TODO: remove first condition when or-json-forms-array-control always renders titles
+
 export class JsonForms {
-    constructor(private readonly page: Page) {}
+    constructor(private readonly page: Page, private dialog: Locator) {
+        this.dialog = this.page.locator("or-mwc-dialog");
+    }
 
     /**
      * Exhaust every `or-json-forms` option based on the provided JSONSchema. The following is done:
@@ -100,96 +105,25 @@ export class JsonForms {
         locator: Locator,
         schema: JsonSchema,
         options?: WalkFormOptions,
-        path: (string | number)[] = [],
+        path: Path = [],
         item = 0,
-        parent?: "array" | "object", // TODO: remove first condition when or-json-forms-array-control always renders titles
+        parent?: Parent, // TODO: remove when or-json-forms-array-control always renders titles
         expected?: any
     ) {
-        const dialog = this.page.locator("or-mwc-dialog");
-
         switch (schema.type) {
             case "array": {
-                locator = locator.locator("or-json-forms-array-control").nth(item);
-                path.push("or-json-forms-array-control", item);
-                await locator.locator("or-collapsible-panel").click();
-                if (!Array.isArray(schema?.items) && schema.items?.oneOf) {
-                    for (const prop of Object.values<any>(schema.items.oneOf)) {
-                        await locator.getByRole("button", { name: "Add Item" }).click();
-                        await dialog.locator("li").getByText(prop.title, { exact: true }).click();
-                        await dialog.getByRole("button", { name: "Add", exact: true }).click();
-                    }
-                } else {
-                    for (let i = 0; i < (schema?.["or:test:item:count"] ?? 0); i++) {
-                        await locator.getByRole("button", { name: "Add Item" }).click();
-                    }
-                }
+                locator = await this.walkArray(locator, schema, path, item);
                 break;
             }
             case "object": {
-                locator = locator.locator("or-json-forms-vertical-layout").nth(item);
-                path.push("or-json-forms-vertical-layout", item);
-                await locator.locator("or-collapsible-panel").click();
-                if (schema.patternProperties) {
-                    await locator.getByRole("button", { name: "Add Parameter" }).click();
-                    await this.page
-                        .getByRole("alertdialog", { name: " - Add" })
-                        .locator("label", { hasText: "Key" })
-                        .pressSequentially("test");
-                    await dialog.getByRole("button", { name: "Add", exact: true }).click();
-                } else {
-                    const properties = options?.selectAllProps
-                        ? Object.keys(schema.properties ?? {})
-                        : schema?.["or:test:props"] ?? [];
-                    for (const key of properties) {
-                        if (key === "type" || key === "id" || schema.required?.includes(key)) continue;
-                        await locator.getByRole("button", { name: "Add Parameter" }).click();
-
-                        const name = Util.camelCaseToSentenceCase(key);
-                        await dialog.locator("or-mwc-list li").getByText(name, { exact: true }).click();
-                        const anyOfPicker = dialog.locator("#schema-picker or-mwc-input");
-                        if (await anyOfPicker.isVisible()) {
-                            await anyOfPicker.click();
-                            await anyOfPicker.locator("li").first().click();
-                        }
-                        await dialog.getByRole("button", { name: "Add", exact: true }).click();
-                    }
-                }
+                locator = await this.walkObject(locator, schema, options, path, item);
                 break;
             }
-            case "string": {
-                /// TODO: remove first condition when or-json-forms-array-control always renders titles
-                const options = parent !== "array" && schema.title ? { name: schema.title, exact: true } : {};
-                locator = locator.getByRole("textbox", options).nth(item);
-                await expect(locator).toBeVisible();
-                await locator.fill(schema["or:test:value"] ?? "test");
-                await expect(locator).toHaveValue(schema["or:test:value"] ?? "test");
-                break;
-            }
-            case "number": {
-                const options = parent !== "array" && schema.title ? { name: schema.title, exact: true } : {};
-                locator = locator.getByRole("spinbutton", options).nth(item);
-                await expect(locator).toBeVisible();
-                await locator.fill(`${schema["or:test:value"] ?? 0}`);
-                await expect(locator).toHaveValue(`${schema["or:test:value"] ?? 0}`);
-                break;
-            }
-            case "integer": {
-                const options = parent !== "array" && schema.title ? { name: schema.title, exact: true } : {};
-                locator = locator.getByRole("spinbutton", options).nth(item);
-                await expect(locator).toBeVisible();
-                await locator.fill(`${schema["or:test:value"] ?? 0}`);
-                await expect(locator).toHaveValue(`${schema["or:test:value"] ?? 0}`);
-                break;
-            }
+            case "string":
+            case "number":
+            case "integer":
             case "boolean": {
-                const options = parent !== "array" && schema.title ? { name: schema.title, exact: true } : {};
-                locator = locator.getByRole("checkbox", options).nth(item);
-                await expect(locator).toBeVisible();
-                await expect(locator).not.toBeChecked();
-                if (schema["or:test:value"]) {
-                    await locator.check();
-                    await expect(locator).toBeChecked();
-                }
+                await this.walkPrimitive(locator, schema, item, parent);
                 break;
             }
         }
@@ -233,6 +167,83 @@ export class JsonForms {
 
     public async getValidity(form: Locator) {
         return form.evaluate((el: OrJSONForms) => el.checkValidity());
+    }
+
+    private async walkArray(locator: Locator, schema: JsonSchema, path: Path, item: number) {
+        locator = locator.locator("or-json-forms-array-control").nth(item);
+        path.push("or-json-forms-array-control", item);
+        await locator.locator("or-collapsible-panel").click();
+        if (!Array.isArray(schema?.items) && schema.items?.oneOf) {
+            for (const prop of Object.values<any>(schema.items.oneOf)) {
+                await locator.getByRole("button", { name: "Add Item" }).click();
+                await this.dialog.locator("li").getByText(prop.title, { exact: true }).click();
+                await this.dialog.getByRole("button", { name: "Add", exact: true }).click();
+            }
+        } else {
+            for (let i = 0; i < (schema?.["or:test:item:count"] ?? 0); i++) {
+                await locator.getByRole("button", { name: "Add Item" }).click();
+            }
+        }
+        return locator;
+    }
+
+    private async walkObject(locator: Locator, schema: JsonSchema, options: WalkFormOptions, path: Path, item: number) {
+        locator = locator.locator("or-json-forms-vertical-layout").nth(item);
+        path.push("or-json-forms-vertical-layout", item);
+        await locator.locator("or-collapsible-panel").click();
+
+        if (schema.patternProperties) {
+            await locator.getByRole("button", { name: "Add Parameter" }).click();
+            await this.page
+                .getByRole("alertdialog", { name: " - Add" })
+                .locator("label", { hasText: "Key" })
+                .pressSequentially("test");
+            await this.dialog.getByRole("button", { name: "Add", exact: true }).click();
+        } else {
+            const properties = options?.selectAllProps
+                ? Object.keys(schema.properties ?? {})
+                : schema?.["or:test:props"] ?? [];
+
+            for (const key of properties) {
+                if (key === "type" || key === "id" || schema.required?.includes(key)) continue;
+                await locator.getByRole("button", { name: "Add Parameter" }).click();
+
+                const name = Util.camelCaseToSentenceCase(key);
+                await this.dialog.locator("or-mwc-list li").getByText(name, { exact: true }).click();
+                const anyOfPicker = this.dialog.locator("#schema-picker or-mwc-input");
+                if (await anyOfPicker.isVisible()) {
+                    await anyOfPicker.click();
+                    await anyOfPicker.locator("li").first().click();
+                }
+                await this.dialog.getByRole("button", { name: "Add", exact: true }).click();
+            }
+        }
+        return locator;
+    }
+
+    private async walkPrimitive(locator: Locator, schema: JsonSchema, item: number, parent?: Parent) {
+        const { type, role, fallback } = [
+            { type: "boolean", role: "checkbox", fallback: false },
+            { type: "integer", role: "spinbutton", fallback: 0 },
+            { type: "number", role: "spinbutton", fallback: 0 },
+            { type: "string", role: "textbox", fallback: "test" },
+        ].find(({ type }) => type === schema.type)!;
+
+        // TODO: remove first condition when or-json-forms-array-control always renders titles
+        const options = parent !== "array" && schema.title ? { name: schema.title, exact: true } : {};
+        locator = locator.getByRole(role as any, options).nth(item);
+        await expect(locator).toBeVisible();
+
+        if (type === "boolean") {
+            await expect(locator).not.toBeChecked();
+            if (schema["or:test:value"]) {
+                await locator.check();
+                await expect(locator).toBeChecked();
+            }
+        } else {
+            await locator.fill(String(schema["or:test:value"] ?? fallback));
+            await expect(locator).toHaveValue(String(schema["or:test:value"] ?? fallback));
+        }
     }
 }
 
