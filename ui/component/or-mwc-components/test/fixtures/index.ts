9a4078d1c28afe019e6f84bbbb91eb188c31dff8
Koen <44117179+Ekhorn@users.noreply.github.com>
2026-01-30 10:16:12 +0100

    Fix default text color example not being shown (#2452)

--- ui/component/or-mwc-components/test/fixtures/index.ts
+++ ui/component/or-mwc-components/test/fixtures/index.ts
@@ -5,10 +5,31 @@ export class MwcInput {
     constructor(private readonly page: Page) {}
 
     /**
+     * Get the underlying native input element of the or-mwc-input element
+     * @param type The {@link InputType} to look for
+     * @param locator The locator to start from
+     * @returns The locator to the underlying <input> element inside the or-mwc-input
+     */
+    getInputByType(type: `${InputType}`, locator?: Locator) {
+        return (locator ?? this.page).locator(`or-mwc-input[type=${type}] #component > input`);
+    }
+
+    /**
      * Returns the locator for the specified {@link InputType.SELECT|select input} option
      */
     getSelectInputOption(option: string, locator?: Locator): Locator {
         return (locator ?? this.page).locator("or-mwc-input li[role=option]").getByText(option, { exact: true });
+    }
+}
+
+export class MwcMenu {
+    constructor(private readonly page: Page) {}
+
+    /**
+     * Returns the locator for the specified menu item
+     */
+    getMenuItem(option: string, locator?: Locator): Locator {
+        return (locator ?? this.page).locator("or-mwc-menu li[role=menuitem]", { hasText: option });
     }
 }
 
