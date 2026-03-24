9fe19611e0273668708869203ef5773db99ec77b
MartinaeyNL <business@martinaey.nl>
2026-03-24 11:07:21 +0100

    WIP: Delay reconnect with 500ms

--- ui/component/or-app/src/index.ts
+++ ui/component/or-app/src/index.ts
@@ -164,16 +164,17 @@ export class OrApp<S extends AppStateKeyed> extends LitElement {
 
         if(document.visibilityState === "visible") {
             this._store.dispatch((setVisibility(true)));
-
-            // When the manager appears on Mobile devices, but the connection is OFFLINE,
-            // we reset the timer to the {appConfig.offlineTimeout} seconds. This is because we saw issues with reopening the app,
-            // and seeing a connection interval of 30+ seconds. We now give the user the benefit of the doubt, by resetting the timer.
-            if(manager.console?.isMobile && this._offline) {
-                console.debug("Mobile is offline, starting timer before showing 'you are offline' screen.");
-                this._startOfflineFallbackTimer(true);
-            }
-            // Always try reconnecting the event provider (just in case we are disconnected)
-            manager.reconnect();
+            setTimeout(() => {
+                // When the manager appears on Mobile devices, but the connection is OFFLINE,
+                // we reset the timer to the {appConfig.offlineTimeout} seconds. This is because we saw issues with reopening the app,
+                // and seeing a connection interval of 30+ seconds. We now give the user the benefit of the doubt, by resetting the timer.
+                if(manager.console?.isMobile && this._offline) {
+                    console.debug("Mobile is offline, starting timer before showing 'you are offline' screen.");
+                    this._startOfflineFallbackTimer(true);
+                }
+                // Always try reconnecting the event provider (just in case we are disconnected)
+                manager.reconnect();
+            }, 500);
         } else {
             this._store.dispatch((setVisibility(false)));
         }
