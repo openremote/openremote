3edfae90ab5e2661b464acd6d217b7c6bf142020
oxey <lucoerlemans37@gmail.com>
2026-05-11 13:31:18 +0200

    add nixos dependencies through a flake to run the project
    adds a `flake.nix`, `.envrc` to load the flake automatically through direnv and adds `.direnv` to
    the gitignore.

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "A Bun devshell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              nodejs
+              docker
+              yarn
+              jdk
+              pkg-config
+            ];
+          };
+      }
+    );
+}
