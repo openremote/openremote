646c3a6ecdfb5901b973f1a86f7e7b65dfd2f60f
MartinaeyNL <business@martinaey.nl>
2026-05-29 16:34:17 +0200

    Few more test fixes

--- ui/app/manager/src/pages/page-roles.ts
+++ ui/app/manager/src/pages/page-roles.ts
@@ -457,7 +457,7 @@ export class PageRoles extends Page<AppStateKeyed> {
                                             </or-vaadin-button>
                                             <or-vaadin-button theme="primary" ?disabled=${!role.name || !role.description || this._compositeRoles.some(r => r.compositeRoleIds.length === 0) || this._compositeRoles.some((r, i) => this._compositeRoles.some((o, j) => i !== j && r.name && r.name === o.name))}
                                                               @click=${() => this._updateRoles()}>
-                                                <or-translate value="save"></or-translate>
+                                                <or-translate value="update"></or-translate>
                                             </or-vaadin-button>
                                         ` : html`
                                             <or-vaadin-button @click=${() => { this._compositeRoles.splice(-1, 1); this._compositeRoles = [...this._compositeRoles] }}>
@@ -465,7 +465,7 @@ export class PageRoles extends Page<AppStateKeyed> {
                                             </or-vaadin-button>
                                             <or-vaadin-button theme="primary" ?disabled=${!role.name || !role.description || this._compositeRoles.some(r => r.compositeRoleIds.length === 0) || this._compositeRoles.some((r, i) => this._compositeRoles.some((o, j) => i !== j && r.name && r.name === o.name))}
                                                               @click=${() => this._updateRoles()}>
-                                                <or-translate value="update"></or-translate>
+                                                <or-translate value="save"></or-translate>
                                             </or-vaadin-button>
                                         `}
                                     </div>
