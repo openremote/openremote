1b3d140dcc3ebdc9b3586d4a104aaeee601fd992
Rich Turner <7072278+richturner@users.noreply.github.com>
2026-06-17 09:03:15 +0100

    WIP

--- test/src/test/groovy/org/openremote/test/system/VersionUtilTest.groovy
+++ test/src/test/groovy/org/openremote/test/system/VersionUtilTest.groovy
@@ -1,15 +1,15 @@
 package org.openremote.test.system
 
-import org.openremote.manager.system.VersionInfo
+import org.openremote.model.util.VersionUtil
 import spock.lang.Specification
 import spock.lang.Unroll
 
-class VersionInfoTest extends Specification {
+class VersionUtilTest extends Specification {
 
     @Unroll
     def "compareVersions('#v1', '#v2') should return #expected"() {
         expect:
-        Integer.signum(VersionInfo.compareVersions(v1, v2)) == Integer.signum(expected)
+        Integer.signum(VersionUtil.compareVersions(v1, v2)) == Integer.signum(expected)
 
         where:
         v1          | v2          || expected
@@ -30,7 +30,7 @@ class VersionInfoTest extends Specification {
     @Unroll
     def "compareVersions with qualifiers: '#v1' vs '#v2'"() {
         expect:
-        Integer.signum(VersionInfo.compareVersions(v1, v2)) == Integer.signum(expected)
+        Integer.signum(VersionUtil.compareVersions(v1, v2)) == Integer.signum(expected)
 
         where:
         v1              | v2              || expected
@@ -42,19 +42,19 @@ class VersionInfoTest extends Specification {
 
     def "compareVersions should handle empty segments"() {
         expect:
-        VersionInfo.compareVersions("1..0", "1.0.0") == 0
-        VersionInfo.compareVersions("1.0.", "1.0.0") == 0
+        VersionUtil.compareVersions("1..0", "1.0.0") == 0
+        VersionUtil.compareVersions("1.0.", "1.0.0") == 0
     }
 
     def "compareVersions should throw NullPointerException for null inputs"() {
         when:
-        VersionInfo.compareVersions(null, "1.0.0")
+        VersionUtil.compareVersions(null, "1.0.0")
 
         then:
         thrown(NullPointerException)
 
         when:
-        VersionInfo.compareVersions("1.0.0", null)
+        VersionUtil.compareVersions("1.0.0", null)
 
         then:
         thrown(NullPointerException)
@@ -62,47 +62,47 @@ class VersionInfoTest extends Specification {
 
     def "isVersionGreater should work correctly"() {
         expect:
-        VersionInfo.isVersionGreater("2.0.0", "1.0.0") == true
-        VersionInfo.isVersionGreater("1.0.0", "2.0.0") == false
-        VersionInfo.isVersionGreater("1.0.0", "1.0.0") == false
+        VersionUtil.isVersionGreater("2.0.0", "1.0.0") == true
+        VersionUtil.isVersionGreater("1.0.0", "2.0.0") == false
+        VersionUtil.isVersionGreater("1.0.0", "1.0.0") == false
     }
 
     def "isVersionGreaterOrEqual should work correctly"() {
         expect:
-        VersionInfo.isVersionGreaterOrEqual("2.0.0", "1.0.0") == true
-        VersionInfo.isVersionGreaterOrEqual("1.0.0", "1.0.0") == true
-        VersionInfo.isVersionGreaterOrEqual("1.0.0", "2.0.0") == false
+        VersionUtil.isVersionGreaterOrEqual("2.0.0", "1.0.0") == true
+        VersionUtil.isVersionGreaterOrEqual("1.0.0", "1.0.0") == true
+        VersionUtil.isVersionGreaterOrEqual("1.0.0", "2.0.0") == false
     }
 
     def "isVersionEqual should work correctly"() {
         expect:
-        VersionInfo.isVersionEqual("1.0.0", "1.0.0") == true
-        VersionInfo.isVersionEqual("1.0", "1.0.0") == true
-        VersionInfo.isVersionEqual("1.0.0", "1.0.1") == false
+        VersionUtil.isVersionEqual("1.0.0", "1.0.0") == true
+        VersionUtil.isVersionEqual("1.0", "1.0.0") == true
+        VersionUtil.isVersionEqual("1.0.0", "1.0.1") == false
     }
 
     def "isVersionLess should work correctly"() {
         expect:
-        VersionInfo.isVersionLess("1.0.0", "2.0.0") == true
-        VersionInfo.isVersionLess("2.0.0", "1.0.0") == false
-        VersionInfo.isVersionLess("1.0.0", "1.0.0") == false
+        VersionUtil.isVersionLess("1.0.0", "2.0.0") == true
+        VersionUtil.isVersionLess("2.0.0", "1.0.0") == false
+        VersionUtil.isVersionLess("1.0.0", "1.0.0") == false
     }
 
     def "isVersionLessOrEqual should work correctly"() {
         expect:
-        VersionInfo.isVersionLessOrEqual("1.0.0", "2.0.0") == true
-        VersionInfo.isVersionLessOrEqual("1.0.0", "1.0.0") == true
-        VersionInfo.isVersionLessOrEqual("2.0.0", "1.0.0") == false
+        VersionUtil.isVersionLessOrEqual("1.0.0", "2.0.0") == true
+        VersionUtil.isVersionLessOrEqual("1.0.0", "1.0.0") == true
+        VersionUtil.isVersionLessOrEqual("2.0.0", "1.0.0") == false
     }
 
     def "parsePart should handle various formats"() {
         expect:
-        VersionInfo.parsePart("123") == 123
-        VersionInfo.parsePart("1-SNAPSHOT") == 1
-        VersionInfo.parsePart("42-alpha") == 42
-        VersionInfo.parsePart("0") == 0
-        VersionInfo.parsePart("") == 0
-        VersionInfo.parsePart("beta") == 0
+        VersionUtil.parsePart("123") == 123
+        VersionUtil.parsePart("1-SNAPSHOT") == 1
+        VersionUtil.parsePart("42-alpha") == 42
+        VersionUtil.parsePart("0") == 0
+        VersionUtil.parsePart("") == 0
+        VersionUtil.parsePart("beta") == 0
     }
 }
 
