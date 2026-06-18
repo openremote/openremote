37fb69ecd980ed1136741fe666b07003403b1aca
Ekhorn <koen@kschellingerhout.nl>
2026-06-18 10:10:51 +0200

    Remove accent color and correct padding/rounding of counters

--- ui/component/or-map/src/controls/legend.ts
+++ ui/component/or-map/src/controls/legend.ts
@@ -98,6 +98,14 @@ export class OrMapLegend extends LitElement {
                 display: none;
             }
 
+            or-vaadin-badge {
+                --vaadin-badge-background: var(--shades-contrast-10, #3A463A0D);
+                border-radius: calc(var(--lumo-border-radius-m) + 2px);
+                font-size: 12px;
+                color: var(--lumo-secondary-text-color);
+                padding: 0 calc(var(--lumo-space-m) - 4px);
+            }
+
             /* List: flat top connecting to title, rounded bottom */
             #legend-list {
                 min-width: 220px;
--- ui/component/or-map/src/controls/preset-filter.ts
+++ ui/component/or-map/src/controls/preset-filter.ts
@@ -71,6 +71,11 @@ export class OrMapPresetFilter extends LitElement {
                 top: 50%;
                 transform: translateY(-50%);
                 pointer-events: none;
+                --vaadin-badge-background: var(--shades-contrast-10, #3A463A0D);
+                border-radius: calc(var(--lumo-border-radius-m) + 2px);
+                font-size: 12px;
+                color: var(--lumo-secondary-text-color);
+                padding: 0 calc(var(--lumo-space-m) - 4px);
             }
         `;
     }
@@ -137,6 +142,13 @@ export class OrMapPresetFilter extends LitElement {
                 }
                 or-vaadin-item.filter-option {
                     padding: unset;
+                }
+                or-vaadin-badge {
+                    --vaadin-badge-background: var(--shades-contrast-10, #3A463A0D);
+                    border-radius: calc(var(--lumo-border-radius-m) + 2px);
+                    font-size: 12px;
+                    color: var(--lumo-secondary-text-color);
+                    padding: 0 calc(var(--lumo-space-m) - 4px);
                 }
             </style>
             <or-vaadin-list-box>
