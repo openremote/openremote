5ae761f54041786f9efafb82f01182401b891ad0
Dennis Kuijs <dennis@denniskuijs.nl>
2026-07-21 09:33:55 +0200

    wip

--- .ci_cd/host_init/clean.sh
+++ .ci_cd/host_init/clean.sh
@@ -6,3 +6,5 @@ echo "Deleting existing postgres data volume"
 docker volume rm or_postgresql-data 2> /dev/null
 echo "Deleting existing manager data volume"
 docker volume rm or_manager-data 2> /dev/null
+echo "Deleting existing extensions data volume"
+docker volume rm or_extensions-data 2> /dev/null
