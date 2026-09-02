6d1afecc586325bf679a015d016c14d605ced2f4
Alex Smolya <alexsmolya@gmail.com>
2026-09-02 21:31:44 +0200

    Fix bar chart component test validation
    Signed-off-by: Alex Smolya <alexsmolya@gmail.com>

--- ui/component/or-attribute-barchart/test/timeframe.test.ts
+++ ui/component/or-attribute-barchart/test/timeframe.test.ts
@@ -1,7 +1,33 @@
+/*
+ * Copyright 2026, OpenRemote Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 import { ct, expect } from "@openremote/test";
 
-import { BarChartInterval, OrAttributeBarChart } from "../src/index";
-import { getChartAxisBounds, getNavigationDuration, shiftTimeframe, type TimeframeUnit } from "../src/timeframe";
+import { BarChartInterval, OrAttributeBarChart } from "@openremote/or-attribute-barchart";
+import {
+  getChartAxisBounds,
+  getNavigationDuration,
+  shiftTimeframe,
+  type TimeframeUnit,
+} from "@openremote/or-attribute-barchart/timeframe";
+
+ct.use({ timezoneId: "Europe/Stockholm" });
 
 const HOUR = 60 * 60 * 1000;
 const DAY = 24 * HOUR;
